34eb75b4c6aa9f932856173957c9ddae7164d678
Chris Boulton <chris@mybb.com>
2005-07-06 05:56:20 +0000

    Doh!

---
+++ inc/languages/english/admin/plugins.lang.php
@@ -0,0 +1,14 @@
+<?php
+$l['nav_plugins'] = "Plugin Manager";
+
+$l['plugin_manager'] = "Plugin Manager";
+$l['plugin'] = "Plugin";
+$l['version'] = "Version";
+$l['author'] = "Author";
+$l['action'] = "Action";
+$l['deactivate_plugin'] = "Deactivate";
+$l['activate_plugin'] = "Activate";
+$l['plugin_activated'] = "The plugin has successfully been activated.";
+$l['plugin_deactivated'] = "The plugin has successfully been deactivated.";
+$l['no_plugins'] = "You do not currently have any plugins installed that exist in your plugins directory.";
+?>
