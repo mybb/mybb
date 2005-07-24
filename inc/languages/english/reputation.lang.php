697e37e8766cfa17e3ab25468986a221c5e62f13
MattR <contact@mybb.com>
2005-07-24 00:30:18 +0000

    Made the reputation error message a bit clearer (nothing too serious).

--- inc/languages/english/reputation.lang.php
+++ inc/languages/english/reputation.lang.php
@@ -1,7 +1,7 @@
 <?php
 $l['reputation'] = "Reputation";
 $l['error'] = "Error";
-$l['error_own_reputation'] = "You cannot add to your own reputation.";
+$l['error_own_reputation'] = "You cannot add to or remove points from your own reputation.";
 $l['error_reputation_same_post'] = "You have already expressed your opinion on this user's post.";
 $l['error_max_reputations_day'] = "You have reached the maximum number of reputations you can give to others per day.";
 $l['error_reputationdisabled'] = "You are not allowed to add reputation to this user.";
