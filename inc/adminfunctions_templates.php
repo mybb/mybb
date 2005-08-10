a0aa76f680dd446ee0e9e83a0dc75c409a0a6b87
Chris Boulton <chris@mybb.com>
2005-08-10 06:23:12 +0000

    Changed to use preg_replace

--- inc/adminfunctions_templates.php
+++ inc/adminfunctions_templates.php
@@ -14,29 +14,21 @@
 // replace something in that template, and then update it. It can also create
 // (by default) templates for sets which arent customised, based off the master
 //
-function find_replace_templatesets($title, $find, $replace, $autocreate=1, $casesensitive=1)
+function find_replace_templatesets($title, $find, $replace, $autocreate=1)
 {
 	global $db;
-	if($casesensitive != 1)
-	{
-		$function = "stri_replace";
-	}
-	else
-	{
-		$function = "str_replace";
-	}
 	if($autocreate != 0)
 	{
 		$query = $db->query("SELECT * FROM ".TABLE_PREFIX."templates WHERE title='$title' AND sid='-2'");
 		$master = $db->fetch_array($query);
-		$master['template'] = addslashes($function($find, $replace, $master['template']));
+		$master['template'] = addslashes(preg_replace($find, $replace, $master['template']));
 	}
 	$query = $db->query("SELECT s.sid, t.template, t.tid FROM ".TABLE_PREFIX."templatesets s LEFT JOIN ".TABLE_PREFIX."templates t ON (t.title='$title' AND t.sid=s.sid)");
 	while($template = $db->fetch_array($query))
 	{
 		if($template['template']) // Custom template exists for this group
 		{
-			$template['template'] = $function($find, $replace, $template['template']);
+			$template['template'] = preg_replace($find, $replace, $template['template']);
 			$updatetemp = array("template" => addslashes($template['template']));
 			$db->update_query(TABLE_PREFIX."templates", $updatetemp, "tid='".$template['tid']."'");
 		}
