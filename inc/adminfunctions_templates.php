3d3dfec2c96150a247226e0320ff8cff976f4c91
Chris Boulton <chris@mybb.com>
2005-08-22 08:10:37 +0000

    Made it so the function returns false without committing ANY changes if a match cannot be found in any single template.

--- inc/adminfunctions_templates.php
+++ inc/adminfunctions_templates.php
@@ -21,16 +21,26 @@ function find_replace_templatesets($title, $find, $replace, $autocreate=1)
 	{
 		$query = $db->query("SELECT * FROM ".TABLE_PREFIX."templates WHERE title='$title' AND sid='-2'");
 		$master = $db->fetch_array($query);
-		$master['template'] = addslashes(preg_replace($find, $replace, $master['template']));
+		$oldmaster = $master['template'];
+		$master['template'] = preg_replace($find, $replace, $master['template']);
+		if($oldmaster == $master['template'])
+		{
+			return false;
+		}
+		$master['template'] = addslashes($master['template'];
 	}
 	$query = $db->query("SELECT s.sid, t.template, t.tid FROM ".TABLE_PREFIX."templatesets s LEFT JOIN ".TABLE_PREFIX."templates t ON (t.title='$title' AND t.sid=s.sid)");
 	while($template = $db->fetch_array($query))
 	{
 		if($template['template']) // Custom template exists for this group
 		{
-			$template['template'] = preg_replace($find, $replace, $template['template']);
-			$updatetemp = array("template" => addslashes($template['template']));
-			$db->update_query(TABLE_PREFIX."templates", $updatetemp, "tid='".$template['tid']."'");
+			$newtemplate = preg_replace($find, $replace, $template['template']);
+			if($newtemplate == $template['template'])
+			{
+				return false;
+			}
+			$template['template'] = $newtemplate;
+			$update[] = $template;
 		}
 		elseif($autocreate != 0) // No template exists, create it based off master
 		{
@@ -43,4 +53,13 @@ function find_replace_templatesets($title, $find, $replace, $autocreate=1)
 			$db->insert_query(TABLE_PREFIX."templates", $newtemp);
 		}
 	}
+	if(is_array($update))
+	{
+		foreach($update as $template)
+		{
+			$updatetemp = array("template" => addslashes($template['template']));
+			$db->update_query(TABLE_PREFIX."templates", $updatetemp, "tid='".$template['tid']."'");
+		}
+	}
+	return true;
 }
