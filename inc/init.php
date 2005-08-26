de7036a9f3873c51d046452b439438e187c4e608
Chris Boulton <chris@mybb.com>
2005-08-26 00:45:23 +0000

    Moved constant from previous commit ino init.php because constants.php wasn't used - also removed constants.php

--- inc/init.php
+++ inc/init.php
@@ -82,4 +82,8 @@ $groupzerogreater = array("pmquota", "maxreputationsday", "attachquota");
 $displaygroupfields = array("title", "description", "namestyle", "usertitle", "stars", "starimage", "image");
 // These are fields in the usergroups table that are also forum permission specific
 $fpermfields = array("canview", "candlattachments", "canpostthreads", "canpostreplys", "canpostattachments", "canratethreads", "caneditposts", "candeleteposts", "candeletethreads", "caneditattachments", "canpostpolls", "canvotepolls", "cansearch");
+
+/* URL Definitions */
+define(PROFILE_URL, "member.php?action=profile&amp;uid={uid}");
+
 ?>
