f6acccc4c964d0bb72616539cdd5e06b7994e12a
Dennis Tsang <contact@mybb.com>
2005-11-11 20:30:15 +0000

    Fixing 2 'click here' messages from a long time ago http://community.mybboard.net/showthread.php?tid=923

--- inc/languages/english/archive.lang.php
+++ inc/languages/english/archive.lang.php
@@ -3,7 +3,7 @@ $l['archive_fullversion'] = "Full Version:";
 $l['archive_replies'] = "Replies";
 $l['archive_reply'] = "Reply";
 $l['archive_pages'] = "Pages:";
-$l['archive_note'] = "You're currently viewing a stripped down version of our content. To view for full version with proper formatting, <a href=\"{1}\">click here</a>";
+$l['archive_note'] = "You're currently viewing a stripped down version of our content.  <a href=\"{1}\">View the full version</a> with proper formatting.";
 $l['archive_reference_urls'] = "Reference URL's";
 $l['archive_nopermission'] = "Sorry but you do not have permission to access this resource.";
 $l['error_nothreads'] = "There are currently no threads in this forum.";
--- inc/languages/english/newreply.lang.php
+++ inc/languages/english/newreply.lang.php
@@ -16,7 +16,7 @@ $l['mod_options'] ="Moderator Options:";
 $l['close_thread'] = "<b>Close Thread</b>: prevent further posting in this thread.";
 $l['stick_thread'] = "<b>Stick Thread:</b> stick this thread to the top of the forum.";
 $l['thread_review'] = "Thread Review (Newest First)";
-$l['thread_review_more'] = "This thread has more than {1} replies. Click <a href=\"showthread.php?tid={2}\">here</a> to read the whole thread.";
+$l['thread_review_more'] = "This thread has more than {1} replies. <a href=\"showthread.php?tid={2}\">Read the whole thread.</a>";
 $l['posted_by'] = "Posted by";
 $l['draft_saved'] = "The new post has successfully been saved as a draft.<br />You will now be taken to your draft listing.";
 ?>
