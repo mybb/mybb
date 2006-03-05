8ff19b7797301f994b7c6ce55ff9aa73d0e155ab
Kilroy <contact@mybb.com>
2006-03-05 15:11:24 +0000

    Ok so I did have time to do something, but this was really, really needed.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  * $Id$
  */
 
-$templatelist = "index,index_whosonline,index_welcomemembertext,index_welcomeguest,index_whosonline_memberbit,forumbit_depth1_cat,forumbit_depth1_forum,forumbit_depth2_cat,forumbit_depth2_forum,forumbit_depth1_forum_lastpost,forumbit_depth2_forum_lastpost,index_modcolumn,forumbit_moderators,forumbit_subforums,index_welcomeguesttext"; 
+$templatelist = "index,index_whosonline,index_welcomemembertext,index_welcomeguest,index_whosonline_memberbit,forumbit_depth1_cat,forumbit_depth1_forum,forumbit_depth2_cat,forumbit_depth2_forum,forumbit_depth1_forum_lastpost,forumbit_depth2_forum_lastpost,index_modcolumn,forumbit_moderators,forumbit_subforums,index_welcomeguesttext";
 $templatelist .= ",index_birthdays_birthday,index_birthdays,index_pms,index_loginform,index_logoutlink,index_stats,forumbit_depth3";
 
 require "./global.php";
@@ -32,7 +32,7 @@ else
 }
 if($mybb->settings['showwol'] != "no" && $mybb->usergroup['canviewonline'] != "no")
 {
-	// Get the online users
+	// Get the online users.
 	$timesearch = time() - $mybb->settings['wolcutoffmins']*60;
 	$comma = '';
 	$query = $db->query("SELECT s.sid, s.ip, s.uid, s.time, s.location, u.username, u.invisible, u.usergroup, u.displaygroup FROM ".TABLE_PREFIX."sessions s LEFT JOIN ".TABLE_PREFIX."users u ON (s.uid=u.uid) WHERE s.time>'$timesearch' ORDER BY u.username ASC, s.time DESC");
@@ -41,13 +41,19 @@ if($mybb->settings['showwol'] != "no" && $mybb->usergroup['canviewonline'] != "n
 	$anoncount = 0;
 	$doneusers = array();
 
+	// Loop through all users.
 	while($user = $db->fetch_array($query))
 	{
+		// Create a key to test if this user is a search bot.
 		$botkey = strtolower(str_replace("bot=", '', $user['sid']));
+
+		// Decide what type of user we are dealing with.
 		if($user['uid'] > 0)
 		{
+			// The user is registered.
 			if($doneusers[$user['uid']] < $user['time'] || !$doneusers[$user['uid']])
 			{
+				// If the user is logged in anonymously, update the count for that.
 				if($user['invisible'] == "yes")
 				{
 					++$anoncount;
@@ -55,6 +61,7 @@ if($mybb->settings['showwol'] != "no" && $mybb->usergroup['canviewonline'] != "n
 				++$membercount;
 				if($user['invisible'] != "yes" || $mybb->usergroup['canviewwolinvis'] == "yes" || $user['uid'] == $mybb->user['uid'])
 				{
+					// If this usergroup can see anonymously logged-in users, mark them.
 					if($user['invisible'] == "yes")
 					{
 						$invisiblemark = "*";
@@ -63,24 +70,31 @@ if($mybb->settings['showwol'] != "no" && $mybb->usergroup['canviewonline'] != "n
 					{
 						$invisiblemark = '';
 					}
+
+					// Properly format the username and assign the template.
 					$user['username'] = formatname($user['username'], $user['usergroup'], $user['displaygroup']);
 					eval("\$onlinemembers .= \"".$templates->get("index_whosonline_memberbit", 1, 0)."\";");
 					$comma = ", ";
 				}
+				// This user has been handled.
 				$doneusers[$user['uid']] = $user['time'];
 			}
 		}
 		elseif(strstr($user['sid'], "bot=") !== false && $session->bots[$botkey])
 		{
+			// The user is a search bot.
 			$onlinemembers .= $comma.formatname($session->bots[$botkey], $botgroup);
 			$comma = ", ";
 			++$botcount;
 		}
 		else
 		{
+			// The user is a guest.
 			++$guestcount;
 		}
 	}
+
+	// Build the who's online bit on the index page.
 	$onlinecount = $membercount + $guestcount;
 	if($onlinecount != 1)
 	{
@@ -117,14 +131,18 @@ if($mybb->settings['showwol'] != "no" && $mybb->usergroup['canviewonline'] != "n
 	$lang->online_note = sprintf($lang->online_note, mynumberformat($onlinecount), $onlinebit, $mybb->settings['wolcutoffmins'], mynumberformat($membercount), $memberbit, mynumberformat($anoncount), $anonbit, mynumberformat($guestcount), $guestbit);
 	eval("\$whosonline = \"".$templates->get("index_whosonline")."\";");
 }
-// Get birthdays
+
+// Build the birthdays for to show on the index page.
 if($mybb->settings['showbirthdays'] != "no")
 {
+	// First, see what day this is.
 	$bdaycount = 0;
 	$bdaytime = time();
 	$bdaydate = mydate("j-n", $bdaytime, '', 0);
 	$year = mydate("Y", $bdaytime, '', 0);
-	$query = $db->query("SELECT uid, username, birthday FROM ".TABLE_PREFIX."users WHERE birthday LIKE '$bdaydate-%'");
+
+	// Select all users who have their birthday today.
+	$query = $db->simple_select(TABLE_PREFIX."users", "uid, username, birthday", "birthday LIKE '$bdaydate-%'");
 	$comma = '';
 	while($bdayuser = $db->fetch_array($query))
 	{
@@ -141,15 +159,21 @@ if($mybb->settings['showbirthdays'] != "no")
 		++$bdaycount;
 		$comma = ", ";
 	}
+
+	// If there are one or more birthdays, show them.
 	if($bdaycount > 0)
 	{
 		eval("\$birthdays = \"".$templates->get("index_birthdays")."\";");
 	}
 }
-// Get Forum Statistics
+
+// Build the forum statistics to show on the index page.
 if($mybb->settings['showindexstats'] != "no")
 {
+	// First, load the stats cache.
 	$stats = $cache->read("stats");
+
+	// Check who's the newest member.
 	if(!$stats['lastusername'])
 	{
 		$newestmember = "no-one";
@@ -158,11 +182,13 @@ if($mybb->settings['showindexstats'] != "no")
 	{
 		$newestmember = "<a href=\"member.php?action=profile&amp;uid=".$stats['lastuid']."\">".$stats['lastusername']."</a>";
 	}
+
+	// Format the stats language.
 	$lang->stats_posts_threads = sprintf($lang->stats_posts_threads, mynumberformat($stats['numposts']), mynumberformat($stats['numthreads']));
 	$lang->stats_numusers = sprintf($lang->stats_numusers, mynumberformat($stats['numusers']));
 	$lang->stats_newestuser = sprintf($lang->stats_newestuser, $newestmember);
-	
-	// Most users online
+
+	// Find out what the highest users online count is.
 	$mostonline = $cache->read("mostonline");
 	if($onlinecount > $mostonline['numusers'])
 	{
@@ -174,23 +200,39 @@ if($mybb->settings['showindexstats'] != "no")
 	$recordcount = $mostonline['numusers'];
 	$recorddate = mydate($mybb->settings['dateformat'], $mostonline['time']);
 	$recordtime = mydate($mybb->settings['timeformat'], $mostonline['time']);
-	
+
+	// Then format that language string.
 	$lang->stats_mostonline = sprintf($lang->stats_mostonline, mynumberformat($recordcount), $recorddate, $recordtime);
-	
+
 	eval("\$forumstats = \"".$templates->get("index_stats")."\";");
 }
-// Get Forums
-$query = $db->query("SELECT f.*, t.subject AS lastpostsubject FROM ".TABLE_PREFIX."forums f LEFT JOIN ".TABLE_PREFIX."threads t ON (t.tid = f.lastposttid) WHERE active!='no' ORDER BY f.pid, f.disporder");
+
+// Get the forums we will need to show.
+$query = $db->query(
+	"SELECT f.*, t.subject AS lastpostsubject
+	FROM ".TABLE_PREFIX."forums f
+	LEFT JOIN ".TABLE_PREFIX."threads t ON (t.tid = f.lastposttid)
+	WHERE active!='no'
+	ORDER BY f.pid, f.disporder
+");
+// Build a forum cache.
 while($forum = $db->fetch_array($query))
 {
 	$fcache[$forum['pid']][$forum['disporder']][$forum['fid']] = $forum;
 
 }
 $forumpermissions = forum_permissions();
-// Get forum moderators
+
+// Get the forum moderators if the setting is enabled.
 if($mybb->settings['modlist'] != "off")
 {
-	$query = $db->query("SELECT m.uid, m.fid, u.username FROM ".TABLE_PREFIX."moderators m LEFT JOIN ".TABLE_PREFIX."users u ON (m.uid=u.uid) ORDER BY u.username");
+	$query = $db->query("
+		SELECT m.uid, m.fid, u.username
+		FROM ".TABLE_PREFIX."moderators m
+		LEFT JOIN ".TABLE_PREFIX."users u ON (m.uid=u.uid)
+		ORDER BY u.username
+	");
+	// Build a moderator cache.
 	while($moderator = $db->fetch_array($query))
 	{
 		$moderatorcache[$moderator['fid']][] = $moderator;
@@ -200,16 +242,26 @@ if($mybb->settings['modlist'] != "off")
 $excols = "index";
 $permissioncache['-1'] = "1";
 $bgcolor = "trow1";
+
+// Decide if we're showing first-level subforums on the index page.
 if($mybb->settings['subforumsindex'] != 0)
 {
 	$showdepth = 3;
 }
 else
 {
-	$showdepth =2;
+	$showdepth = 2;
 }
 $forums = getforums();
 
+/**
+* Get a list of forums.
+*
+* @param unknown
+* @param int The depth to return forums with.
+* @param unknown
+* @return string The filled-in forum listing template.
+*/
 function getforums($pid="0", $depth=1, $permissions="")
 {
 	global $fcache, $moderatorcache, $forumpermissions, $theme, $mybb, $mybbforumread, $settings, $mybbuser, $excols, $templates, $bgcolor, $collapsed, $lang, $showdepth, $forumpass, $plugins, $parser;
