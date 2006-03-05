e49ee55f3db32cb2ac0732dd477ac827ae288b8a
decswxaqz <contact@mybb.com>
2006-03-05 11:10:22 +0000

    -No displayable groups bug -Mods not displayed if no displayable groups -Fixed if no mods either then no message -Commenting -Queries now simple_select

--- showteam.php
+++ showteam.php
@@ -20,71 +20,93 @@ $plugins->run_hooks("showteam_start");
 
 $teamquery = "";
 $comma = '';
-$query = $db->query("SELECT * FROM ".TABLE_PREFIX."usergroups WHERE showforumteam='yes' AND gid!='6' ORDER BY title ASC");
+
+$options = array(
+	'order_by' => 'u.title',
+	'order_dir' => 'ASC',
+);
+//Get all visible usergroups that aren't moderators
+$query = $db->simple_select(TABLE_PREFIX."usergroups u", "u.*", "u.showforumteam='yes' AND u.gid!='6'", $options);
 while($usergroup = $db->fetch_array($query))
 {
 	$teams[$usergroup['gid']] = $usergroup;
 	$teamquery .= "$comma'$usergroup[gid]'";
 	$comma = ",";
 }
-$query = $db->query("SELECT u.*, u.usergroup, u.displaygroup FROM ".TABLE_PREFIX."users u WHERE u.displaygroup IN ($teamquery) OR (u.displaygroup = 0 AND u.usergroup IN ($teamquery)) ORDER BY u.username ASC");
-while($user = $db->fetch_array($query))
+//If we have some groups to display then if statement
+//If no groups then proceed to moderators
+if(!empty($teamquery))
 {
-	if($user['displaygroup'] == 0)
+	$options = array(
+		'order_by' => 'u.username',
+		'order_dir' => 'ASC',
+	);
+	//Get the users in those groups
+	$query = $db->simple_select(TABLE_PREFIX."users u", "u.*", "u.displaygroup IN ($teamquery) OR (u.displaygroup = 0 AND u.usergroup IN ($teamquery))", $options);
+	while($user = $db->fetch_array($query))
 	{
-		$users[$user['usergroup']][$user['uid']] = $user;
-	}
-	else
-	{
-		$users[$user['displaygroup']][$user['uid']] = $user;
-	}
-}
-if(!is_array($users))
-{
-	error($lang->error_noteamstoshow);
-}
-$usergrouprows = '';
-$usergroups = '';
-while(list($gid, $usergroup) = each($teams))
-{
-	if(is_array($users[$gid]))
-	{
-		$bgcolor = "trow1";
-		while(list($uid, $user) = each($users[$gid]))
+		if($user['displaygroup'] == 0)
 		{
-			$post['uid'] = $user['uid'];
-			$user['username'] = formatname($user['username'], $user['usergroup'], $user['displaygroup']);
-			if($user['hideemail'] != "yes")
-			{
-				eval("\$emailcode = \"".$templates->get("postbit_email")."\";");
-			}
-			else
-			{
-				$emailcode = '';
-			}
-			if($user['receivepms'] != "no" && $mybb->settings['enablepms'] != "no")
-			{
-				eval("\$pmcode = \"".$templates->get("postbit_pm")."\";");
-			}
-			else
-			{
-				$pmcode = '';
-			}
-			eval("\$usergrouprows .= \"".$templates->get("showteam_usergroup_user")."\";");
-			if($bgcolor == "trow1")
-			{
-				$bgcolor = "trow2";
-			}
-			else
+			$users[$user['usergroup']][$user['uid']] = $user;
+		}
+		else
+		{
+			$users[$user['displaygroup']][$user['uid']] = $user;
+		}
+	}
+	//If there are users then start to display them
+	//Else carry on to moderators
+	if(is_array($users))
+	{
+		$usergrouprows = '';
+		$usergroups = '';
+		//For each team
+		while(list($gid, $usergroup) = each($teams))
+		{
+			//If there are users in this group
+			if(is_array($users[$gid]))
 			{
 				$bgcolor = "trow1";
-			}
-		}
-		eval("\$usergroups .= \"".$templates->get("showteam_usergroup")."\";");
-	}
-	$usergrouprows = '';
-}
+				//For each user in this group
+				while(list($uid, $user) = each($users[$gid]))
+				{
+					$post['uid'] = $user['uid'];
+					$user['username'] = formatname($user['username'], $user['usergroup'], $user['displaygroup']);
+					if($user['hideemail'] != "yes")
+					{
+						eval("\$emailcode = \"".$templates->get("postbit_email")."\";");
+					}
+					else
+					{
+						$emailcode = '';
+					}
+					if($user['receivepms'] != "no" && $mybb->settings['enablepms'] != "no")
+					{
+						eval("\$pmcode = \"".$templates->get("postbit_pm")."\";");
+					}
+					else
+					{
+						$pmcode = '';
+					}
+					eval("\$usergrouprows .= \"".$templates->get("showteam_usergroup_user")."\";");
+					if($bgcolor == "trow1")
+					{
+						$bgcolor = "trow2";
+					}
+					else
+					{
+						$bgcolor = "trow1";
+					}
+				}//End for each user in group
+				eval("\$usergroups .= \"".$templates->get("showteam_usergroup")."\";");
+			}//End for each group
+			$usergrouprows = '';
+		}//For each team
+	}//End if users
+}//End if displayable groups
+
 unset($user);
+//Get list of moderators
 $query = $db->query("
 	SELECT m.fid, m.uid, u.username, u.usergroup, u.displaygroup, u.hideemail, u.receivepms, f.name
 	FROM ".TABLE_PREFIX."moderators m
@@ -102,7 +124,14 @@ while($mod = $db->fetch_array($query))
 	$modforums[$mod['uid']] .= "<a href=\"forumdisplay.php?fid=$mod[fid]\">$mod[name]</a>";
 }
 $modrows = '';
-if(is_array($modsarray))
+
+//If there are no moderators then display error message
+//Otherwise, display users
+if(!is_array($modsarray))
+{
+	error($lang->error_noteamstoshow);
+}
+else
 {
 	$bgcolor = "trow1";
 	while(list($uid, $user) = each($modsarray))
@@ -129,7 +158,7 @@ if(is_array($modsarray))
 		$username = formatname($user['username'], $user['usergroup'], $user['displaygroup']);
 		$location = $user['location'];
 		eval("\$modrows .= \"".$templates->get("showteam_moderators_mod")."\";");
-		if($bgcolor == "trow1") 
+		if($bgcolor == "trow1")
 		{
 			$bgcolor = "trow2";
 		}
