b844db48e74beee930a7f3a959fc632cbc0dd3be
Kilroy <contact@mybb.com>
2006-03-08 07:05:10 +0000

    Improved feed output to better comply with feed standards.

--- inc/class_syndication.php
+++ inc/class_syndication.php
@@ -116,9 +116,23 @@ class Syndication
 	 */
 	function generate_feed()
 	{
-		header("Content-Type: text/xml");
+		// Output an appropriate header matching the feed type.
+		switch($this->feed_type)
+		{
+			case "atom1.0":
+				header("Content-Type: application/atom+xml");
+			break;
+
+			case "rss0.92":
+			case "rss2.0":
+			default:
+				header("Content-Type: application/rss+xml");
+			break;
+		}
+
 		echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 
+		// Build the parts of the feed.
 		$this->build_header();
 		$this->build_entries();
 		$this->build_footer();
@@ -136,17 +150,17 @@ class Syndication
 			case "rss2.0":
 				echo "<rss version=\"2.0\">\n";
 				echo "\t<channel>\n";
-				echo "\t\t<title>".htmlspecialchars($mybb->settings['bbname'])."</title>\n";
+				echo "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
 				echo "\t\t<link>".$mybb->settings['bburl']."</link>\n";
-				echo "\t\t<description>".htmlspecialchars($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
-				echo "\t\t<lastBuildDate>".date("r")."</lastBuildDate>\n";
+				echo "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
+				echo "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
 				echo "\t\t<generator>MyBB ".$mybboard['internalver']."</generator>\n";
 			break;
 
 			case "atom1.0":
 				echo "<feed xmlns=\"http://www.w3.org/2005/Atom\">\n";
-				echo "\t<title>".htmlspecialchars($mybb->settings['bbname'])."</title>\n";
-				echo "\t<id>".$mybb->settings['bburl']."</id>\n";
+				echo "\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
+				echo "\t<id>".$mybb->settings['bburl']."/</id>\n";
 				echo "\t<link rel=\"self\" href=\"".$mybb->settings['bburl']."/syndication.php?type=atom1.0&amp;limit=".$this->limit."\"/>\n";
 				echo "\t<updated>".date("Y-m-d\TH:i:s\Z")."</updated>\n";
 				echo "\t<generator uri=\"http://mybboard.com\" version=\"".$mybboard['internalver']."\">MyBB</generator>\n";
@@ -155,20 +169,20 @@ class Syndication
 			case "rss0.92":
 				echo "<rss version=\"0.92\">\n";
 				echo "\t<channel>\n";
-				echo "\t\t<title>".htmlspecialchars($mybb->settings['bbname'])."</title>\n";
+				echo "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
 				echo "\t\t<link>".$mybb->settings['bburl']."</link>\n";
-				echo "\t\t<description>".htmlspecialchars($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
-				echo "\t\t<lastBuildDate>".date("r")."</lastBuildDate>\n";
+				echo "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
+				echo "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
 				echo "\t\t<language>en</language>\n";
 			break;
 
 			default:
 				echo "<rss version=\"0.92\">\n";
 				echo "\t<channel>\n";
-				echo "\t\t<title>".htmlspecialchars($mybb->settings['bbname'])."</title>\n";
+				echo "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
 				echo "\t\t<link>".$mybb->settings['bburl']."</link>\n";
-				echo "\t\t<description>".htmlspecialchars($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
-				echo "\t\t<lastBuildDate>".date("r")."</lastBuildDate>\n";
+				echo "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
+				echo "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
 				echo "\t\t<language>en</language>\n";
 			break;
 		}
@@ -193,12 +207,12 @@ class Syndication
 		);
 		while($thread = $db->fetch_array($query))
 		{
-			$thread['subject'] = htmlspecialchars($thread['subject']);
-			$thread['forumname'] = htmlspecialchars($thread['forumname']);
+			$thread['subject'] = htmlentities($thread['subject']);
+			$thread['forumname'] = htmlentities($thread['forumname']);
 			$postdate = mydate($mybb->settings['dateformat'], $thread['dateline'], "", 0);
 			$posttime = mydate($mybb->settings['timeformat'], $thread['dateline'], "", 0);
 			$thread['postmessage'] = nl2br(htmlspecialchars_uni($thread['postmessage']));
-			$last_updated = mydate("r", $thread['dateline'], "", 0);
+			$last_updated = mydate("D, d M Y H:i:s O", $thread['dateline'], "", 0);
 			$last_updated_atom = mydate("Y-m-d\TH:i:s\Z", $thread['dateline'], "", 0);
 			switch($this->feed_type)
 			{
@@ -207,10 +221,10 @@ class Syndication
 					echo "\t\t\t<guid>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</guid>\n";
 					echo "\t\t\t<title>".$thread['subject']."</title>\n";
 					echo "\t\t\t<author>".$thread['username']."</author>\n";
-					$description = htmlspecialchars($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlspecialchars($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
+					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
 					if($thread['postmessage'])
 					{
-						$description .= "\n<br /><br />".$thread['postmessage'];
+						$description .= "\n<br />".htmlentities($thread['postmessage']);
 					}
 					echo "\t\t\t<description><![CDATA[".$description."]]></description>";
 					echo "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</link>\n";
@@ -227,10 +241,10 @@ class Syndication
 					echo "\t\t<author>\n";
 					echo "\t\t\t<name>".$thread['username']."</name>\n";
 					echo "\t\t</author>\n";
-					$description = htmlspecialchars($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlspecialchars($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
+					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
 					if($thread['postmessage'])
 					{
-						$description .= "\n<br />".$thread['postmessage'];
+						$description .= "\n<br />".htmlentities($thread['postmessage']);
 					}
 					echo "\t\t\t<content type=\"html\"><![CDATA[".$description."]]></content>";
 					echo "\t</entry>\n";
@@ -240,10 +254,10 @@ class Syndication
 					echo "\t\t<item>\n";
 					echo "\t\t\t<title>".$thread['subject']."</title>\n";
 					echo "\t\t\t<author>".$thread['username']."</author>\n";
-					$description = htmlspecialchars($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlspecialchars($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
+					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
 					if($thread['postmessage'])
 					{
-						$description .= "\n<br />".$thread['postmessage'];
+						$description .= "\n<br />".htmlentities($thread['postmessage']);
 					}
 					echo "\t\t\t<description><![CDATA[".$description."]]></description>";
 					echo "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</link>\n";
@@ -254,10 +268,10 @@ class Syndication
 					echo "\t\t<item>\n";
 					echo "\t\t\t<title>".$thread['subject']."</title>\n";
 					echo "\t\t\t<author>".$thread['username']."</author>\n";
-					$description = htmlspecialchars($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlspecialchars($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
+					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
 					if($thread['postmessage'])
 					{
-						$description .= "\n<br />".$thread['postmessage'];
+						$description .= "\n<br />".htmlentities($thread['postmessage']);
 					}
 					echo "\t\t\t<description><![CDATA[".$description."]]></description>";
 					echo "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</link>\n";
--- syndication.php
+++ syndication.php
@@ -21,19 +21,19 @@ $syndication = new Syndication();
 $syndication->set_feed_type($mybb->input['type']);
 $syndication->set_limit($mybb->input['limit']);
 
-/* Syndicate a specific forum or all viewable? */
+// Syndicate a specific forum or all viewable?
 if(isset($mybb->input['fid']))
 {
 	$flist = $mybb->input['fid'];
 	$forums = explode(',', $flist);
 	$syndication->set_forum_list($forums);
 }
-else 
+else
 {
 	$syndication->set_forum_list();
 }
 
-/* This is where all the good stuff happens. */
+// This is where all the good stuff happens.
 $syndication->generate_feed();
 
 ?>
