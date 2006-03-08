8615dcc40f54bf69e918fee796f9acd087042490
Kilroy <contact@mybb.com>
2006-03-08 19:07:27 +0000

    Here's my lame attempt at a proper syndication class. Would like to hear comments. Also, it's not working, it starts downloading on view.

--- inc/class_syndication.php
+++ inc/class_syndication.php
@@ -12,12 +12,7 @@
 /*
 Example code:
 
-$syndication = new Syndication();
-$syndication->set_feed_type('atom');
-$syndication->set_limit(20);
-$forums = array(1, 5, 6);
-$syndication->set_forum_list($forums);
-$syndication->generate_feed();
+TO BE WRITTEN
 
 */
 
@@ -28,40 +23,33 @@ class Syndication
 	 *
 	 * @var string
 	 */
-	var $feed_type = 'rss0.92';
+	var $feed_format = 'rss0.92';
 
 	/**
-	 * The number of items to list.
+	 * The XML to output.
 	 *
-	 * @var int
+	 * @var string
 	 */
-	var $limit = 15;
-
-	/**
-	 * The list of forums to grab from.
-	 *
-	 * @var array
-	 */
-	var $forumlist;
+	var $xml = "";
 
 	/**
 	 * Set the type of feed to be used.
 	 *
 	 * @param string The feed type.
 	 */
-	function set_feed_type($feed_type)
+	function set_feed_format($feed_format)
 	{
-		if($feed_type == 'rss2.0')
+		if($feed_format == 'rss2.0')
 		{
-			$this->feed_type = 'rss2.0';
+			$this->feed_format = 'rss2.0';
 		}
-		elseif($feed_type == 'atom1.0')
+		elseif($feed_format == 'atom1.0')
 		{
-			$this->feed_type = 'atom1.0';
+			$this->feed_format = 'atom1.0';
 		}
 		else
 		{
-			$this->feed_type = 'rss0.92';
+			$this->feed_format = 'rss0.92';
 		}
 	}
 
@@ -72,7 +60,7 @@ class Syndication
 	 */
 	function set_limit($limit)
 	{
-		if($limit < 1)
+		if(intval($limit) < 1)
 		{
 			error($lang->error_invalid_limit);
 		}
@@ -83,41 +71,13 @@ class Syndication
 	}
 
 	/**
-	 * Set the forum(s) to grab posts from.
-	 *
-	 * @param array Array of forum ids.
-	 */
-	function set_forum_list($forumlist = array())
-	{
-		$unviewable = getunviewableforums();
-		if($unviewable)
-		{
-			$unviewable = "AND f.fid NOT IN($unviewable)";
-		}
-
-		if(!empty($forumlist))
-		{
-			$forum_ids = "'-1'";
-			foreach($forumlist as $fid)
-			{
-				$forum_ids .= ",'".intval($fid)."'";
-			}
-			$this->forumlist = "AND f.fid IN ($forum_ids) $unviewable";
-		}
-		else
-		{
-			$this->forumlist = $unviewable;
-		}
-	}
-
-	/**
 	 * Generate and echo XML for the feed.
 	 *
 	 */
-	function generate_feed()
+	function output_feed()
 	{
 		// Output an appropriate header matching the feed type.
-		switch($this->feed_type)
+		switch($this->feed_format)
 		{
 			case "atom1.0":
 				header("Content-Type: application/atom+xml");
@@ -130,182 +90,169 @@ class Syndication
 			break;
 		}
 
-		echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
-
-		// Build the parts of the feed.
-		$this->build_header();
-		$this->build_entries();
-		$this->build_footer();
+		$this->xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n".$this->xml;
 	}
 
 	/**
-	 * Build the feed header.
+	 * Add the main wrapper to the feed.
 	 *
 	 */
-	function build_header()
+	function start_wrapper()
 	{
 		global $mybb, $lang, $mybboard;
-		switch($this->feed_type)
+
+		// Output the appropriate header.
+		switch($this->feed_format)
 		{
+			// Rss 2.0 wants a <channel>.
 			case "rss2.0":
-				echo "<rss version=\"2.0\">\n";
-				echo "\t<channel>\n";
-				echo "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
-				echo "\t\t<link>".$mybb->settings['bburl']."</link>\n";
-				echo "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
-				echo "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
-				echo "\t\t<generator>MyBB ".$mybboard['internalver']."</generator>\n";
+				$this->xml .= "<rss version=\"2.0\">\n";
+				$this->xml .= "\t<channel>\n";
+				$this->xml .= "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
+				$this->xml .= "\t\t<link>".$mybb->settings['bburl']."</link>\n";
+				$this->xml .= "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
+				$this->xml .= "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
+				$this->xml .= "\t\t<generator>MyBB ".$mybboard['internalver']."</generator>\n";
 			break;
 
+			// Atom 1.0 needs nothing.
 			case "atom1.0":
-				echo "<feed xmlns=\"http://www.w3.org/2005/Atom\">\n";
-				echo "\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
-				echo "\t<id>".$mybb->settings['bburl']."/</id>\n";
-				echo "\t<link rel=\"self\" href=\"".$mybb->settings['bburl']."/syndication.php?type=atom1.0&amp;limit=".$this->limit."\"/>\n";
-				echo "\t<updated>".date("Y-m-d\TH:i:s\Z")."</updated>\n";
-				echo "\t<generator uri=\"http://mybboard.com\" version=\"".$mybboard['internalver']."\">MyBB</generator>\n";
+				$this->xml .= "<feed xmlns=\"http://www.w3.org/2005/Atom\">\n";
+				$this->xml .= "\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
+				$this->xml .= "\t<id>".$mybb->settings['bburl']."/</id>\n";
+				$this->xml .= "\t<link rel=\"self\" href=\"".$mybb->settings['bburl']."/syndication.php?type=atom1.0&amp;limit=".$this->limit."\"/>\n";
+				$this->xml .= "\t<updated>".date("Y-m-d\TH:i:s\Z")."</updated>\n";
+				$this->xml .= "\t<generator uri=\"http://mybboard.com\" version=\"".$mybboard['internalver']."\">MyBB</generator>\n";
 			break;
 
+			// Rss 0.92 wants a <channel>.
 			case "rss0.92":
-				echo "<rss version=\"0.92\">\n";
-				echo "\t<channel>\n";
-				echo "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
-				echo "\t\t<link>".$mybb->settings['bburl']."</link>\n";
-				echo "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
-				echo "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
-				echo "\t\t<language>en</language>\n";
+				$this->xml .= "<rss version=\"0.92\">\n";
+				$this->xml .= "\t<channel>\n";
+				$this->xml .= "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
+				$this->xml .= "\t\t<link>".$mybb->settings['bburl']."</link>\n";
+				$this->xml .= "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
+				$this->xml .= "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
+				$this->xml .= "\t\t<language>en</language>\n";
 			break;
 
+			// Default is RSS 0.92.
 			default:
-				echo "<rss version=\"0.92\">\n";
-				echo "\t<channel>\n";
-				echo "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
-				echo "\t\t<link>".$mybb->settings['bburl']."</link>\n";
-				echo "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
-				echo "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
-				echo "\t\t<language>en</language>\n";
+				$this->xml .= "<rss version=\"0.92\">\n";
+				$this->xml .= "\t<channel>\n";
+				$this->xml .= "\t\t<title>".htmlentities($mybb->settings['bbname'])."</title>\n";
+				$this->xml .= "\t\t<link>".$mybb->settings['bburl']."</link>\n";
+				$this->xml .= "\t\t<description>".htmlentities($mybb->settings['bbname'])." - ".$mybb->settings['bburl']."</description>\n";
+				$this->xml .= "\t\t<lastBuildDate>".date("D, d M Y H:i:s O")."</lastBuildDate>\n";
+				$this->xml .= "\t\t<language>en</language>\n";
 			break;
 		}
 	}
 
 	/**
-	 * Build the feed entries.
+	 * Add a post to the feed.
 	 *
 	 */
-	function build_entries()
+	function add_post($post)
 	{
-		global $db, $mybb, $lang;
-		$query = $db->query("
-			SELECT t.*, f.name AS forumname, p.message AS postmessage
-			FROM ".TABLE_PREFIX."threads t
-			LEFT JOIN ".TABLE_PREFIX."forums f ON (f.fid=t.fid)
-			LEFT JOIN ".TABLE_PREFIX."posts p ON (p.pid=t.firstpost)
-			WHERE 1=1
-			AND p.visible=1 $this->forumlist
-			ORDER BY t.dateline DESC
-			LIMIT 0, ".$this->limit
-		);
-		while($thread = $db->fetch_array($query))
+		global $mybb, $lang;
+
+		// Make a $post array containing the necessary details.
+		$post['tid'] = $thread['tid'];
+		$post['subject'] = htmlentities($thread['subject']);
+		$post['fid'] = $thread['fid'];
+		$post['forumname'] = htmlentities($thread['forumname']);
+		$post['date'] = mydate($mybb->settings['dateformat'], $thread['dateline'], "", 0);
+		$post['time'] = mydate($mybb->settings['timeformat'], $thread['dateline'], "", 0);
+		$post['username'] = htmlentities($thread['username']);
+		$post['message'] = nl2br(htmlentities($thread['postmessage']));
+		$post['last_updated'] = mydate("D, d M Y H:i:s O", $thread['dateline'], "", 0);
+		$post['last_updated_atom'] = mydate("Y-m-d\TH:i:s\Z", $thread['dateline'], "", 0);
+
+		// Append to the feed XML, depending on the feed format.
+		switch($this->feed_format)
 		{
-			$thread['subject'] = htmlentities($thread['subject']);
-			$thread['forumname'] = htmlentities($thread['forumname']);
-			$postdate = mydate($mybb->settings['dateformat'], $thread['dateline'], "", 0);
-			$posttime = mydate($mybb->settings['timeformat'], $thread['dateline'], "", 0);
-			$thread['postmessage'] = nl2br(htmlspecialchars_uni($thread['postmessage']));
-			$last_updated = mydate("D, d M Y H:i:s O", $thread['dateline'], "", 0);
-			$last_updated_atom = mydate("Y-m-d\TH:i:s\Z", $thread['dateline'], "", 0);
-			switch($this->feed_type)
-			{
-				case "rss2.0";
-					echo "\t\t<item>\n";
-					echo "\t\t\t<guid>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</guid>\n";
-					echo "\t\t\t<title>".$thread['subject']."</title>\n";
-					echo "\t\t\t<author>".$thread['username']."</author>\n";
-					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
-					if($thread['postmessage'])
-					{
-						$description .= "\n<br />".htmlentities($thread['postmessage']);
-					}
-					echo "\t\t\t<description><![CDATA[".$description."]]></description>";
-					echo "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</link>\n";
-					echo "\t\t\t<category domain=\"".$mybb->settings['bburl']."/forumdisplay.php?fid=".$thread['fid']."\">".$thread['forumname']."</category>\n";
-					echo "\t\t\t<pubDate>".$last_updated."</pubDate>\n";
-					echo "\t\t</item>\n";
-				break;
+			// Add an <item> as entry for RSS 2.0.
+			case "rss2.0";
+				$this->xml .= "\t\t<item>\n";
+				$this->xml .= "\t\t\t<guid>".$mybb->settings['bburl']."/showthread.php?tid=".$post['tid']."&amp;action=newpost</guid>\n";
+				$this->xml .= "\t\t\t<title>".$post['subject']."</title>\n";
+				$this->xml .= "\t\t\t<author>".$post['username']."</author>\n";
+				$description = htmlentities($lang->forum)." ".$post['forumname']."\r\n<br />".$lang->posted_by." ".$post['username']." ".$lang->on." ".$post['date']." ".$post['time'];
+				$description .= "\n<br />".$post['message'];
+				$this->xml .= "\t\t\t<description><![CDATA[".$post['message']."]]></description>";
+				$this->xml .= "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$post['tid']."&amp;action=newpost</link>\n";
+				$this->xml .= "\t\t\t<category domain=\"".$mybb->settings['bburl']."/forumdisplay.php?fid=".$post['fid']."\">".$post['forumname']."</category>\n";
+				$this->xml .= "\t\t\t<pubDate>".$post['last_updated']."</pubDate>\n";
+				$this->xml .= "\t\t</item>\n";
+			break;
 
-				case "atom1.0":
-					echo "\t<entry>\n";
-					echo "\t\t<id>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</id>\n";
-					echo "\t\t<title>".$thread['subject']."</title>\n";
-					echo "\t\t<updated>".$last_updated_atom."</updated>\n";
-					echo "\t\t<author>\n";
-					echo "\t\t\t<name>".$thread['username']."</name>\n";
-					echo "\t\t</author>\n";
-					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
-					if($thread['postmessage'])
-					{
-						$description .= "\n<br />".htmlentities($thread['postmessage']);
-					}
-					echo "\t\t\t<content type=\"html\"><![CDATA[".$description."]]></content>";
-					echo "\t</entry>\n";
-				break;
+			// Add an <entry> as entry for Atom 1.0.
+			case "atom1.0":
+				$this->xml .= "\t<entry>\n";
+				$this->xml .= "\t\t<id>".$mybb->settings['bburl']."/showthread.php?tid=".$post['tid']."&amp;action=newpost</id>\n";
+				$this->xml .= "\t\t<title>".$post['subject']."</title>\n";
+				$this->xml .= "\t\t<updated>".$post['last_updated_atom']."</updated>\n";
+				$this->xml .= "\t\t<author>\n";
+				$this->xml .= "\t\t\t<name>".$post['username']."</name>\n";
+				$this->xml .= "\t\t</author>\n";
+				$description = htmlentities($lang->forum)." ".$post['forumname']."\r\n<br />".$lang->posted_by." ".$post['username']." ".$lang->on." ".$post['date']." ".$post['time'];
+				$description .= "\n<br />".$post['message'];
+				$this->xml .= "\t\t\t<content type=\"html\"><![CDATA[".$description."]]></content>";
+				$this->xml .= "\t</entry>\n";
+			break;
 
-				case "rss0.92":
-					echo "\t\t<item>\n";
-					echo "\t\t\t<title>".$thread['subject']."</title>\n";
-					echo "\t\t\t<author>".$thread['username']."</author>\n";
-					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
-					if($thread['postmessage'])
-					{
-						$description .= "\n<br />".htmlentities($thread['postmessage']);
-					}
-					echo "\t\t\t<description><![CDATA[".$description."]]></description>";
-					echo "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</link>\n";
-					echo "\t\t</item>\n";
-				break;
+			// Add an <item> as entry for RSS 0.92.
+			case "rss0.92":
+				$this->xml .= "\t\t<item>\n";
+				$this->xml .= "\t\t\t<title>".$post['subject']."</title>\n";
+				$this->xml .= "\t\t\t<author>".$post['username']."</author>\n";
+				$description = htmlentities($lang->forum)." ".$post['forumname']."\r\n<br />".$lang->posted_by." ".$post['username']." ".$lang->on." ".$post['date']." ".$post['time'];
+				$description .= "\n<br />".$post['message'];
+				$this->xml .= "\t\t\t<description><![CDATA[".$post['message']."]]></description>";
+				$this->xml .= "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$post['tid']."&amp;action=newpost</link>\n";
+				$this->xml .= "\t\t</item>\n";
+			break;
 
-				default:
-					echo "\t\t<item>\n";
-					echo "\t\t\t<title>".$thread['subject']."</title>\n";
-					echo "\t\t\t<author>".$thread['username']."</author>\n";
-					$description = htmlentities($lang->forum." ".$thread['forumname'])."\r\n<br />".htmlentities($lang->posted_by." ".$thread['username']." ".$lang->on." ".$postdate." ".$posttime);
-					if($thread['postmessage'])
-					{
-						$description .= "\n<br />".htmlentities($thread['postmessage']);
-					}
-					echo "\t\t\t<description><![CDATA[".$description."]]></description>";
-					echo "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$thread['tid']."&amp;action=newpost</link>\n";
-					echo "\t\t</item>\n";
-				break;
-			}
+			// Default is RSS 0.92.
+			default:
+				$this->xml .= "\t\t<item>\n";
+				$this->xml .= "\t\t\t<title>".$post['subject']."</title>\n";
+				$this->xml .= "\t\t\t<author>".$post['username']."</author>\n";
+				$description = htmlentities($lang->forum)." ".$post['forumname']."\r\n<br />".$lang->posted_by." ".$post['username']." ".$lang->on." ".$post['date']." ".$post['time'];
+				$description .= "\n<br />".$post['message'];
+				$this->xml .= "\t\t\t<description><![CDATA[".$post['message']."]]></description>";
+				$this->xml .= "\t\t\t<link>".$mybb->settings['bburl']."/showthread.php?tid=".$post['tid']."&amp;action=newpost</link>\n";
+				$this->xml .= "\t\t</item>\n";
+			break;
 		}
 	}
 
 	/**
-	 * Build the feed footer.
+	 * Add the wrapper end.
 	 *
 	 */
-	function build_footer()
+	function end_wrapper()
 	{
-		switch($this->feed_type)
+		switch($this->feed_format)
 		{
 			case "rss2.0":
-				echo "\t</channel>\n";
-				echo "</rss>";
+				$this->xml .= "\t</channel>\n";
+				$this->xml .= "</rss>";
 			break;
 
 			case "atom1.0":
-				echo "</feed>";
+				$this->xml .= "</feed>";
 			break;
 
 			case "rss0.92":
-				echo "\t</channel>\n";
-				echo "</rss>";
+				$this->xml .= "\t</channel>\n";
+				$this->xml .= "</rss>";
 			break;
 
 			default:
-				echo "\t</channel>\n";
-				echo "</rss>";
+				$this->xml .= "\t</channel>\n";
+				$this->xml .= "</rss>";
 			break;
 		}
 	}
--- syndication.php
+++ syndication.php
@@ -16,24 +16,72 @@ require "./global.php";
 // Load global language phrases
 $lang->load("syndication");
 
+// Load syndication class.
 require_once "inc/class_syndication.php";
 $syndication = new Syndication();
-$syndication->set_feed_type($mybb->input['type']);
-$syndication->set_limit($mybb->input['limit']);
+
+// Set the feed type and add a feed wrapper.
+$syndication->set_feed_format($mybb->input['type']);
+$syndication->start_wrapper();
+
+// Find out the thread limit.
+$thread_limit = intval($mybb->input['limit']);
 
 // Syndicate a specific forum or all viewable?
 if(isset($mybb->input['fid']))
 {
-	$flist = $mybb->input['fid'];
-	$forums = explode(',', $flist);
-	$syndication->set_forum_list($forums);
+	$forumlist = $mybb->input['fid'];
+	$forumlist = explode(',', $forumlist);
 }
 else
 {
-	$syndication->set_forum_list();
+	$forumlist = "";
 }
 
-// This is where all the good stuff happens.
-$syndication->generate_feed();
+// Get the forums the user is not allowed to see.
+$unviewable = getunviewableforums();
+
+// If there are any, add SQL to exclude them.
+if($unviewable)
+{
+	$unviewable = "AND f.fid NOT IN($unviewable)";
+}
+
+// If there are no forums to syndicate, syndicate all viewable.
+if(!empty($forumlist))
+{
+	$forum_ids = "'-1'";
+	foreach($forumlist as $fid)
+	{
+		$forum_ids .= ",'".intval($fid)."'";
+	}
+	$forumlist = "AND f.fid IN ($forum_ids) $unviewable";
+}
+else
+{
+	$forumlist = $unviewable;
+}
+
+// Get the threads to syndicate.
+$query = $db->query("
+	SELECT t.*, f.name AS forumname, p.message AS postmessage
+	FROM ".TABLE_PREFIX."threads t
+	LEFT JOIN ".TABLE_PREFIX."forums f ON (f.fid=t.fid)
+	LEFT JOIN ".TABLE_PREFIX."posts p ON (p.pid=t.firstpost)
+	WHERE 1=1
+	AND p.visible=1 ".$forumlist."
+	ORDER BY t.dateline DESC
+	LIMIT 0, ".$thread_limit
+);
+
+// Loop through all the threads.
+while($thread = $db->fetch_array($query))
+{
+	$syndication->add_post($thread);
+}
+
+// Then output the feed XML.
+$syndication->end_wrapper();
+$syndication->output_feed();
 
 ?>
