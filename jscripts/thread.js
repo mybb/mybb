af0c6a3789a1c0e3ceb33233e83fd37d73e34ee0
Chris Boulton <chris@mybb.com>
2006-03-14 03:08:15 +0000

    JS fixes.

--- jscripts/thread.js
+++ jscripts/thread.js
@@ -39,7 +39,7 @@ var Thread = {
 	
 	reportPost: function(pid)
 	{
-		popupWin("report.php?pid="+pid, "reportPost", 400, 300)
+		MyBB.popupWin("report.php?pid="+pid, "reportPost", 400, 300)
 	}
 }
 Event.observe(window, 'load', Thread.init);
