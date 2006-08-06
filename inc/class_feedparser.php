53bf4f4e65eba7b8708283f625b9f0a8a7421014
Kilroy <contact@mybb.com>
2006-08-06 10:30:22 +0000

    Accomodating for future Atom parsing, as well.

--- inc/class_feedparser.php
+++ inc/class_feedparser.php
@@ -24,14 +24,14 @@ class FeedParser
 	 * @var array
 	 */
 	var $channel = array();
-	
+
 	/**
 	 * Any error the feed parser may encounter
 	 *
 	 * @var string
 	 */
 	var $error;
-	
+
 	/**
 	 * Parses a feed with the specified filename (or URL)
 	 *
@@ -42,37 +42,37 @@ class FeedParser
 	{
 		// Include the XML parser
 		require_once MYBB_ROOT."inc/class_xml.php";
-		
+
 		// Load the feed we want to parse
 		$contents = @implode("", @file($feed));
-		
+
 		// Could not load the feed, return an error
 		if(!$contents)
 		{
 			$this->error = "invalid_file";
 			return false;
 		}
-		
+
 		// Parse the feed and get the tree
 		$parser = new XMLParser($contents);
 		$tree = $parser->get_tree();
-		
+
 		// If the feed is invalid, throw back an error
 		if($tree == false)
 		{
 			$this->error = "invalid_feed_xml";
 			return false;
 		}
-		
+
 		// Change array key names to lower case
 		$tree = $this->keys_to_lowercase($tree);
-		
+
 		// This is an RSS feed, parse it
 		if(array_key_exists("rss", $tree))
 		{
 			$this->parse_rss($tree['rss']);
 		}
-		
+
 		// We don't know how to parse this feed
 		else
 		{
@@ -80,7 +80,7 @@ class FeedParser
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Parses an XML structure in the format of an RSS feed
 	 *
@@ -95,7 +95,7 @@ class FeedParser
 			"link" => $feed_contents['channel']['link']['value'],
 			"description" => $feed_contents['channel']['description']['value'],
 			"date" => $feed_contents['channel']['pubdate']['value'],
-			"date_timestamp" => $this->get_timestamp($feed_contents['channel']['pubdate']['value'])
+			"date_timestamp" => $this->get_rss_timestamp($feed_contents['channel']['pubdate']['value'])
 		);
 
 		// The XML parser does not create a multidimensional array of items if there is one item, so fake it
@@ -103,39 +103,39 @@ class FeedParser
 		{
 			$feed_contents['channel']['item'] = array($feed_contents['channel']['item']);
 		}
-		
+
 		// Loop through each of the items in the feed
 		foreach($feed_contents['channel']['item'] as $feed_item)
 		{
 			// Here is a nice long stretch of code for parsing items, we do it this way because most elements are optional in an
 			// item and we only want to assign what we have.
-			
+
 			$item = array();
-			
-			
+
+
 			// Set the item title if we have it
 			if(array_key_exists("title", $feed_item))
 			{
 				$item['title'] = $feed_item['title']['value'];
 			}
-			
+
 			if(array_key_exists("description", $feed_item))
 			{
 				$item['description'] = $feed_item['description']['value'];
 			}
-			
+
 			if(array_key_exists("link", $feed_item))
 			{
 				$item['link'] = $feed_item['link']['value'];
 			}
-			
+
 			// If we have a pub date, store it and attempt to generate a unix timestamp from it
 			if(array_key_exists("pubdate", $feed_item))
 			{
 				$item['date'] = $feed_item['pubdate']['value'];
-				$item['date_timestamp'] = $this->get_timestamp($item['date']);
+				$item['date_timestamp'] = $this->get_rss_timestamp($item['date']);
 			}
-			
+
 			// If we have a GUID
 			if(array_key_exists("guid", $feed_item))
 			{
@@ -146,7 +146,7 @@ class FeedParser
 			{
 				$item['guid'] = md5($item['link'].$item['title']);
 			}
-			
+
 			// If we have some content, set it
 			if(array_key_exists("content:encoded", $feed_item))
 			{
@@ -156,8 +156,8 @@ class FeedParser
 			{
 				$item['content'] = $feed_item['content']['value'];
 			}
-			
-			// We have a DC based creator, set it			
+
+			// We have a DC based creator, set it
 			if(array_key_exists("dc:creator", $feed_item))
 			{
 				$item['author'] = $feed_item['dc:creator']['value'];
@@ -167,13 +167,13 @@ class FeedParser
 			{
 				$item['author'] = $fed_item['author']['value'];
 			}
-			
-			// Assign the item to our list of items				
+
+			// Assign the item to our list of items
 			$this->items[] = $item;
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Convert all array keys within an array to lowercase
 	 *
@@ -197,14 +197,14 @@ class FeedParser
 		}
 		return $new_array;
 	}
-	
+
 	/**
 	 * Converts an RSS date stamp in to a unix timestamp
 	 *
 	 * @param string The RSS date
 	 * @return integer The unix timestamp (if successful), 0 if unsuccessful
 	 */
-	function get_timestamp($date)
+	function get_rss_timestamp($date)
 	{
 		$stamp = strtotime($date);
 		if($stamp <= 0)
