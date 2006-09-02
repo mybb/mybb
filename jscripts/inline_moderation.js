17634bca48d34adf9492cd73f5b26f3f06aeaaf8
Chris Boulton <chris@mybb.com>
2006-09-02 10:19:00 +0000

    Desperately needed to add this before release so I can use it on the community forums to shuffle threads around :D

--- jscripts/inline_moderation.js
+++ jscripts/inline_moderation.js
@@ -130,6 +130,48 @@
 		goButton.value = go_text+" (0)";
 		Cookie.unset(inlineModeration.cookieName);
 		return true;
+	},
+	
+	checkAll: function(master)
+	{
+		inputs = document.getElementsByTagName("input");
+		if(!inputs)
+		{
+			return false;
+		}
+		inlineCookie = Cookie.get(inlineModeration.cookieName);
+		if(inlineCookie)
+		{
+			inlineIds = inlineCookie.split("|");
+		}
+		var newIds = new Array();
+		for(var i=0;i<inputs.length;i++)
+		{
+			var element = inputs[i];
+			inlineCheck = element.id.split("_");
+			if((element.name != "allbox") && (element.type == "checkbox") && (inlineCheck[0] == "inlinemod"))
+			{
+				id = inlineCheck[1];
+				element.checked = master.checked;
+				if(master.checked == true)
+				{
+					inlineModeration.inlineCount++;
+					newIds[newIds.length] = id;
+				}
+				else
+				{
+					inlineModeration.inlineCount--;	
+				}
+			}
+		}
+		inlineData = "|"+newIds.join("|")+"|";
+		goButton = $("inline_go");
+		if(inlineModeration.inlineCount < 0)
+		{
+			inlineModeration.inlineCount = 0;
+		}
+		goButton.value = go_text+" ("+inlineModeration.inlineCount+")";
+		Cookie.set(inlineModeration.cookieName, inlineData, 3600000);		
 	}
 }
 Event.observe(window, "load", inlineModeration.init);
