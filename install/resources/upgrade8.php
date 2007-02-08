51b552d11fdd4f1c11c0fe545531454b83587c25
Chris Boulton <chris@mybb.com>
2007-02-08 13:53:44 +0000

    Fixing upgrade8.php in branch.. Don't have access to trunk here on server so I can't take a look at it now.

--- install/resources/upgrade8.php
+++ install/resources/upgrade8.php
@@ -15,21 +15,32 @@
 
 
 $upgrade_detail = array(
-	"revert_all_templates" => 0,
-	"revert_all_themes" => 0,
-	"revert_all_settings" => 0
+        "revert_all_templates" => 0,
+        "revert_all_themes" => 0,
+        "revert_all_settings" => 0
 );
 
 @set_time_limit(0);
 
-if(!$db->field_exists('oldadditionalgroups', "banned"))
-{	
-	$db->query("ALTER TABLE ".TABLE_PREFIX."banned ADD oldadditionalgroups text NOT NULL AFTER oldgroup");
-}
-
-if(!$db->field_exists('olddisplaygroup', "banned"))
+function upgrade8_dbchanges()
 {
-	$db->query("ALTER TABLE ".TABLE_PREFIX."banned ADD olddisplaygroup int NOT NULL default '0' AFTER oldadditionalgroups");
-}
+	global $db, $output, $mybb;
 
+	$output->print_header("Performing Queries");
+
+	echo "<p>Performing necessary upgrade queries..</p>";
+
+	if(!$db->field_exists('oldadditionalgroups', TABLE_PREFIX."banned"))
+	{
+			$db->query("ALTER TABLE ".TABLE_PREFIX."banned ADD oldadditionalgroups text NOT NULL AFTER oldgroup");
+	}
+
+	if(!$db->field_exists('olddisplaygroup', TABLE_PREFIX."banned"))
+	{
+			$db->query("ALTER TABLE ".TABLE_PREFIX."banned ADD olddisplaygroup int NOT NULL default '0' AFTER oldadditionalgroups");
+	}
+	$contents .= "Click next to continue with the upgrade process.</p>";
+	$output->print_contents($contents);
+	$output->print_footer("8_done");
+}
 ?>
