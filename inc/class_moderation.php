3f31eaf496353a8fe1f4ddcaddfe224cc18761f2
CraKteR <contact@mybb.com>
2007-07-01 21:23:15 +0000

    It seems like joins in delete queries == bad on some setups. Changing to one select query and one delete query to stop people from reporting it. The MySQL documents seems to be incorrect though.

--- inc/class_moderation.php
+++ inc/class_moderation.php
@@ -1623,7 +1623,17 @@ class Moderation
 			if(count($groups) > 0)
 			{
 				$groups_csv = implode(',', $groups);
-				$db->query("DELETE s FROM (".TABLE_PREFIX."favorites s, ".TABLE_PREFIX."users u) WHERE s.type='s' AND s.tid IN ({$tids_csv}) AND s.uid=u.uid AND (u.usergroup IN ({$groups_csv}){$additional_groups})");
+				$query = $db->query("
+					SELECT s.tid, u.uid
+					FROM ".TABLE_PREFIX."favorites s
+					LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid=s.uid)
+					WHERE s.type='s' AND s.tid IN ({$tids_csv})
+					AND (u.usergroup IN ({$groups_csv}){$additional_groups})
+				");
+				while($favorites = $db->fetch_array($query))
+				{
+					$db->query("DELETE FROM ".TABLE_PREFIX."favorites WHERE uid='{$favorites['uid']}' AND tid='{$favorites['tid']}'");
+				}
 			}
 		}
 		// Delete all subscriptions of this thread
