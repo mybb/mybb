a5733355d8f3d013d790ec958a2aa8209e508416
Ryan Gordon <contact@mybb.com>
2008-10-28 01:01:03 +0000

    Bypassing MIME Spoofing protection by guessing the url to the direct attachment fix

--- inc/functions_upload.php
+++ inc/functions_upload.php
@@ -412,7 +412,7 @@ function upload_attachment($attachment)
 	}
 	
 	// All seems to be good, lets move the attachment!
-	$filename = "post_".$mybb->user['uid']."_".TIME_NOW.".attach";
+	$filename = "post_".$mybb->user['uid']."_".TIME_NOW."_".md5(uniqid(rand(), true)).".attach";
 	
 	$file = upload_file($attachment, $mybb->settings['uploadspath']."/".$month_dir, $filename);
 	
