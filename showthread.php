7a8d1085ef6f5f75932685067f8211387c6c3ffd
Tomm M <contact@mybb.com>
2010-12-28 22:11:05 +0000

    Fixes Unnecessary DB switch (fixes:1390)

--- showthread.php
+++ showthread.php
@@ -955,32 +955,16 @@ if($mybb->input['action'] == "thread")
 	// Show the similar threads table if wanted.
 	if($mybb->settings['showsimilarthreads'] != 0)
 	{
-		switch($db->type)
-		{
-			case "sqlite":
-			case "pgsql":
-			$query = $db->query("
-				SELECT t.*, t.username AS threadusername, p.displaystyle AS threadprefix, u.username, MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') AS relevance
-				FROM ".TABLE_PREFIX."threads t
-				LEFT JOIN ".TABLE_PREFIX."threadprefixes p ON (p.pid = t.prefix)
-				LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid = t.uid)
-				WHERE t.fid='{$thread['fid']}' AND t.tid!='{$thread['tid']}' AND t.visible='1' AND t.closed NOT LIKE 'moved|%' AND MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') >= '{$mybb->settings['similarityrating']}'
-				ORDER BY t.lastpost DESC
-				LIMIT 0, {$mybb->settings['similarlimit']}
-			");
-			break;
-			default:
-			$query = $db->query("
-				SELECT t.*, t.username AS threadusername, p.displaystyle AS threadprefix, u.username, MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') AS relevance
-				FROM ".TABLE_PREFIX."threads t
-				LEFT JOIN ".TABLE_PREFIX."threadprefixes p ON (p.pid = t.prefix)
-				LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid = t.uid)
-				WHERE t.fid='{$thread['fid']}' AND t.tid!='{$thread['tid']}' AND t.visible='1' AND t.closed NOT LIKE 'moved|%' AND MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') >= '{$mybb->settings['similarityrating']}'
-				ORDER BY t.lastpost DESC
-				LIMIT 0, {$mybb->settings['similarlimit']}
-			");
-		}
-		
+		$query = $db->query("
+			SELECT t.*, t.username AS threadusername, p.displaystyle AS threadprefix, u.username, MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') AS relevance
+			FROM ".TABLE_PREFIX."threads t
+			LEFT JOIN ".TABLE_PREFIX."threadprefixes p ON (p.pid = t.prefix)
+			LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid = t.uid)
+			WHERE t.fid='{$thread['fid']}' AND t.tid!='{$thread['tid']}' AND t.visible='1' AND t.closed NOT LIKE 'moved|%' AND MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') >= '{$mybb->settings['similarityrating']}'
+			ORDER BY t.lastpost DESC
+			LIMIT 0, {$mybb->settings['similarlimit']}
+		");
+
 		$count = 0;
 		$similarthreadbits = '';
 		$icon_cache = $cache->read("posticons");
