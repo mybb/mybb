f4a4c68f5e3a2f045df20f1df0140ca87d331976
Tomm M <contact@mybb.com>
2011-01-14 11:25:36 +0000

    Fixes Show 'Similar Threads' Table option in current form will not work in PostgreSQL (fixes:1175)

--- showthread.php
+++ showthread.php
@@ -960,15 +960,30 @@ if($mybb->input['action'] == "thread")
 	// Show the similar threads table if wanted.
 	if($mybb->settings['showsimilarthreads'] != 0)
 	{
-		$query = $db->query("
-			SELECT t.*, t.username AS threadusername, p.displaystyle AS threadprefix, u.username, MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') AS relevance
-			FROM ".TABLE_PREFIX."threads t
-			LEFT JOIN ".TABLE_PREFIX."threadprefixes p ON (p.pid = t.prefix)
-			LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid = t.uid)
-			WHERE t.fid='{$thread['fid']}' AND t.tid!='{$thread['tid']}' AND t.visible='1' AND t.closed NOT LIKE 'moved|%' AND MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') >= '{$mybb->settings['similarityrating']}'
-			ORDER BY t.lastpost DESC
-			LIMIT 0, {$mybb->settings['similarlimit']}
-		");
+		switch($db->type)
+		{
+			case "pgsql":
+				$query = $db->query("
+					SELECT t.*, t.username AS threadusername, p.displaystyle AS threadprefix, u.username
+					FROM ".TABLE_PREFIX."threads t
+					LEFT JOIN ".TABLE_PREFIX."threadprefixes p ON (p.pid = t.prefix)
+					LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid = t.uid), plainto_tsquery ('".$db->escape_string($thread['subject'])."') AS query
+					WHERE t.fid='{$thread['fid']}' AND t.tid!='{$thread['tid']}' AND t.visible='1' AND t.closed NOT LIKE 'moved|%' AND t.subject @@ query AND ts_rank_cd(to_tsvector('english',t.subject), query ) >= '{$mybb->settings['similarityrating']}'
+					ORDER BY t.lastpost DESC
+					OFFSET 0 LIMIT {$mybb->settings['similarlimit']}
+				");
+				break;
+			default:
+				$query = $db->query("
+					SELECT t.*, t.username AS threadusername, p.displaystyle AS threadprefix, u.username, MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') AS relevance
+					FROM ".TABLE_PREFIX."threads t
+					LEFT JOIN ".TABLE_PREFIX."threadprefixes p ON (p.pid = t.prefix)
+					LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid = t.uid)
+					WHERE t.fid='{$thread['fid']}' AND t.tid!='{$thread['tid']}' AND t.visible='1' AND t.closed NOT LIKE 'moved|%' AND MATCH (t.subject) AGAINST ('".$db->escape_string($thread['subject'])."') >= '{$mybb->settings['similarityrating']}'
+					ORDER BY t.lastpost DESC
+					LIMIT 0, {$mybb->settings['similarlimit']}
+				");
+		}
 
 		$count = 0;
 		$similarthreadbits = '';
