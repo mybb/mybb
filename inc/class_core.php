e6c66cf28eac4f57de4a65e6c9a468f74891a127
Tomm M <contact@mybb.com>
2011-03-17 15:23:11 +0000

    Updating the Core version number to 1.6.2

--- inc/class_core.php
+++ inc/class_core.php
@@ -15,14 +15,14 @@ class MyBB {
 	 *
 	 * @var string
 	 */
-	public $version = "1.6.1";
+	public $version = "1.6.2";
 	
 	/**
 	 * The version code of MyBB we're running.
 	 *
 	 * @var integer
 	 */
-	public $version_code = 1601;
+	public $version_code = 1602;
 	
 	/**
 	 * The current working directory.
