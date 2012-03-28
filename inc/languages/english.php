cfd3655edaca2d83b82d3a779085ede1962f8e84
Tomm M <contact@mybb.com>
2012-03-28 11:08:55 +0000

    Preparing for 1.6.7

--- inc/languages/english.php
+++ inc/languages/english.php
@@ -16,7 +16,7 @@ $langinfo['author'] = "MyBulletinBoard";
 $langinfo['website'] = "http://mybb.com/";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1605";
+$langinfo['version'] = "1607";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
