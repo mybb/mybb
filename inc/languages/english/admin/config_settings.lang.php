95d145afd3e540bf4339a927c1c557569069ed8a
Paul H. <pjbear94@yahoo.com>
2012-06-30 22:23:26 -0500

    Added language message for hidden captcha conflict error

--- inc/languages/english/admin/config_settings.lang.php
+++ inc/languages/english/admin/config_settings.lang.php
@@ -79,6 +79,7 @@ $l['error_cannot_edit_php'] = "This is a special type of setting which cannot be
 $l['error_ajax_search'] = "There was a problem searching for settings:";
 $l['error_ajax_unknown'] = "An unknown error occurred while searching for settings.";
 $l['error_chmod_settings_file'] = "The settings file \"./inc/settings.php\" isn't writable. Please CHMOD to 777.<br />For more information on CHMODing, see the <a href=\"http://wiki.mybb.com/index.php/HowTo_Chmod\" target=\"_blank\">MyBB Wiki</a>.";
+$l['error_hidden_captcha_conflict'] = "The hidden captcha value cannot be \"{1}\" as it conflicts with another registration field.";
 
 $l['success_setting_added'] = "The setting has been created successfully.";
 $l['success_setting_updated'] = "The setting has been updated successfully.";
