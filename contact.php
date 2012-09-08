c8122cf22f8b4bdf862a1257d42e8db42386f38d
Diogo Parrinha <piratanervo@gmail.com>
2012-09-08 22:04:10 +0100

    WIP #16 Contact Form - perhaps we should add a CAPTCHA?

---
+++ contact.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * MyBB 1.6
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ *
+ * Website: http://mybb.com
+ * License: http://mybb.com/about/license
+ *
+ * $Id: contact.php 5297 2010-12-28 22:01:14Z Tomm $
+ */
+
+define("IN_MYBB", 1);
+define('THIS_SCRIPT', 'contact.php');
+
+$templatelist = "contact";
+require_once "./global.php";
+
+// Load global language phrases
+$lang->load("contact");
+
+if(!$mybb->user['uid'])
+{
+	error_no_permission();
+}
+
+$errors = '';
+
+if($mybb->request_method == "post" && isset($mybb->input['submit']))
+{
+	// Verify incoming POST request
+	verify_post_check($mybb->input['my_post_key']);
+	
+	// Validate input
+	if(!isset($mybb->input['message']) || trim_blank_chrs($mybb->input['message']) == '')
+	{
+		$errors[] = $lang->contact_no_message;
+	}
+	
+	if(!isset($mybb->input['subject']) || trim_blank_chrs($mybb->input['subject']) == '')
+	{
+		$errors[] = $lang->contact_no_subject;
+	}
+	
+	if(!isset($mybb->input['email']) || trim_blank_chrs($mybb->input['email']) == '')
+	{
+		$errors[] = $lang->contact_no_email;
+	}
+	else
+	{
+		// Validate email
+		if(!validate_email_format($mybb->input['email']))
+		{
+			$errors[] = $lang->contact_no_email;
+		}
+	}
+	
+	// Should we have a CAPTCHA? Perhaps yes...
+	
+	if(empty($errors))
+	{
+		// Email the administrator
+		my_mail($mybb->settings['adminemail'], trim_blank_chrs($mybb->input['subject']), trim_blank_chrs($mybb->input['message']), trim_blank_chrs($mybb->input['email']));
+		
+		// Redirect
+		redirect('contact.php', $lang->contact_success_message);
+	}
+	else
+	{
+		$errors = inline_error($errors);
+	}
+}
+
+// Generate CAPTCHA?
+
+// Contact page
+eval("\$page = \"".$templates->get("contact")."\";");
+output_page($page);
+exit;
+
+?>
---
+++ inc/languages/english/contact.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: contact.lang.php 5297 2010-12-28 22:01:14Z Tomm $
+ */
+
+$l['contact'] = 'Contact Us';
+$l['contact_no_message'] = 'You have not provided a message to send.';
+$l['contact_no_subject'] = 'You are required to enter a subject.';
+$l['contact_no_email'] = 'You must enter a valid email address.';
+$l['contact_success_message'] = 'Your message has been successfully sent to the Administrator.';
+$l['contact_subject'] = 'Subject';
+$l['contact_message'] = 'Message';
+$l['contact_email'] = 'Email';
+$l['contact_email_desc'] = 'Enter your email so we can contact you back.';
+$l['contact_send'] = 'Send';
+?>
