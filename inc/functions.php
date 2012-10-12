888ee2adc27ffa20182b77081489bcd06cc55663
Tom Moore <tomm@mybb.com>
2012-10-12 10:46:39 +0100

    Fixes #1489, #2028 SAPI_NAME constant

--- inc/functions.php
+++ inc/functions.php
@@ -3462,13 +3462,14 @@ function build_archive_link($type, $id="")
 	global $mybb;
 	
 	// If the server OS is not Windows and not Apache or the PHP is running as a CGI or we have defined ARCHIVE_QUERY_STRINGS, use query strings - DIRECTORY_SEPARATOR checks if running windows
-	if((DIRECTORY_SEPARATOR == '\\' && stripos($_SERVER['SERVER_SOFTWARE'], 'apache') == false) || strstr(php_sapi_name(), 'cgi') || defined("ARCHIVE_QUERY_STRINGS"))
+	//if((DIRECTORY_SEPARATOR == '\\' && is_numeric(stripos($_SERVER['SERVER_SOFTWARE'], "apache")) == false) || is_numeric(stripos(SAPI_NAME, "cgi")) !== false || defined("ARCHIVE_QUERY_STRINGS"))
+	if($mybb->settings['seourls_archive'] == 1)
 	{
-		$base_url = $mybb->settings['bburl']."/archive/index.php?";
+		$base_url = $mybb->settings['bburl']."/archive/index.php/";
 	}
 	else
 	{
-		$base_url = $mybb->settings['bburl']."/archive/index.php/";
+		$base_url = $mybb->settings['bburl']."/archive/index.php?";
 	}
 
 	switch($type)
