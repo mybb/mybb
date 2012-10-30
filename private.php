cc3de1f490375d3cb064d94e0fe86e71e0481386
Tom Moore <tomm@mybb.com>
2012-10-30 09:49:09 +0000

    Fixes #2097 Word censor in replying to PMs

--- private.php
+++ private.php
@@ -691,7 +691,7 @@ if($mybb->input['action'] == "send")
 		$post['userusername'] = $mybb->user['username'];
 		$post['postusername'] = $mybb->user['username'];
 		$post['message'] = $mybb->input['message'];
-		$post['subject'] = $subject;
+		$post['subject'] = htmlspecialchars_uni($mybb->input['subject']);
 		$post['icon'] = $mybb->input['icon'];
 		$post['smilieoff'] = $options['disablesmilies'];
 		$post['dateline'] = TIME_NOW;
