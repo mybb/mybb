94acc2eb053e30af6ee9894a021211c99486980b
Diogo Parrinha <piratanervo@gmail.com>
2012-11-27 12:11:51 +0000

    WIP #16 An actual warnings class, rather than a crap load of functions

---
+++ inc/datahandlers/warnings.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * MyBB 1.6
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ *
+ * Website: http://mybb.com
+ * License: http://mybb.com/about/license
+ *
+ * $Id: $
+ */
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+/**
+ * Login handling class, provides common structure to handle login events.
+ *
+ */
+class WarningsHandler extends DataHandler
+{
+	/**
+	 * The language file used in the data handler.
+	 *
+	 * @var string
+	 */
+	public $language_file = 'datahandler_warnings';
+
+	/**
+	 * The prefix for the language variables used in the data handler.
+	 *
+	 * @var string
+	 */
+	public $language_prefix = 'warnings';
+	
+	private $write_warning_data;
+	private $read_warning_data;
+	
+	public function validate_maximum()
+	{
+		global $mybb, $db, $lang;
+		
+		if($mybb->usergroup['maxwarningsday'] != 0)
+		{
+			$timecut = TIME_NOW-60*60*24;
+			$query = $db->simple_select("warnings", "COUNT(wid) AS given_today", "issuedby='{$mybb->user['uid']}' AND dateline>'$timecut'");
+			$given_today = $db->fetch_field($query, "given_today");
+			if($given_today >= $mybb->usergroup['maxwarningsday'])
+			{
+				$this->set_error('reached_max_warnings_day', array($mybb->usergroup['maxwarningsday']));
+				return false;
+			}
+		}
+	}
+	
+	public function get($wid)
+	{
+		$wid = (int)$wid;
+		if($wid <= 0)
+		{
+			return false;
+		}
+		
+		$query = $db->simple_select("warnings", "*", "wid='".$wid."'");
+		$this->read_warning_data = $db->fetch_array($query);
+		
+		return $this->read_warning_data;
+	}
+	
+	public update_warning()
+	{
+		global $db, $mybb, $plugins;
+		
+		$warning = &$this->data;
+		
+		$warning['wid'] = (int)$warning['wid'];
+		if($warning['wid'] <= 0)
+		{
+			return false;
+		}
+		
+		$this->write_warning_data = array(
+			"expired" => 1,
+			"daterevoked" => TIME_NOW,
+			"revokedby" => $mybb->user['uid'],
+			"revokereason" => $db->escape_string($warning['reason'])
+		);
+		
+		$db->update_query("warnings", $this->write_warning_data, "wid='{$warning['wid']}'");
+		
+		return $this->write_warning_data;
+	}
+
+	public insert_warning()
+	{
+		global $db, $mybb, $plugins;
+		
+		$warning = &$this->data;
+		
+		$this->write_warning_data = array(
+			"uid" => $warning['uid'],
+			"tid" => intval($warning['tid']),
+			"pid" => intval($warning['pid']),
+			"title" => $db->escape_string($warning['title']),
+			"points" => intval($warning['points']),
+			"dateline" => TIME_NOW,
+			"issuedby" => $mybb->user['uid'],
+			"expires" => $warning['expires'],
+			"expired" => 0,
+			"revokereason" => '',
+			"notes" => $db->escape_string($warning['notes'])
+		);
+		
+		$this->write_warning_data['wid'] = $db->insert_query("warnings", $this->write_warning_data);
+		
+		return $this->write_warning_data;
+	}
+}
+?>
---
+++ inc/languages/english/datahandler_warnings.lang.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['warnings_reached_max_warnings_day'] = 'You cannot warn anyone as you have reached your limit on the number of warnings you can give out per day.<br /><br />The maximum number of warnings you can give out per day is {1}."';
+?>
