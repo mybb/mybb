2897421a338dbd3064723482b74b44dbd6c263c6
Stefan <mybbcms@yahoo.de>
2012-12-12 11:38:12 +0100

    Upgrade script for template changes

---
+++ install/resources/upgrade25.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * MyBB 1.6
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybboard.com
+ * License: http://mybb.com/about/license
+ *
+ * $Id$
+ */
+
+/**
+ * Upgrade Script: 1.6.8
+ */
+
+$upgrade_detail = array(
+	"revert_all_templates" => 0,
+	"revert_all_themes" => 0,
+	"revert_all_settings" => 0
+);
+
+@set_time_limit(0);
+
+/* Nothing to do for 1.6.8 */
+?>
