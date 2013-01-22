1635c23b9b273d7cd4155e68568b9f29b7164533
Euan <euantorano@gmail.com>
2013-01-22 18:29:33 +0000

    New update task
    A task file to fetch latest news from the MyBB blog and store it in the
    cache. Todo: Fetch version info as well, add task on install/upgrade and
    display news in ACP Dashboard.

---
+++ inc/tasks/newsupdate.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * MyBB 1.6
+ * Copyright 2013 MyBB Group, All Rights Reserved
+ *
+ * Website: http://mybb.com
+ * License: http://mybb.com/about/license
+ */
+
+if (!defined('IN_MYBB')) {
+	die('Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.');
+}
+
+require_once MYBB_ROOT.'/inc/functions.php';
+
+function task_newsupdate($task)
+{
+	global $cache;
+
+	if (!extension_loaded('SimpleXML')) {
+		add_task_log($task, 'The SimpleXML extension is not installed. Please contact your host.');
+	}
+
+	$fetchedNews = fetch_remote_file('http://blog.mybb.com/feed/');
+	$feed        = new SimpleXMLElement($fetchedNews);
+
+	$latestNews = array();
+	foreach ($feed->channel->item as $newsItem) {
+		$latestNews[] = array(
+			'title'     => (string) $newsItem->title,
+			'link'      => (string) $newsItem->link,
+			'published' => (int) strtotime($newsItem->pubDate),
+		);
+	}
+
+	$cache->update('latest_news', $latestNews);
+
+	add_task_log($task, 'Latest news updated.');
+}
