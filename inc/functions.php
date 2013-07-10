a942c039ed94669719b3b0a9feff1dc0120430d0
Josh Harmon <harmon.josh99@gmail.com>
2013-07-09 22:20:31 -0700

    Improve pull #94 MySQLi->MySQL

--- inc/functions.php
+++ inc/functions.php
@@ -62,6 +62,13 @@ function output_page($contents)
 			{
 				$memory_usage = '';
 			}
+			// MySQLi is still MySQL, so present it that way to the user
+			$database_server = $db->short_title;
+			
+			if($database_server == 'MySQLi')
+			{
+				$database_server = 'MySQL';
+			}
 
 			$debugstuff = "Generated in $totaltime ($percentphp% PHP / $percentsql% MySQL)<br />SQL Queries: $db->query_count /  Server Load: $serverload$memory_usage<br />[<a href=\"$debuglink\" target=\"_blank\">advanced details</a>]<br />";
 			$contents = str_replace("<debugstuff>", $debugstuff, $contents);
