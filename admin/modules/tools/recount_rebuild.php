b1d3ca6a2c24412393e76423a01b216b4344a54c
Nathan Malcolm <n.k.l.malcolm@gmail.com>
2013-07-12 12:53:44 +0100

    Update inline JS in recount_rebuild.php

--- admin/modules/tools/recount_rebuild.php
+++ admin/modules/tools/recount_rebuild.php
@@ -220,7 +220,7 @@ function check_proceed($current, $finish, $next_page, $per_page, $name, $name2, 
 		echo "<div class=\"confirm_action\">\n";
 		echo "<p>{$lang->confirm_proceed_rebuild}</p>\n";
 		echo "<br />\n";
-		echo "<script type=\"text/javascript\">window.onload = function() { var button = $$('#proceed_button'); if(button[0]) { button[0].value = '{$lang->automatically_redirecting}'; button[0].disabled = true; button[0].style.color = '#aaa'; button[0].style.borderColor = '#aaa'; document.forms[0].submit(); }}</script>";
+		echo "<script type=\"text/javascript\">$(function() { var button = $(\"#proceed_button\"); if(button.length > 0) { button.val(\"{$lang->automatically_redirecting}\"); button.attr(\"disabled\", true); button.css(\"color\", \"#aaa\"); button.css(\"borderColor\", \"#aaa\"); document.forms[0].submit(); }})</script>";
 		echo "<p class=\"buttons\">\n";
 		echo $form->generate_submit_button($lang->proceed, array('class' => 'button_yes', 'id' => 'proceed_button'));
 		echo "</p>\n";
