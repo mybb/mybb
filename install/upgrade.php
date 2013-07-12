f300fdaea8974f250e3d10db19bf2e7591f4543f
Stefan-ST <mybbcms@yahoo.de>
2013-07-12 15:31:28 +0200

    Centralizing error handling

--- install/upgrade.php
+++ install/upgrade.php
@@ -9,21 +9,6 @@
  * $Id$
  */
 
-if(function_exists("unicode_decode"))
-{
-    // Unicode extension introduced in 6.0
-    error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE ^ E_STRICT);
-}
-elseif(defined("E_DEPRECATED"))
-{
-    // E_DEPRECATED introduced in 5.3
-    error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
-}
-else
-{
-    error_reporting(E_ALL & ~E_NOTICE);
-}
-
 define('MYBB_ROOT', dirname(dirname(__FILE__))."/");
 define("INSTALL_ROOT", dirname(__FILE__)."/");
 define("TIME_NOW", time());
@@ -34,6 +19,9 @@ if(function_exists('date_default_timezone_set') && !ini_get('date.timezone'))
 {
 	date_default_timezone_set('GMT');
 }
+
+require_once MYBB_ROOT.'inc/class_error.php';
+$error_handler = new errorHandler();
 
 require_once MYBB_ROOT."inc/class_core.php";
 $mybb = new MyBB;
