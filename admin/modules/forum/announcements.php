a65821ba4623f22f4a774f39c5b837f78dbfcfa5
Paul Bender <paulbender@live.com>
2013-07-15 22:48:35 -0400

    Fixes #2250 Admin Log errors

--- admin/modules/forum/announcements.php
+++ admin/modules/forum/announcements.php
@@ -734,7 +734,7 @@ if($mybb->input['action'] == "delete")
 		$plugins->run_hooks("admin_forum_announcements_delete_commit");
 		
 		// Log admin action
-		log_admin_action($announcement['aid'], $announcement['title']);
+		log_admin_action($announcement['aid'], $announcement['subject']);
 		$cache->update_forumsdisplay();
 
 		flash_message($lang->success_announcement_deleted, 'success');
