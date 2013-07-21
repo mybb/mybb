ece38591b5e0bd8fb2609e8c923f67ddd5090c0c
Mike Creuzer <audentio@gmail.com>
2013-07-21 13:35:23 -0500

    As per Nathan Malcom's request, ACP tabs are linkable now.

--- admin/jscripts/tabs.js
+++ admin/jscripts/tabs.js
@@ -1,5 +1,5 @@
 $(function () {
-	var tabContainers = $("[id^=tab_]");
+	/*var tabContainers = $("[id^=tab_]");
 
     $("ul#tabs a").click(function () {
         tabContainers.hide().filter(this.hash).show();
@@ -8,5 +8,37 @@ $(function () {
         $(this).addClass("active");
 
         return false;
-    }).filter(":first").click();
+    }).filter(":first").click();*/
+	
+	
+	
+	$('ul.tabs').each(function(){
+		var $activeTab, $activeContent, $links = $(this).find('a');
+
+		$activeTab = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+		$activeTab.addClass('active');
+		$activeContent = $($activeTab.attr('href'));
+
+		// Hide the remaining content
+		$links.not($activeTab).each(function () {
+			$($(this).attr('href')).hide();
+		});
+
+		// Tab functionality
+		$(this).on('click', 'a', function(e){
+
+			$activeTab.removeClass('active');
+			$activeContent.hide();
+
+			$activeTab = $(this);
+			$activeContent = $($(this).attr('href'));
+
+			$activeTab.addClass('active');
+			$activeContent.show();
+
+			e.preventDefault();
+		});
+	});
+	
+	
 });
