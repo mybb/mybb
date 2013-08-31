d428f59f932df281c7b4450d04de494431847161
Stefan-ST <mybbcms@yahoo.de>
2013-08-31 14:46:54 +0200

    Fixes #1483 Large attachments, greater than PHP memory limit, fail

--- attachment.php
+++ attachment.php
@@ -107,7 +107,12 @@ if($mybb->input['thumbnail'])
 	header("Content-type: ".$type);
 	$thumb = $mybb->settings['uploadspath']."/".$attachment['thumbnail'];
 	header("Content-length: ".@filesize($thumb));
-	echo file_get_contents($thumb);
+	$handle = fopen($thumb, 'rb');
+	while(!feof($handle))
+	{
+		echo fread($handle, 8192);
+	}
+	fclose($handle);
 }
 else
 {
@@ -153,7 +158,12 @@ else
 	}
 	
 	header("Content-length: {$attachment['filesize']}");
-	header("Content-range: bytes=0-".($attachment['filesize']-1)."/".$attachment['filesize']); 
-	echo file_get_contents($mybb->settings['uploadspath']."/".$attachment['attachname']);
+	header("Content-range: bytes=0-".($attachment['filesize']-1)."/".$attachment['filesize']);
+	$handle = fopen($mybb->settings['uploadspath']."/".$attachment['attachname'], 'rb');
+	while(!feof($handle))
+	{
+		echo fread($handle, 8192);
+	}
+	fclose($handle);
 }
 ?>
