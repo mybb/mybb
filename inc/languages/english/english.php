7ab53d89c9397d433d3210d875f63ae5cddc24db
faviouz <fabiu.maia@gmail.com>
2013-09-28 13:32:53 +0100

    Move english.php inside english/

---
+++ inc/languages/english/english.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2013 MyBB Group, All Rights Reserved
+ *
+ * $Id$
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "English (American)";
+
+// The author of the language
+$langinfo['author'] = "MyBB Group";
+
+// The language authors website
+$langinfo['website'] = "http://www.mybb.com/";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1700";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "en";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
+?>
