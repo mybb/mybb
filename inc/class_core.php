9ac03cc9053b79e04ea6e391c34b7e98de7ceb8e
Stefan-ST <mybbcms@yahoo.de>
2013-12-13 20:23:59 +0100

    Preparing MyBB 1.6.12

--- inc/class_core.php
+++ inc/class_core.php
@@ -15,14 +15,14 @@ class MyBB {
 	 *
 	 * @var string
 	 */
-	public $version = "1.6.11";
+	public $version = "1.6.12";
 	
 	/**
 	 * The version code of MyBB we're running.
 	 *
 	 * @var integer
 	 */
-	public $version_code = 1611;
+	public $version_code = 1612;
 	
 	/**
 	 * The current working directory.
--- inc/languages/english.php
+++ inc/languages/english.php
@@ -16,7 +16,7 @@ $langinfo['author'] = "MyBulletinBoard";
 $langinfo['website'] = "http://mybb.com/";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1611";
+$langinfo['version'] = "1612";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
