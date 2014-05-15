6131ab649df653f86c5dd6f66e5df8e7a09c75fc
Stefan-ST <mybbcms@yahoo.de>
2014-05-15 22:22:07 +0200

    Merge pull request #392 from ATofighi/fix-forumlink
    Forums with threads cannot have forum link
    Removes unnecessary space

--- admin/modules/forum/management.php
+++ admin/modules/forum/management.php
@@ -1294,6 +1294,15 @@ if($mybb->input['action'] == "edit")
 			}
 		}
 
+		if(!empty($mybb->input['linkto']) && empty($forum_data['linkto']))
+		{
+			$query = $db->simple_select('threads', 'COUNT(tid) as num_threads', "fid = '{$fid}'", array("limit" => 1));
+			if($db->fetch_field($query, "num_threads") > 0)
+			{
+				$errors[] = $lang->error_forum_link_not_empty;
+			}
+		}
+
 		if(!$errors)
 		{
 			if($pid < 0)
