d951191a7d7ae0a6e2d045225868946b2c6847fb
PirataNervo <piratanervo@gmail.com>
2014-05-28 01:37:22 +0100

    Working on #251 jQuery Conversion
    Converted inline settings search to jQuery

---
+++ admin/jscripts/search.js
@@ -0,0 +1,86 @@
+var SettingSearch = {
+
+	error_unknown : "",
+	
+	init: function(settings_search, error_unknown)
+	{
+		this.error_unknown = error_unknown;
+		
+		$('#settings_search').bind("submit", this.onSubmit);
+		$('#search_results').css('display', 'none');
+		
+		$('#search').focusin(function() {
+			if($('#search').val() == settings_search)
+			{
+				$('#search').removeClass('search_default');
+				$('#search').val('');
+			}
+		});
+		
+		$('#search').focusout(function() {
+			if($('#search').val() == "")
+			{
+				$('#search').addClass('search_default');
+				$('#search').val(settings_search);
+				$("#search_results").css('display', "none");
+				$("#group_list").css('display', "");
+			}
+		});
+	},
+
+	onSubmit: function(e)
+	{
+		e.preventDefault();
+		if($('#search').val() != "")
+		{
+			$.jGrowl('Searching...');
+			pars = "module=config-settings&action=change&ajax_search=1&search="+encodeURIComponent($('#search').val());
+			$.ajax({
+				type: 'get',
+				url: "index.php",
+				data: pars,
+				complete: function (request, status)
+				{
+					try
+					{
+						var json = $.parseJSON(request.responseText);
+						if(typeof json == 'object')
+						{
+							if(json.hasOwnProperty("errors"))
+							{
+								$("div.jGrowl").jGrowl("close");
+
+								$.each(json.errors, function(i, message)
+								{
+									$.jGrowl('There was an error posting your reply: '+message);
+								});
+								return false;
+							}
+						}
+					}
+					catch(error)
+					{
+						$('#search_results').css('display', '');
+						$('#group_list').css('display', 'none');
+						$('#search_results').html(request.responseText);
+						loadPeekers();
+						$.jGrowl('Done!');
+						return false;
+					}
+		
+					/*if(request.responseText.match(/<error>(.*)<\/error>/) || request.responseText == "")
+					{
+						message = request.responseText.match(/<error>(.*)<\/error>/);
+						if(!message[1])
+						{
+							message[1] = this.error_unknown;
+						}
+						alert(message[1]);
+						$.jGrowl('The following error has occurred: '+message[1]);
+					}*/
+						
+				}
+			});
+		}
+	},
+};
