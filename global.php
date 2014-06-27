cdcc7a553ceb49b5b57732a003cac984d278d3f5
PaulBender <paulbender@live.com>
2014-06-27 19:17:42 -0400

    Working on #454 Hide stuff users don't have permission to use Hide "Contact Us" link if diabled to guests or disabled altogether.

--- global.php
+++ global.php
@@ -411,7 +411,7 @@ else
 	$templatelist = '';
 }
 
-$templatelist .= "headerinclude,header,footer,gobutton,htmldoctype,header_welcomeblock_member,header_welcomeblock_guest,header_welcomeblock_member_admin,global_pm_alert,global_unreadreports,error,footer_languageselect_option";
+$templatelist .= "headerinclude,header,footer,gobutton,htmldoctype,header_welcomeblock_member,header_welcomeblock_guest,header_welcomeblock_member_admin,global_pm_alert,global_unreadreports,error,footer_languageselect_option,footer_contactus";
 $templatelist .= ",global_pending_joinrequests,nav,nav_sep,nav_bit,nav_sep_active,nav_bit_active,footer_languageselect,footer_themeselect,header_welcomeblock_member_moderator,redirect,header_menu_calendar,nav_dropdown,footer_themeselector";
 $templatelist .= ",global_boardclosed_warning,global_bannedwarning,error_inline,error_nopermission_loggedin,error_nopermission,debug_summary,header_quicksearch,header_menu_search,header_menu_memberlist,usercp_themeselector_option";
 $templates->cache($db->escape_string($templatelist));
@@ -741,6 +741,13 @@ if($mybb->settings['showthemeselect'] != 0)
 		$theme_redirect_url = get_current_location(true, 'theme');
 		eval('$theme_select = "'.$templates->get('footer_themeselect').'";');
 	}
+}
+
+// If we use the contact form, show 'Contact Us' link when appropriate
+$contact_us = '';
+if(($mybb->settings['contactlink'] == "contact.php" && $mybb->settings['contact'] == 1 && ($mybb->settings['contact_guests'] != 1 && $mybb->user['uid'] == 0 || $mybb->user['uid'] > 0)) || $mybb->settings['contactlink'] != "contact.php")
+{
+	eval('$contact_us = "'.$templates->get('footer_contactus').'";');
 }
 
 // DST Auto detection enabled?
