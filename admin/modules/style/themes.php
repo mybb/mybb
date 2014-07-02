mybb/mybb

commit f9dfafc022040c9108ebb1f2c89bfb27c3c5fbdb

Fixes #815 Warning: Invalid argument supplied for foreach() in admin/modules/style/themes.php when there are no stylesheets.

admin/modules/style/themes.php
@@ -509,7 +509,7 @@ if($mybb->input['action'] == "export")
 
 	if($mybb->request_method == "post")
 	{
-		$properties = unserialize($theme['properties']);
+		$properties = my_unserialize($theme['properties']);
 
 		$xml = "<?xml version=\"1.0\" encoding=\"{$lang->settings['charset']}\"?".">\r\n";
 		$xml .= "<theme name=\"".htmlspecialchars_uni($theme['name'])."\" version=\"".$mybb->version_code."\">\r\n";
@@ -530,7 +530,7 @@ if($mybb->input['action'] == "export")
 		$xml .= "\t</properties>\r\n";
 
 		// Fetch list of all of the stylesheets for this theme
-		$file_stylesheets = unserialize($theme['stylesheets']);
+		$file_stylesheets = my_unserialize($theme['stylesheets']);
 
 		$stylesheets = array();
 		$inherited_load = array();
@@ -538,7 +538,7 @@ if($mybb->input['action'] == "export")
 		// Now we loop through the list of stylesheets for each file
 		foreach($file_stylesheets as $file => $action_stylesheet)
 		{
-			if($file == 'inherited')
+			if($file == 'inherited' !is_array($action_stylesheet))
 			{
 				continue;
 			}
@@ -938,7 +938,7 @@ if($mybb->input['action'] == "delete")
 		$query = $db->simple_select("themes", "tid,stylesheets", "tid != '{$theme['tid']}'", array('order_by' => "pid, name"));
 		while($theme2 = $db->fetch_array($query))
 		{
-			$theme2['stylesheets'] = unserialize($theme2['stylesheets']);
+			$theme2['stylesheets'] = my_unserialize($theme2['stylesheets']);
 
 			if(!$theme2['stylesheets']['inherited'])
 			{
@@ -1056,7 +1056,7 @@ if($mybb->input['action'] == "edit")
 			$errors[] = $lang->error_invalid_templateset;
 		}
 
-		$theme_properties = unserialize($theme['properties']);
+		$theme_properties = my_unserialize($theme['properties']);
 		if($theme_properties['disporder'])
 		{
 			$properties['disporder'] = $theme_properties['disporder'];
@@ -1141,7 +1141,7 @@ if($mybb->input['action'] == "edit")
 	}
 
 	// Fetch list of all of the stylesheets for this theme
-	$file_stylesheets = unserialize($theme['stylesheets']);
+	$file_stylesheets = my_unserialize($theme['stylesheets']);
 
 	$stylesheets = array();
 	$inherited_load = array();
@@ -1149,7 +1149,7 @@ if($mybb->input['action'] == "edit")
 	// Now we loop through the list of stylesheets for each file
 	foreach($file_stylesheets as $file => $action_stylesheet)
 	{
-		if($file == 'inherited')
+		if($file == 'inherited' !is_array($action_stylesheet))
 		{
 			continue;
 		}
@@ -1224,7 +1224,7 @@ if($mybb->input['action'] == "edit")
 		asort($orders, SORT_NUMERIC);
 
 		// Save the orders in the theme properties
-		$properties = unserialize($theme['properties']);
+		$properties = my_unserialize($theme['properties']);
 		$properties['disporder'] = $orders;
 
 		$update_array = array(
@@ -1267,7 +1267,7 @@ if($mybb->input['action'] == "edit")
 		'description' => $lang->duplicate_theme_desc
 	);
 
-	$properties = unserialize($theme['properties']);
+	$properties = my_unserialize($theme['properties']);
 	$page->output_nav_tabs($sub_tabs, 'edit_stylesheets');
 
 	$table = new Table;
@@ -1717,7 +1717,7 @@ if($mybb->input['action'] == "stylesheet_properties")
 		}
 	}
 
-	$properties = unserialize($theme['properties']);
+	$properties = my_unserialize($theme['properties']);
 	$page->add_breadcrumb_item(htmlspecialchars_uni($theme['name']), "index.php?module=style-themes&amp;action=edit&amp;tid={$mybb->input['tid']}");
 	$page->add_breadcrumb_item(htmlspecialchars_uni($stylesheet['name'])." {$lang->properties}", "index.php?module=style-themes&amp;action=edit_properties&amp;tid={$mybb->input['tid']}");
 
@@ -1997,7 +1997,7 @@ if($mybb->input['action'] == "edit_stylesheet" && (!isset($mybb->input['mode']) 
 
 		if($mybb->input['serialized'] == 1)
 		{
-			$mybb->input['css_bits'] = unserialize($mybb->input['css_bits']);
+			$mybb->input['css_bits'] = my_unserialize($mybb->input['css_bits']);
 		}
 
 		$css_to_insert = '';
@@ -2580,7 +2580,7 @@ if($mybb->input['action'] == "add_stylesheet")
 
 	$page->add_breadcrumb_item(htmlspecialchars_uni($theme['name']), "index.php?module=style-themes&amp;action=edit&amp;tid={$mybb->input['tid']}");
 	$page->add_breadcrumb_item($lang->add_stylesheet);
-	$properties = unserialize($theme['properties']);
+	$properties = my_unserialize($theme['properties']);
 
 	$page->output_header("{$lang->themes} - {$lang->add_stylesheet}");
 

admin/inc/functions.php
@@ -432,7 +432,7 @@ function get_admin_permissions($get_uid="", $get_gid="")
 			"limit" => "1"
 		);
 		$query = $db->simple_select("adminoptions", "permissions", "(uid='-{$get_gid}' OR uid='0') AND permissions != ''", $options);
-		return unserialize($db->fetch_field($query, "permissions"));
+		return my_unserialize($db->fetch_field($query, "permissions"));
 	}
 	else
 	{
@@ -454,7 +454,7 @@ function get_admin_permissions($get_uid="", $get_gid="")
 		$query = $db->simple_select("adminoptions", "permissions, uid", "(uid='{$uid}'{$group_sql}) AND permissions != ''", $options);
 		while($perm = $db->fetch_array($query))
 		{
-			$perm['permissions'] = unserialize($perm['permissions']);
+			$perm['permissions'] = my_unserialize($perm['permissions']);
 
 			// Sorting out which permission is which
 			if($perm['uid'] > 0)
@@ -541,7 +541,7 @@ function change_admin_permission($tab, $page="", $default=1)
 	$query = $db->simple_select("adminoptions", "uid, permissions", "permissions != ''");
 	while($adminoption = $db->fetch_array($query))
 	{
-		$adminoption['permissions'] = unserialize($adminoption['permissions']);
+		$adminoption['permissions'] = my_unserialize($adminoption['permissions']);
 
 		if($default == -1)
 		{

admin/inc/functions_themes.php
@@ -103,7 +103,7 @@ function import_theme_xml($xml, $options=array())
 
 			if($property == 'colors' || $property == 'disporder')
 			{
-				$data = @unserialize($value['value']);
+				$data = my_unserialize($value['value']);
 
 				if(!is_array($data))
 				{
@@ -238,7 +238,7 @@ function import_theme_xml($xml, $options=array())
 		$query = $db->simple_select("themes", "stylesheets", "tid = '{$theme_id}'");
 		if($db->num_rows($query))
 		{
-			$inherited_stylesheets = unserialize($db->fetch_field($query, "stylesheets"));
+			$inherited_stylesheets = my_unserialize($db->fetch_field($query, "stylesheets"));
 
 			if(is_array($inherited_stylesheets['inherited']))
 			{
@@ -573,7 +573,7 @@ function build_new_theme($name, $properties=null, $parent=1)
 		$parent_theme = $db->fetch_array($query);
 		if(count($properties) == 0 || !is_array($properties))
 		{
-			$parent_properties = unserialize($parent_theme['properties']);
+			$parent_properties = my_unserialize($parent_theme['properties']);
 			if(!empty($parent_properties))
 			{
 				foreach($parent_properties as $property => $value)
@@ -597,7 +597,7 @@ function build_new_theme($name, $properties=null, $parent=1)
 			}
 		}
 
-		$parent_stylesheets = unserialize($parent_theme['stylesheets']);
+		$parent_stylesheets = my_unserialize($parent_theme['stylesheets']);
 		if(!empty($parent_stylesheets))
 		{
 			foreach($parent_stylesheets as $location => $value)
@@ -1042,7 +1042,7 @@ function update_theme_stylesheet_list($tid, $theme = false, $update_disporders =
 
 		if(!is_array($properties))
 		{
-			$properties = unserialize($theme['properties']);
+			$properties = my_unserialize($theme['properties']);
 		}
 
 		foreach($stylesheets as $stylesheet)
@@ -1179,8 +1179,8 @@ function cache_themes()
 		$query = $db->simple_select("themes", "*", "", array('order_by' => "pid, name"));
 		while($theme = $db->fetch_array($query))
 		{
-			$theme['properties'] = unserialize($theme['properties']);
-			$theme['stylesheets'] = unserialize($theme['stylesheets']);
+			$theme['properties'] = my_unserialize($theme['properties']);
+			$theme['stylesheets'] = my_unserialize($theme['stylesheets']);
 			$theme_cache[$theme['tid']] = $theme;
 
 			if($theme['def'] == 1)
@@ -1335,7 +1335,7 @@ function build_theme_array($ignoretid = null, $parent=0, $depth=0)
 function fetch_theme_stylesheets($theme)
 {
 	// Fetch list of all of the stylesheets for this theme
-	$file_stylesheets = unserialize($theme['stylesheets']);
+	$file_stylesheets = my_unserialize($theme['stylesheets']);
 
 	if(!is_array($file_stylesheets))
 	{

admin/inc/functions_view_manager.php
@@ -329,9 +329,9 @@ document.write('".str_replace("/", "\/", $field_select)."');
 		}
 		else
 		{
-			$admin_view['conditions'] = unserialize($admin_view['conditions']);
-			$admin_view['fields'] = unserialize($admin_view['fields']);
-			$admin_view['profile_fields'] = unserialize($admin_view['custom_profile_fields']);
+			$admin_view['conditions'] = my_unserialize($admin_view['conditions']);
+			$admin_view['fields'] = my_unserialize($admin_view['fields']);
+			$admin_view['profile_fields'] = my_unserialize($admin_view['custom_profile_fields']);
 			$mybb->input = array_merge($mybb->input, $admin_view);
 
 			$mybb->input['isdefault'] = 0;
@@ -494,8 +494,8 @@ document.write('".str_replace("/", "\/", $field_select)."');
 		$query = $db->simple_select("adminviews", "*", $type_where);
 		while($admin_view = $db->fetch_array($query))
 		{
-			$fields = unserialize($admin_view['fields']);
-			$conditions = unserialize($admin_view['conditions']);
+			$fields = my_unserialize($admin_view['fields']);
+			$conditions = my_unserialize($admin_view['conditions']);
 
 			$admin_view['title'] = str_replace(']]>', ']]]]><![CDATA[>', $admin_view['title']);
 			$admin_view['sortby'] = str_replace(']]>', ']]]]><![CDATA[>', $admin_view['sortby']);
@@ -635,7 +635,7 @@ function set_default_view($type, $vid)
 	global $mybb, $db;
 
 	$query = $db->simple_select("adminoptions", "defaultviews", "uid='{$mybb->user['uid']}'");
-	$default_views = unserialize($db->fetch_field($query, "defaultviews"));
+	$default_views = my_unserialize($db->fetch_field($query, "defaultviews"));
 	if(!$db->num_rows($query))
 	{
 		$create = true;
@@ -661,7 +661,7 @@ function fetch_default_view($type)
 {
 	global $mybb, $db;
 	$query = $db->simple_select("adminoptions", "defaultviews", "uid='{$mybb->user['uid']}'");
-	$default_views = unserialize($db->fetch_field($query, "defaultviews"));
+	$default_views = my_unserialize($db->fetch_field($query, "defaultviews"));
 	if(!is_array($default_views))
 	{
 		return false;

admin/index.php
@@ -319,7 +319,7 @@ else
 		}
 		else
 		{
-			$admin_session['data'] = @unserialize($admin_session['data']);
+			$admin_session['data'] = my_unserialize($admin_session['data']);
 
 			// Fetch the user from the admin session
 			$mybb->user = get_user($admin_session['uid']);

admin/modules/config/mod_tools.php
@@ -386,7 +386,7 @@ if($mybb->input['action'] == "edit_thread_tool")
 	{
 		$query = $db->simple_select("modtools", "*", "tid = '{$mybb->input['tid']}'");
 		$modtool = $db->fetch_array($query);
-		$thread_options = unserialize($modtool['threadoptions']);
+		$thread_options = my_unserialize($modtool['threadoptions']);
 
 		$mybb->input['title'] = $modtool['name'];
 		$mybb->input['description'] = $modtool['description'];
@@ -1377,8 +1377,8 @@ if($mybb->input['action'] == "edit_post_tool")
 	{
 		$query = $db->simple_select("modtools", "*", "tid = '{$mybb->input['tid']}'");
 		$modtool = $db->fetch_array($query);
-		$thread_options = unserialize($modtool['threadoptions']);
-		$post_options = unserialize($modtool['postoptions']);
+		$thread_options = my_unserialize($modtool['threadoptions']);
+		$post_options = my_unserialize($modtool['postoptions']);
 
 		$mybb->input['title'] = $modtool['name'];
 		$mybb->input['description'] = $modtool['description'];

admin/modules/config/warning.php
@@ -285,7 +285,7 @@ if($mybb->input['action'] == "edit_level")
 				"percentage" => $level['percentage'],
 			)
 		);
-		$action = unserialize($level['action']);
+		$action = my_unserialize($level['action']);
 		if($action['type'] == 1)
 		{
 			$mybb->input['action_1_usergroup'] = $action['usergroup'];
@@ -663,7 +663,7 @@ if($mybb->input['action'] == "levels")
 	while($level = $db->fetch_array($query))
 	{
 		$table->construct_cell("<strong>{$level['percentage']}%</strong>", array("class" => "align_center"));
-		$action = unserialize($level['action']);
+		$action = my_unserialize($level['action']);
 		$period = fetch_friendly_expiration($action['length']);
 
 		// Get the right language for the ban period

admin/modules/forum/attachments.php
@@ -298,26 +298,26 @@ if($mybb->input['action'] == "orphans")
 		// Incoming attachments which exist as files but not in database
 		if($mybb->input['bad_attachments'])
 		{
-			$bad_attachments = unserialize($mybb->input['bad_attachments']);
+			$bad_attachments = my_unserialize($mybb->input['bad_attachments']);
 			$results = count($bad_attachments);
 		}
 
 		$aids = array();
 		if($mybb->input['missing_attachment_files'])
 		{
-			$missing_attachment_files = unserialize($mybb->input['missing_attachment_files']);
+			$missing_attachment_files = my_unserialize($mybb->input['missing_attachment_files']);
 			$aids = array_merge($aids, $missing_attachment_files);
 		}
 
 		if($mybb->input['missing_threads'])
 		{
-			$missing_threads = unserialize($mybb->input['missing_threads']);
+			$missing_threads = my_unserialize($mybb->input['missing_threads']);
 			$aids = array_merge($aids, $missing_threads);
 		}
 
 		if($mybb->input['incomplete_attachments'])
 		{
-			$incomplete_attachments = unserialize($mybb->input['incomplete_attachments']);
+			$incomplete_attachments = my_unserialize($mybb->input['incomplete_attachments']);
 			$aids = array_merge($aids, $incomplete_attachments);
 		}
 

commit 245cf7a3e389990f37aee98da2fe27f5f7431af6

Fixing error introduced in f9dfafc.

admin/modules/style/themes.php
@@ -538,7 +538,7 @@ if($mybb->input['action'] == "export")
 		// Now we loop through the list of stylesheets for each file
 		foreach($file_stylesheets as $file => $action_stylesheet)
 		{
-			if($file == 'inherited' !is_array($action_stylesheet))
+			if($file == 'inherited' || !is_array($action_stylesheet))
 			{
 				continue;
 			}
@@ -1149,7 +1149,7 @@ if($mybb->input['action'] == "edit")
 	// Now we loop through the list of stylesheets for each file
 	foreach($file_stylesheets as $file => $action_stylesheet)
 	{
-		if($file == 'inherited' !is_array($action_stylesheet))
+		if($file == 'inherited'|| !is_array($action_stylesheet))
 		{
 			continue;
 		}