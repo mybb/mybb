mybb/mybb

commit fd241e8a9ab5935fb8df7f308c4fd7c83ae2d2f9

WIP #882 Add more hooks v2
Fixes ACP hooks positions.

admin/inc/class_page.php
@@ -73,7 +73,12 @@ class DefaultPage
 	{
 		global $mybb, $admin_session, $lang, $plugins;
 
-		$plugins->run_hooks("admin_page_output_header");
+		$args = array(
+			'this' => &$this,
+			'title' => &$title,
+		);
+
+		$plugins->run_hooks("admin_page_output_header", $args);
 
 		if(!$title)
 		{
@@ -189,7 +194,12 @@ lang.saved = \"{$lang->saved}\";
 	{
 		global $mybb, $maintimer, $db, $lang, $plugins;
 
-		$plugins->run_hooks("admin_page_output_footer");
+		$args = array(
+			'this' => &$this,
+			'quit' => &$quit,
+		);
+
+		$plugins->run_hooks("admin_page_output_footer", $args);
 
 		$memory_usage = get_friendly_size(get_memory_usage());
 
@@ -790,7 +800,16 @@ EOF;
 	 */
 	function output_confirm_action($url, $message="", $title="")
 	{
-		global $lang;
+		global $lang, $plugins;
+
+		$args = array(
+			'this' => &$this,
+			'url' => &$url,
+			'message' => &$message,,
+			'title' => &$title,
+		);
+
+		$plugins->run_hooks('admin_page_output_confirm_action', $args);
 
 		if(!$message)
 		{
@@ -798,6 +817,7 @@ EOF;
 		}
 		$this->output_header($title);
 		$form = new Form($url, 'post');
+
 		echo "<div class=\"confirm_action\">\n";
 		echo "<p>{$message}</p>\n";
 		echo "<br />\n";
@@ -806,6 +826,7 @@ EOF;
 		echo $form->generate_submit_button($lang->no, array("name" => "no", 'class' => 'button_no'));
 		echo "</p>\n";
 		echo "</div>\n";
+
 		$form->end();
 		$this->output_footer();
 	}

admin/modules/config/attachment_types.php
@@ -139,8 +139,6 @@ if($mybb->input['action'] == "add")
 
 if($mybb->input['action'] == "edit")
 {
-	$plugins->run_hooks("admin_config_attachment_types_edit");
-
 	$query = $db->simple_select("attachtypes", "*", "atid='".intval($mybb->input['atid'])."'");
 	$attachment_type = $db->fetch_array($query);
 
@@ -149,6 +147,8 @@ if($mybb->input['action'] == "edit")
 		flash_message($lang->error_invalid_attachment_type, 'error');
 		admin_redirect("index.php?module=config-attachment_types");
 	}
+
+	$plugins->run_hooks("admin_config_attachment_types_edit");
 
 	if($mybb->request_method == "post")
 	{
@@ -182,9 +182,9 @@ if($mybb->input['action'] == "edit")
 				"icon" => $db->escape_string($mybb->input['icon'])
 			);
 
-			$db->update_query("attachtypes", $updated_type, "atid='{$attachment_type['atid']}'");
-
 			$plugins->run_hooks("admin_config_attachment_types_edit_commit");
+
+			$db->update_query("attachtypes", $updated_type, "atid='{$attachment_type['atid']}'");
 
 			// Log admin action
 			log_admin_action($attachment_type['atid'], $mybb->input['extension']);
@@ -254,8 +254,6 @@ if($mybb->input['action'] == "edit")
 
 if($mybb->input['action'] == "delete")
 {
-	$plugins->run_hooks("admin_config_attachment_types_delete");
-
 	if($mybb->input['no'])
 	{
 		admin_redirect("index.php?module=config-attachment_types");
@@ -269,6 +267,8 @@ if($mybb->input['action'] == "delete")
 		flash_message($lang->error_invalid_attachment_type, 'error');
 		admin_redirect("index.php?module=config-attachment_types");
 	}
+
+	$plugins->run_hooks("admin_config_attachment_types_delete");
 
 	if($mybb->request_method == "post")
 	{
@@ -292,8 +292,6 @@ if($mybb->input['action'] == "delete")
 
 if(!$mybb->input['action'])
 {
-	$plugins->run_hooks("admin_config_attachment_types_start");
-
 	$page->output_header($lang->attachment_types);
 
 	$sub_tabs['attachment_types'] = array(
@@ -305,6 +303,8 @@ if(!$mybb->input['action'])
 		'title' => $lang->add_new_attachment_type,
 		'link' => "index.php?module=config-attachment_types&amp;action=add",
 	);
+
+	$plugins->run_hooks("admin_config_attachment_types_start");
 
 	$page->output_nav_tabs($sub_tabs, 'attachment_types');
 

admin/modules/config/badwords.php
@@ -81,8 +81,6 @@ if($mybb->input['action'] == "add" && $mybb->request_method == "post")
 
 if($mybb->input['action'] == "delete")
 {
-	$plugins->run_hooks("admin_config_badwords_delete");
-
 	$query = $db->simple_select("badwords", "*", "bid='".intval($mybb->input['bid'])."'");
 	$badword = $db->fetch_array($query);
 
@@ -98,6 +96,8 @@ if($mybb->input['action'] == "delete")
 	{
 		admin_redirect("index.php?module=config-badwords");
 	}
+
+	$plugins->run_hooks("admin_config_badwords_delete");
 
 	if($mybb->request_method == "post")
 	{
@@ -122,8 +122,6 @@ if($mybb->input['action'] == "delete")
 
 if($mybb->input['action'] == "edit")
 {
-	$plugins->run_hooks("admin_config_badwords_edit");
-
 	$query = $db->simple_select("badwords", "*", "bid='".intval($mybb->input['bid'])."'");
 	$badword = $db->fetch_array($query);
 
@@ -133,6 +131,8 @@ if($mybb->input['action'] == "edit")
 		flash_message($lang->error_invalid_bid, 'error');
 		admin_redirect("index.php?module=config-badwords");
 	}
+
+	$plugins->run_hooks("admin_config_badwords_edit");
 
 	if($mybb->request_method == "post")
 	{
@@ -158,9 +158,9 @@ if($mybb->input['action'] == "edit")
 				"replacement" => $db->escape_string($mybb->input['replacement'])
 			);
 
-			$db->update_query("badwords", $updated_badword, "bid='{$badword['bid']}'");
-
 			$plugins->run_hooks("admin_config_badwords_edit_commit");
+
+			$db->update_query("badwords", $updated_badword, "bid='{$badword['bid']}'");
 
 			// Log admin action
 			log_admin_action($badword['bid'], $mybb->input['badword']);
@@ -208,8 +208,6 @@ if($mybb->input['action'] == "edit")
 
 if(!$mybb->input['action'])
 {
-	$plugins->run_hooks("admin_config_badwords_start");
-
 	$page->output_header($lang->bad_words);
 
 	$sub_tabs['badwords'] = array(
@@ -217,6 +215,8 @@ if(!$mybb->input['action'])
 		'description' => $lang->bad_word_filters_desc,
 		'link' => "index.php?module=config-badwords"
 	);
+
+	$plugins->run_hooks("admin_config_badwords_start");
 
 	$page->output_nav_tabs($sub_tabs, "badwords");
 

admin/modules/config/banning.php
@@ -92,8 +92,6 @@ if($mybb->input['action'] == "add" && $mybb->request_method == "post")
 
 if($mybb->input['action'] == "delete")
 {
-	$plugins->run_hooks("admin_config_banning_delete");
-
 	$query = $db->simple_select("banfilters", "*", "fid='".intval($mybb->input['fid'])."'");
 	$filter = $db->fetch_array($query);
 
@@ -103,6 +101,8 @@ if($mybb->input['action'] == "delete")
 		flash_message($lang->error_invalid_filter, 'error');
 		admin_redirect("index.php?module=config-banning");
 	}
+
+	$plugins->run_hooks("admin_config_banning_delete");
 
 	if($filter['type'] == 3)
 	{

admin/modules/config/calendars.php
@@ -65,7 +65,11 @@ if($mybb->input['action'] == "add")
 				"allowsmilies" => $db->escape_string($mybb->input['allowsmilies'])
 			);
 
+			$plugins->run_hooks("admin_config_calendars_add_commit_start");
+
 			$cid = $db->insert_query("calendars", $calendar);
+
+			$plugins->run_hooks("admin_config_calendars_add_commit_end");
 
 			// Log admin action
 			log_admin_action($cid, $mybb->input['name']);
@@ -128,8 +132,6 @@ if($mybb->input['action'] == "add")
 
 if($mybb->input['action'] == "permissions")
 {
-	$plugins->run_hooks("admin_config_calendars_permissions");
-
 	$usergroups = array();
 
 	$query = $db->simple_select("calendars", "*", "cid='".intval($mybb->input['cid'])."'");
@@ -141,6 +143,8 @@ if($mybb->input['action'] == "permissions")
 		flash_message($lang->error_invalid_calendar, 'error');
 		admin_redirect("index.php?module=config-calendars");
 	}
+
+	$plugins->run_hooks("admin_config_calendars_permissions");
 
 	$query = $db->simple_select("usergroups", "*", "", array("order" => "name"));
 	while($usergroup = $db->fetch_array($query))
@@ -271,8 +275,6 @@ if($mybb->input['action'] == "permissions")
 
 if($mybb->input['action'] == "edit")
 {
-	$plugins->run_hooks("admin_config_calendars_edit");
-
 	$query = $db->simple_select("calendars", "*", "cid='".intval($mybb->input['cid'])."'");
 	$calendar = $db->fetch_array($query);
 
@@ -282,6 +284,8 @@ if($mybb->input['action'] == "edit")
 		flash_message($lang->error_invalid_calendar, 'error');
 		admin_redirect("index.php?module=config-calendars");
 	}
+
+	$plugins->run_hooks("admin_config_calendars_edit");
 
 	if($mybb->request_method == "post")
 	{
@@ -311,9 +315,9 @@ if($mybb->input['action'] == "edit")
 				"allowsmilies" => $db->escape_string($mybb->input['allowsmilies'])
 			);
 
-			$db->update_query("calendars", $updated_calendar, "cid = '".intval($mybb->input['cid'])."'");
-
 			$plugins->run_hooks("admin_config_calendars_edit_commit");
+
+			$db->update_query("calendars", $updated_calendar, "cid = '".intval($mybb->input['cid'])."'");
 
 			// Log admin action
 			log_admin_action($calendar['cid'], $mybb->input['name']);
@@ -371,8 +375,6 @@ if($mybb->input['action'] == "edit")
 
 if($mybb->input['action'] == "delete")
 {
-	$plugins->run_hooks("admin_config_calendars_delete");
-
 	$query = $db->simple_select("calendars", "*", "cid='".intval($mybb->input['cid'])."'");
 	$calendar = $db->fetch_array($query);
 
@@ -382,6 +384,8 @@ if($mybb->input['action'] == "delete")
 		flash_message($lang->error_invalid_calendar, 'error');
 		admin_redirect("index.php?module=config-calendars");
 	}
+
+	$plugins->run_hooks("admin_config_calendars_delete");
 
 	// User clicked no
 	if($mybb->input['no'])
@@ -412,12 +416,12 @@ if($mybb->input['action'] == "delete")
 
 if($mybb->input['action'] == "update_order" && $mybb->request_method == "post")
 {
-	$plugins->run_hooks("admin_config_calendars_update_order");
-
 	if(!is_array($mybb->input['disporder']))
 	{
 		admin_redirect("index.php?module=config-calendars");
 	}
+
+	$plugins->run_hooks("admin_config_calendars_update_order");
 
 	foreach($mybb->input['disporder'] as $cid => $order)
 	{

admin/modules/config/help_documents.php
@@ -299,9 +299,9 @@ if($mybb->input['action'] == "edit")
 					"disporder" => intval($mybb->input['disporder'])
 				);
 
-				$db->update_query("helpsections", $sql_array, "sid = '{$sid}'");
-
 				$plugins->run_hooks("admin_config_help_documents_edit_section_commit");
+
+				$db->update_query("helpsections", $sql_array, "sid = '{$sid}'");
 
 				// Log admin action
 				log_admin_action($sid, $mybb->input['name'], 'section');
@@ -407,9 +407,9 @@ if($mybb->input['action'] == "edit")
 					"disporder" => intval($mybb->input['disporder'])
 				);
 
-				$db->update_query("helpdocs", $sql_array, "hid = '{$hid}'");
-
 				$plugins->run_hooks("admin_config_help_documents_edit_page_commit");
+
+				$db->update_query("helpdocs", $sql_array, "hid = '{$hid}'");
 
 				// Log admin action
 				log_admin_action($hid, $mybb->input['name'], 'document');
@@ -482,13 +482,13 @@ if($mybb->input['action'] == "edit")
 // Delete something
 if($mybb->input['action'] == "delete")
 {
-	$plugins->run_hooks("admin_config_help_documents_delete");
-
 	// User clicked no
 	if($mybb->input['no'])
 	{
 		admin_redirect("index.php?module=config-help_documents");
 	}
+
+	$plugins->run_hooks("admin_config_help_documents_delete");
 
 	// Do delete something?
 	if($mybb->request_method == "post")
@@ -568,8 +568,6 @@ if($mybb->input['action'] == "delete")
 // List document and sections
 if(!$mybb->input['action'])
 {
-	$plugins->run_hooks("admin_config_help_documents_start");
-
 	$page->output_header($lang->help_documents);
 
 	$sub_tabs['manage_help_documents'] = array(
@@ -587,6 +585,8 @@ if(!$mybb->input['action'])
 		'title'	=> $lang->add_new_section,
 		'link'	=> "index.php?module=config-help_documents&amp;action=add&amp;type=section"
 	);
+
+	$plugins->run_hooks("admin_config_help_documents_start");
 
 	$page->output_nav_tabs($sub_tabs, 'manage_help_documents');
 

admin/modules/config/languages.php
@@ -22,8 +22,6 @@ $plugins->run_hooks("admin_config_languages_begin");
 
 if($mybb->input['action'] == "edit_properties")
 {
-	$plugins->run_hooks("admin_config_languages_edit_properties");
-
 	$editlang = basename($mybb->input['lang']);
 	$file = MYBB_ROOT."inc/languages/".$editlang.".php";
 	if(!file_exists($file))
@@ -31,6 +29,8 @@ if($mybb->input['action'] == "edit_properties")
 		flash_message($lang->error_invalid_file, 'error');
 		admin_redirect("index.php?module=config-languages");
 	}
+
+	$plugins->run_hooks("admin_config_languages_edit_properties");
 
 	if($mybb->request_method == "post")
 	{
@@ -180,8 +180,6 @@ if($mybb->input['action'] == "edit_properties")
 
 if($mybb->input['action'] == "quick_phrases")
 {
-	$plugins->run_hooks("admin_config_languages_quick_phrases");
-
 	// Validate input
 	$editlang = basename($mybb->input['lang']);
 	$folder = MYBB_ROOT."inc/languages/".$editlang."/";
@@ -193,6 +191,8 @@ if($mybb->input['action'] == "quick_phrases")
 		flash_message($lang->error_invalid_set, 'error');
 		admin_redirect("index.php?module=config-languages");
 	}
+
+	$plugins->run_hooks("admin_config_languages_quick_phrases");
 
 	$quick_phrases = array(
 		'member.lang.php' => array(
@@ -324,8 +324,6 @@ if($mybb->input['action'] == "quick_phrases")
 
 if($mybb->input['action'] == "edit")
 {
-	$plugins->run_hooks("admin_config_languages_edit");
-
 	// Validate input
 	$editlang = basename($mybb->input['lang']);
 	$folder = MYBB_ROOT."inc/languages/".$editlang."/";
@@ -345,6 +343,8 @@ if($mybb->input['action'] == "edit")
 		flash_message($lang->error_invalid_set, 'error');
 		admin_redirect("index.php?module=config-languages");
 	}
+
+	$plugins->run_hooks("admin_config_languages_edit");
 
 	if(isset($mybb->input['file']))
 	{
@@ -610,8 +610,6 @@ if($mybb->input['action'] == "edit")
 
 if(!$mybb->input['action'])
 {
-	$plugins->run_hooks("admin_config_languages_start");
-
 	$page->output_header($lang->languages);
 
 	$sub_tabs['languages'] = array(
@@ -624,6 +622,8 @@ if(!$mybb->input['action'])
 		'link' => "http://www.mybb.com/downloads/translations",
 		'target' => "_blank"
 	);
+
+	$plugins->run_hooks("admin_config_languages_start");
 
 	$page->output_nav_tabs($sub_tabs, 'languages');
 

admin/modules/config/mod_tools.php
@@ -20,8 +20,6 @@ $plugins->run_hooks("admin_config_mod_tools_begin");
 
 if($mybb->input['action'] == "delete_post_tool")
 {
-	$plugins->run_hooks("admin_config_mod_tools_delete_post_tool");
-
 	$query = $db->simple_select("modtools", "*", "tid='{$mybb->input['tid']}'");
 	$tool = $db->fetch_array($query);
 
@@ -37,6 +35,8 @@ if($mybb->input['action'] == "delete_post_tool")
 	{
 		admin_redirect("index.php?module=config-mod_tools&action=post_tools");
 	}
+
+	$plugins->run_hooks("admin_config_mod_tools_delete_post_tool");
 
 	if($mybb->request_method == 'post')
 	{
@@ -60,8 +60,6 @@ if($mybb->input['action'] == "delete_post_tool")
 
 if($mybb->input['action'] == "delete_thread_tool")
 {
-	$plugins->run_hooks("admin_config_mod_tools_delete_thread_tool");
-
 	$query = $db->simple_select("modtools", "*", "tid='{$mybb->input['tid']}'");
 	$tool = $db->fetch_array($query);
 
@@ -77,6 +75,8 @@ if($mybb->input['action'] == "delete_thread_tool")
 	{
 		admin_redirect("index.php?module=config-mod_tools");
 	}
+
+	$plugins->run_hooks("admin_config_mod_tools_delete_thread_tool");
 
 	if($mybb->request_method == 'post')
 	{
@@ -151,14 +151,14 @@ if($mybb->input['action'] == "post_tools")
 
 if($mybb->input['action'] == "edit_thread_tool")
 {
-	$plugins->run_hooks("admin_config_mod_tools_edit_thread_tool");
-
 	$query = $db->simple_select("modtools", "COUNT(tid) as tools", "tid = '{$mybb->input['tid']}' AND type='t'");
 	if($db->fetch_field($query, "tools") < 1)
 	{
 		flash_message($lang->error_invalid_thread_tool, 'error');
 		admin_redirect("index.php?module=config-mod_tools");
 	}
+
+	$plugins->run_hooks("admin_config_mod_tools_edit_thread_tool");
 
 	if($mybb->request_method == 'post')
 	{
@@ -1120,14 +1120,14 @@ if($mybb->input['action'] == "add_thread_tool")
 
 if($mybb->input['action'] == "edit_post_tool")
 {
-	$plugins->run_hooks("admin_config_mod_tools_edit_post_tool");
-
 	$query = $db->simple_select("modtools", "COUNT(tid) as tools", "tid = '{$mybb->input['tid']}' AND type='p'");
 	if($db->fetch_field($query, "tools") < 1)
 	{
 		flash_message($lang->error_invalid_post_tool, 'error');
 		admin_redirect("index.php?module=config-mod_tools&action=post_tools");
 	}
+
+	$plugins->run_hooks("admin_config_mod_tools_edit_post_tool");
 
 	if($mybb->request_method == 'post')
 	{

commit e33a74230246fab6eea00bf2e4bf8440936d7482

Fix PHP error introduced in fd241e8

admin/inc/class_page.php
@@ -111,7 +111,7 @@ class DefaultPage
 		echo "	<script type=\"text/javascript\" src=\"../jscripts/general.js\"></script>\n";
 		echo "	<script type=\"text/javascript\" src=\"./jscripts/admincp.js\"></script>\n";
 		echo "	<script type=\"text/javascript\" src=\"./jscripts/tabs.js\"></script>\n";
-		
+
 		echo "	<link rel=\"stylesheet\" href=\"jscripts/jqueryui/css/redmond/jquery-ui-1.10.4.custom.min.css\" />\n";
 		echo "	<script src=\"jscripts/jqueryui/js/jquery-ui-1.10.4.custom.min.js\"></script>\n";
 
@@ -805,7 +805,7 @@ EOF;
 		$args = array(
 			'this' => &$this,
 			'url' => &$url,
-			'message' => &$message,,
+			'message' => &$message,
 			'title' => &$title,
 		);
 
@@ -842,7 +842,7 @@ EOF;
 	{
 		global $lang, $mybb, $smiliecache, $cache;
 
-		// Smilies		
+		// Smilies
 		$emoticon = "";
 		$emoticons_enabled = "false";
 		if($smilies && $mybb->settings['smilieinserter'] != 0 && $mybb->settings['smilieinsertercols'] && $mybb->settings['smilieinsertertot'])
@@ -985,7 +985,7 @@ opt_editor = {
 {$editor_language}
 $(function() {
 	$("#{$bind}").sceditor(opt_editor);
-      
+
 	MyBBEditor = $("#{$bind}").sceditor("instance");
 	{$sourcemode}
 });