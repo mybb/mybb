58e373fa7366385d47c28f90ef222f99a2511b14
Euan Torano <euantorano@gmail.com>
2014-07-21 11:15:02 +0100

    Working on spam log pruning.

--- admin/modules/tools/spamlog.php
+++ admin/modules/tools/spamlog.php
@@ -15,7 +15,122 @@ if(!defined("IN_MYBB"))
 
 $page->add_breadcrumb_item($lang->spamlog, "index.php?module=tools-spamlog");
 
+$sub_tabs['spam_logs'] = array(
+	'title' => $lang->spam_logs,
+	'link' => "index.php?module=tools-spamlog",
+	'description' => $lang->spam_logs_desc
+);
+$sub_tabs['prune_spam_logs'] = array(
+	'title' => $lang->prune_spam_logs,
+	'link' => "index.php?module=tools-spamlog&amp;action=prune",
+	'description' => $lang->prune_spam_logs_desc
+);
+
 $plugins->run_hooks("admin_tools_spamlog_begin");
+
+if($mybb->input['action'] == 'prune')
+{
+	if(!is_super_admin($mybb->user['uid']))
+	{
+		flash_message($lang->cannot_perform_action_super_admin_general, 'error');
+		admin_redirect("index.php?module=tools-spamlog");
+	}
+
+	$plugins->run_hooks("admin_tools_spamlog_prune");
+
+	if($mybb->request_method == 'post')
+	{
+		$is_today = false;
+		if($mybb->input['older_than'] <= 0)
+		{
+			$is_today = true;
+			$mybb->input['older_than'] = 1;
+		}
+		$where = 'dateline < '.(TIME_NOW-(intval($mybb->input['older_than'])*86400));
+
+		// Searching for entries in a specific module
+		if($mybb->input['filter_username'])
+		{
+			$where .= " AND username='".$db->escape_string($mybb->input['filter_username'])."'";
+		}
+		
+		// Searching for entries in a specific module
+		if($mybb->input['filter_email'])
+		{
+			$where .= " AND email='".$db->escape_string($mybb->input['filter_email'])."'";
+		}
+
+		$query = $db->delete_query("spamlog", $where);
+		$num_deleted = $db->affected_rows();
+
+		$plugins->run_hooks("admin_tools_spamlog_prune_commit");
+
+		// Log admin action
+		log_admin_action($mybb->input['older_than'], $mybb->input['filter_username'], $mybb->input['filter_email'], $num_deleted);
+
+		$success = $lang->success_pruned_spam_logs;
+		if($is_today == true && $num_deleted > 0)
+		{
+			$success .= ' '.$lang->note_logs_locked;
+		}
+		elseif($is_today == true && $num_deleted == 0)
+		{
+			flash_message($lang->note_logs_locked, 'error');
+			admin_redirect('index.php?module=tools-spamlog');
+		}
+		flash_message($success, 'success');
+		admin_redirect('index.php?module=tools-spamlog');
+	}
+	$page->add_breadcrumb_item($lang->prune_spam_logs, 'index.php?module=tools-spamlog&amp;action=prune');
+	$page->output_header($lang->prune_spam_logs);
+	$page->output_nav_tabs($sub_tabs, 'prune_spam_logs');
+
+	// Fetch filter options
+	$sortbysel[$mybb->input['sortby']] = 'selected="selected"';
+	$ordersel[$mybb->input['order']] = 'selected="selected"';
+
+	$username_options = array();
+	$username_options[''] = $lang->all_usernames;
+	$username_options['0'] = '----------';
+	$query = $db->query("
+		SELECT DISTINCT l.username
+		FROM ".TABLE_PREFIX."spamlog l
+		ORDER BY l.username ASC
+	");
+	while($username = $db->fetch_array($query))
+	{
+		$username_options[$username['username']] = htmlspecialchars_uni($username['username']);
+	}
+	
+	$email_options = array();
+	$email_options[''] = $lang->all_emails;
+	$email_options['0'] = '----------';
+	$query = $db->query("
+		SELECT DISTINCT l.email
+		FROM ".TABLE_PREFIX."spamlog l
+		ORDER BY l.email ASC
+	");
+	while($email = $db->fetch_array($query))
+	{
+		$email_options[$email['email']] = htmlspecialchars_uni($email['email']);
+	}
+
+	$form = new Form("index.php?module=tools-spamlog&amp;action=prune", "post");
+	$form_container = new FormContainer($lang->prune_spam_logs);
+	$form_container->output_row($lang->spam_username, "", $form->generate_select_box('filter_username', $username_options, $mybb->input['filter_username'], array('id' => 'filter_username')), 'filter_username');
+	$form_container->output_row($lang->spam_email, "", $form->generate_select_box('filter_email', $email_options, $mybb->input['filter_email'], array('id' => 'filter_email')), 'filter_email');
+	if(!$mybb->input['older_than'])
+	{
+		$mybb->input['older_than'] = '30';
+	}
+	$form_container->output_row($lang->date_range, "", $lang->older_than.$form->generate_text_box('older_than', $mybb->input['older_than'], array('id' => 'older_than', 'style' => 'width: 30px'))." {$lang->days}", 'older_than');
+	$form_container->end();
+	$buttons[] = $form->generate_submit_button($lang->prune_spam_logs);
+	$form->output_submit_wrapper($buttons);
+	$form->end();
+
+	$page->output_footer();
+}
 
 if(!$mybb->input['action'])
 {
@@ -51,14 +166,6 @@ if(!$mybb->input['action'])
 	$start = ($view_page - 1) * $per_page;
 	// Build the base URL for pagination links
 	$url = 'index.php?module=tools-spamlog';
-	if(is_array($mybb->input['filter']) && count($mybb->input['filter']))
-	{
-		foreach($mybb->input['filter'] as $field => $value)
-		{
-			$value = urlencode($value);
-			$url .= "&amp;filter[{$field}]={$value}";
-		}
-	}
 
 	// The actual query
 	$sql   = "
--- inc/languages/english/admin/tools_spamlog.lang.php
+++ inc/languages/english/admin/tools_spamlog.lang.php
@@ -7,10 +7,17 @@
 
 $l['spam_logs']       = 'Spam Logs';
 $l['spam_logs_desc']  = 'This section allows you to view a history of users blocked by the spam filters.';
+$l['prune_spam_logs']       = 'Prune Spam Logs';
+$l['prune_spam_logs_desc']  = 'Here you can prune the spam logs matching a specified criteria.';
 $l['spam_username']   = 'Username';
 $l['spam_email']      = 'Email Address';
 $l['spam_ip']         = 'IP Address';
 $l['spam_date']       = 'Date';
 $l['spam_confidence'] = 'Confidence';
-$l['no_spam_logs'] = 'No users have been blocked by the spam filters yet.'
-?>
+$l['no_spam_logs'] = 'No users have been blocked by the spam filters yet.';
+$l['success_pruned_spam_logs'] = 'The spam logs have been pruned successfully.';
+$l['note_logs_locked'] = 'For security reasons, logs less than 24 hours old cannot be pruned.';
+$l['all_usernames'] = 'All usernames';
+$l['all_emails'] = ' All Email Addresses';
+$l['date_range'] = "Date range:";
+$l['days'] = "days";
