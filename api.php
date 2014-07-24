21450db29c00b6d2c653fc67a606436cc27303c1
Eric Hocking <xaoseric@users.noreply.github.com>
2014-07-24 12:58:40 -0500

    create api front end file

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MyBB 1.8
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+define("IN_MYBB", 1);
+require './global.php';
+
+$api = new api();
+$api->run();
+
+?>
