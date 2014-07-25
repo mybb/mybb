cb1cf70a9a1a1bb42e99f00f1b757f75afe71b99
effone <effone@demonate.com>
2014-07-25 17:03:26 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -268,7 +268,13 @@ else
 
 $mybb->input['subject'] = htmlspecialchars_uni($mybb->input['subject']);
 $mybb->input['message'] = htmlspecialchars_uni($mybb->input['message']);
-$mybb->input['email'] = htmlspecialchars_uni($mybb->input['email']);
+
+if($mybb->user['uid'] && !$mybb->get_input('email'))
+{
+    $mybb->input['email'] = htmlspecialchars_uni($mybb->user['email']);
+} else {
+    $mybb->input['email'] = $mybb->get_input('email');
+}
 
 $plugins->run_hooks('contact_end');
 
