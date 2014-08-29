182a85f57a7ca47d7af1d2841e962a47a07adbf6
Stefan T. <Stefan-ST@users.noreply.github.com>
2014-08-29 10:10:22 +0200

    Preparing 1.8.0

--- inc/languages/english.php
+++ inc/languages/english.php
@@ -15,7 +15,7 @@ $langinfo['author'] = "MyBB Group";
 $langinfo['website'] = "http://www.mybb.com/";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1700";
+$langinfo['version'] = "1800";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
