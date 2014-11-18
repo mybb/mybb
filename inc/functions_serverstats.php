8ce91d25b525e4179bfe3080ae285a5384b86c1c
PirataNervo <piratanervo@gmail.com>
2014-11-18 11:45:35 +0000

    Fixing double sanitizing

--- inc/functions_serverstats.php
+++ inc/functions_serverstats.php
@@ -278,7 +278,7 @@ function build_server_stats($is_install=1, $prev_version='', $current_version=''
 	{
 		$return['info_sent_success'] = true;
 	}
-	$return['info_image'] = "<img src='".htmlspecialchars_uni($server_stats_url)."&amp;img=1' />";
+	$return['info_image'] = "<img src='".$server_stats_url."&amp;img=1' />";
 	$return['info_get_string'] = $string;
 
 	return $return;
