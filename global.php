3284975c6bee9a17cb11b0d24b63c631ac988cf5
Stefan-ST <mybbcms@yahoo.de>
2015-01-25 15:19:56 +0100

    Variable initialization

--- global.php
+++ global.php
@@ -17,7 +17,7 @@ if(!$working_dir)
 // Load main MyBB core file which begins all of the magic
 require_once $working_dir.'/inc/init.php';
 
-$shutdown_queries = array();
+$shutdown_queries = $shutdown_functions = array();
 
 // Read the usergroups cache as well as the moderators cache
 $groupscache = $cache->read('usergroups');
