2ca3f05059bef53eeb2ddb9314c48f13c61d5daa
Omar Gonzalez <Sama34@users.noreply.github.com>
2015-02-18 22:30:43 -0800

    Fix #1836 split used in class_parser.php

--- inc/class_parser.php
+++ inc/class_parser.php
@@ -1342,7 +1342,8 @@ class postParser
 		switch($video)
 		{
 			case "dailymotion":
-				list($id, ) = split("_", $path[2], 1); // http://www.dailymotion.com/video/fds123_title-goes-here
+				$id = explode('_', $path[2]);
+				$id = $id[0]; // http://www.dailymotion.com/video/fds123_title-goes-here
 				break;
 			case "metacafe":
 				$id = $path[2]; // http://www.metacafe.com/watch/fds123/title_goes_here/
