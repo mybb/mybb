4b41a8b82707fd57a8215f8c68c099c8e9e8bc04
Mathias Garbe <mail@mathias-garbe.de>
2015-05-30 00:13:19 +0200

    Fix #2003 Deleting an event in 'editevent' without checking checkbox throws errors
    Adds a new action 'do_deleteevent' for deleting events and displays a proper error message (with a new language string) when it is not checked. The JavaScript function MyBB.deleteEvent(eid) is also using this action now. The action 'do_editevent' is not capable of deleting events anymore.
    
    https://github.com/mybb/mybb/issues/2003
    
    Bumps version of general.js and sets version to 1806 on the modified template
    
    Updates all links to general.js to version 1806.
    
    Bumps headerinclude version to 1806.
    
    Adds plugin hooks `calendar_do_deleteevent_start` and `calendar_do_deleteevent_end`

--- install/resources/output.php
+++ install/resources/output.php
@@ -40,7 +40,7 @@ class installerOutput {
 	<title>{$this->title} &gt; {$title}</title>
 	<link rel="stylesheet" href="stylesheet.css" type="text/css" />
 	<script type="text/javascript" src="../jscripts/jquery.js"></script>
-	<script type="text/javascript" src="../jscripts/general.js"></script>
+	<script type="text/javascript" src="../jscripts/general.js?ver=1806"></script>
 	{$dbconfig_add}
 </head>
 <body>
