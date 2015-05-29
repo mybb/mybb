700f8acdbc98f44bc2c166c5aa165f7012210655
Mathias Garbe <mail@mathias-garbe.de>
2015-05-29 11:49:38 +0200

    Fix #2001 Calendar does not show errors when adding events
    The '$event_errors' variable was cleared for no reason right before outputting the template, therefore deleting the errors.
    
    https://github.com/mybb/mybb/issues/2001

--- calendar.php
+++ calendar.php
@@ -542,8 +542,6 @@ if($mybb->input['action'] == "addevent")
 		eval("\$calendar_select .= \"".$templates->get("calendar_addevent_calendarselect_hidden")."\";");
 	}
 
-	$event_errors = '';
-
 	$plugins->run_hooks("calendar_addevent_end");
 
 	eval("\$addevent = \"".$templates->get("calendar_addevent")."\";");
