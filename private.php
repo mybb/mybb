31200d55b1e3a43450dfab405442bdc0852ec1dd
Nathan <Naththo@outlook.com>
2015-06-28 12:57:33 +0100

    Modifications

--- private.php
+++ private.php
@@ -1111,8 +1111,7 @@ if($mybb->input['action'] == "read")
 	}
 
 	// Fetch recipient names from the database
-	$bcc_recipients = $to_recipients = array();
-	$bcc_form_val = $to_recipients = array();
+	$bcc_recipients = $to_recipients = $bcc_form_val = array();
 	$query = $db->simple_select('users', 'uid, username', "uid IN ({$uid_sql})");
 	while($recipient = $db->fetch_array($query))
 	{
@@ -1126,7 +1125,6 @@ if($mybb->input['action'] == "read")
 		else if(in_array($recipient['uid'], $pm['recipients']['to']))
 		{
 			$to_recipients[] = build_profile_link($recipient['username'], $recipient['uid']);
-			$bcc_form_val[] = $recipient['username'];
 		}
 	}
 
@@ -1134,7 +1132,7 @@ if($mybb->input['action'] == "read")
 	if(count($bcc_recipients) > 0)
 	{
 		$bcc_recipients = implode(', ', $bcc_recipients);
-		$bcc_form_val = implode(', ', $bcc_form_val);
+		$bcc_form_val = implode(',', $bcc_form_val);
 		eval("\$bcc = \"".$templates->get("private_read_bcc")."\";");
 	}
 
