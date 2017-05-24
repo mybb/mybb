256fbe97bdf172fad5ccddab890246f692152931
Euan T <euantorano@users.noreply.github.com>
2017-05-24 19:45:15 +0100

    Update upgrade41.php

--- install/resources/upgrade41.php
+++ install/resources/upgrade41.php
@@ -28,13 +28,13 @@ function upgrade41_dbchanges()
 	$db->delete_query('spiders', 'name=\'Blekko\'');
 
 	// Insert Modern Spiders
-	db->insert_query('spiders', array("name" => "Discord", "useragent" => "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)"));
-	db->insert_query('spiders', array("name" => "Applebot", "useragent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5 (Applebot/0.1; +http://www.apple.com/go/applebot)"));
-	db->insert_query('spiders', array("name" => "CheckHost", "useragent" => "CheckHost (http://check-host.net/)"));
-	db->insert_query('spiders', array("name" => "Pingdom", "useragent" => "Pingdom.com_bot_version_1.4_(http://www.pingdom.com)"));
-	db->insert_query('spiders', array("name" => "DuckDuckGo", "useragent" => "DuckDuckBot/1.1; (+http://duckduckgo.com/duckduckbot.html)"));
-	db->insert_query('spiders', array("name" => "UptimeRobot", "useragent" => "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"));
+	$db->insert_query('spiders', array("name" => "Discord", "useragent" => "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)"));
+	$db->insert_query('spiders', array("name" => "Applebot", "useragent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.2.5 (KHTML, like Gecko) Version/8.0.2 Safari/600.2.5 (Applebot/0.1; +http://www.apple.com/go/applebot)"));
+	$db->insert_query('spiders', array("name" => "CheckHost", "useragent" => "CheckHost (http://check-host.net/)"));
+	$db->insert_query('spiders', array("name" => "Pingdom", "useragent" => "Pingdom.com_bot_version_1.4_(http://www.pingdom.com)"));
+	$db->insert_query('spiders', array("name" => "DuckDuckGo", "useragent" => "DuckDuckBot/1.1; (+http://duckduckgo.com/duckduckbot.html)"));
+	$db->insert_query('spiders', array("name" => "UptimeRobot", "useragent" => "Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)"));
 
-  $output->print_contents("<p>Click next to continue with the upgrade process.</p>");
+  	$output->print_contents("<p>Click next to continue with the upgrade process.</p>");
 	$output->print_footer("41_done");
 }
