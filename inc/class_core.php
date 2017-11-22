359efbbcd09335f7986c269614c2fbc557c864dd
Tomasz Mlynski <devilshakerz@gmail.com>
2017-11-22 20:44:11 +0100

    1.8.14 meta

--- inc/class_core.php
+++ inc/class_core.php
@@ -14,14 +14,14 @@ class MyBB {
 	 *
 	 * @var string
 	 */
-	public $version = "1.8.13";
+	public $version = "1.8.14";
 
 	/**
 	 * The version code of MyBB we're running.
 	 *
 	 * @var integer
 	 */
-	public $version_code = 1813;
+	public $version_code = 1814;
 
 	/**
 	 * The current working directory.
