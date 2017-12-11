935b3113f2ae6943dd319faf18fccab725a67cf6
Euan Torano <euantorano@gmail.com>
2017-12-11 17:00:16 +0000

    Adding Laravel container
    Signed-off-by: Euan Torano <euantorano@gmail.com>

---
+++ inc/src/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MyBB;
+
+use Illuminate\Container\Container;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Get an instance of an interface or class fom the IoC container.
+ *
+ * @param string $className The name of the type to resolve. If this is null or an empty string, the container itself will be returned.
+ * @param array $parameters An optional array of parameters to pass whilst resolving an instance.
+ *
+ * @return ContainerInterface|mixed
+ */
+function app($className = null, array $parameters = [])
+{
+	if (is_null($className) || empty($className)) {
+		return Container::getInstance();
+	}
+
+	return Container::getInstance()->make($className, $parameters);
+}
