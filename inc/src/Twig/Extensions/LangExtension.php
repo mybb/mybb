4b3f6fe71369108151f9ba0dec6450d7f90e6735
Euan T <euantorano@users.noreply.github.com>
2018-01-05 18:05:35 +0000

    Start implementing Twig for the front-end (#2963)
    * Starting work on moving front end to Twig
    
    This marks the start of integrating Twig into the front end. There are two global variables shared with all views - mybb and lang.
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Starting the long process of template translation
    
    Not much to show yet, but this is a first step towards seeing that key variables such as $mybb, $lang and $theme can be accessed.
    
    Need to sort out loading of stylesheets still to make things look decent.
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Changing how language is registered
    
    There is a new LangExtension that provides a `trans` function along with the `lang` global.
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Showing drafts by adding drafts to an array
    
    Moved setting of theme global to theme extension.
    
    Need to fix the use of |raw in some places - we should generate the HTML inside the views themselves instead…
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Finished the majority of work on the drafts page
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Add return type hints
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Fixing trow usage currently following 1.8 standard
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>

---
+++ inc/src/Twig/Extensions/LangExtension.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MyBB\Twig\Extensions;
+
+/**
+ * A Twig extension class to provide functionality related to translations.
+ */
+class LangExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+{
+    /**
+     * @var \MyLanguage $lang
+     */
+    private $lang;
+
+    /**
+     * Create a new instance of the LangExtension.
+     *
+     * @param \MyLanguage $lang
+     */
+    public function __construct(\MyLanguage $lang)
+    {
+        $this->lang = $lang;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('trans', [$this, 'trans']),
+        ];
+    }
+
+    /**
+     * Returns a list of global variables to add to the existing list.
+     *
+     * @return array An array of global variables
+     */
+    public function getGlobals()
+    {
+        return [
+            'lang' => $this->lang,
+        ];
+    }
+
+    /**
+     * Get a translation that takes a number of parameters.
+     *
+     * @param string $languageVariable The name of the language variable to translate.
+     * @param array ...$params A list of parameters to inject into the translation.
+     *
+     * @return string The resolved translation string.
+     */
+    public function trans(string $languageVariable, ...$params) : string
+    {
+        return $this->lang->sprintf($this->lang->$languageVariable, ...$params);
+    }
+}
--- inc/src/bootstrap.php
+++ inc/src/bootstrap.php
@@ -5,6 +5,8 @@ namespace MyBB;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
+use MyBB\Twig\Extensions\LangExtension;
+use MyBB\Twig\Extensions\ThemeExtension;
 use Psr\Container\ContainerInterface;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -12,18 +14,70 @@ require_once __DIR__ . '/../vendor/autoload.php';
 /** @var \Illuminate\Contracts\Container\Container $container */
 $container = app();
 
+// MyBB
+$container->singleton(\MyBB::class, function() {
+    return $GLOBALS['mybb'];
+});
+
+$container->alias(\MyBB::class, 'mybb');
+
+// DB
+$container->singleton(\DB_Base::class, function() {
+    return $GLOBALS['db'];
+});
+
+$container->alias(\DB_Base::class, 'db');
+
+// Plugins
+$container->singleton(\pluginSystem::class, function() {
+    return $GLOBALS['plugins'];
+});
+
+$container->alias(\pluginSystem::class, 'plugins');
+
+// Lang
+$container->singleton(\MyLanguage::class, function() {
+    return $GLOBALS['lang'];
+});
+
+$container->alias(\MyLanguage::class, 'lang');
+
 // Twig
 $container->singleton(\Twig_Environment::class, function(ContainerInterface $container) {
-    // TODO: The loader should be aware of both front-end and ACP themes
-    $loader = new \Twig_Loader_Filesystem([
-        __DIR__ . '/../views'
-    ]);
+    if (defined('IN_ADMINCP')) {
+        $paths = [
+            __DIR__ . '/../views/admin',
+        ];
+    } else {
+        // TODO: views for the current theme, it's parent, it's parent's parent, etc. should be here
+        // The filesystem loader works by using files from the first array entry.
+        // If a file doesn't exist, it looks in the second array entry and so on. This allows us to easily implement template inheritance.
 
-    return new \Twig_Environment($loader, [
-        'debug' => true,
+        $paths = [
+            __DIR__ . '/../views/base',
+        ];
+    }
+
+    /** @var \pluginSystem $plugins */
+    $plugins = $container->get(\pluginSystem::class);
+
+    $plugins->run_hooks('twig_environment_before_loader', $paths);
+
+    $loader = new \Twig_Loader_Filesystem($paths);
+
+    $env = new \Twig_Environment($loader, [
+        'debug' => true, // TODO: In live environments this should be false
         'cache' => __DIR__ . '/../../cache/views',
-
     ]);
+
+    $env->addExtension(new ThemeExtension($container->get(\MyBB::class), $container->get(\DB_Base::class)));
+    $env->addExtension(new LangExtension($container->get(\MyLanguage::class)));
+
+    $plugins->run_hooks('twig_environment_env', $env);
+
+    $env->addGlobal('mybb', $container->get(\MyBB::class));
+
+    return $env;
 });
 
 $container->alias(\Twig_Environment::class, 'twig');
