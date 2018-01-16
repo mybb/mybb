b69cf040b0b7b8916383fe8ce2b34cb6ab79fd73
Matslom <mateosl111@gmail.com>
2018-01-16 17:26:33 +0100

    [FIX] string to array conversation causing Illegal string offset warning on PHP 7.2 (#2995)
    * [FIX] string to array conversation causing Illegal string offset warning
    on PHP 7.2
    
    * Use 4 spaces instead of tab

--- calendar.php
+++ calendar.php
@@ -2251,7 +2251,8 @@ if(!$mybb->input['action'])
     $in_month = 0;
 
     // Iterate weeks (each week gets a row)
-	$week = $day_bit = '';
+    $week = [];
+    $day_bit = [];
     for ($row = 0; $row < 6; ++$row) {
         $days = [];
         foreach ($weekdays as $weekday_id => $weekday) {
