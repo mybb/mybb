9c8d89f1c8c46ceacce7028e8e569461bb794d12
Mark Vincent <admin@rantcentralforums.com>
2018-01-18 16:59:42 -0600

    report_thanks

--- report.php
+++ report.php
@@ -142,8 +142,9 @@ if (empty($error) &&
 
 		$plugins->run_hooks('report_do_report_end');
 
-		eval("\$report_thanks = \"".$templates->get("report_thanks")."\";");
-		echo $report_thanks;
+		echo \MyBB\template('report/report_thanks.twig', [
+			'report' => $report,
+		]);
 		exit;
 	} else {
 		// Bad user!
