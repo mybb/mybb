e7c24133b72d5fbdf1b7198c9158db9b9898b934
PaulBender <paulbender@live.com>
2018-03-16 19:43:50 -0400

    Working on #2972 Template Conversion to Twig Format (showteam.php)

--- showteam.php
+++ showteam.php
@@ -70,139 +70,98 @@ if(!$users_in)
 $forum_permissions = forum_permissions();
 
 $query = $db->simple_select("users", "uid, username, displaygroup, usergroup, ignorelist, hideemail, receivepms, lastactive, lastvisit, invisible, away", "displaygroup IN ($groups_in) OR (displaygroup='0' AND usergroup IN ($groups_in)) OR uid IN ($users_in)", array('order_by' => 'username'));
-while($user = $db->fetch_array($query))
-{
-	// If this user is a moderator
-	if(isset($moderators[$user['uid']]))
-	{
-		foreach($moderators[$user['uid']] as $forum)
-		{
-			if($forum_permissions[$forum['fid']]['canview'] == 1)
-			{
-				$forum_url = get_forum_link($forum['fid']);
-				eval("\$forumlist .= \"".$templates->get("showteam_moderators_forum")."\";");
-			}
-		}
-		$user['forumlist'] = $forumlist;
-		$forumlist = '';
-		$usergroups[6]['user_list'][$user['uid']] = $user;
-	}
+while ($user = $db->fetch_array($query)) {
+    // If this user is a moderator
+    if (isset($moderators[$user['uid']])) {
+        $forumlist = [];
+        foreach ($moderators[$user['uid']] as $forum) {
+            if ($forum_permissions[$forum['fid']]['canview'] == 1) {
+                $forum['url'] = get_forum_link($forum['fid']);
 
-	if($user['displaygroup'] == '6' || $user['usergroup'] == '6')
-	{
-		$usergroups[6]['user_list'][$user['uid']] = $user;
-	}
+                $forumlist[] = $forum;
+            }
+        }
 
-	// Are they also in another group which is being shown on the list?
-	if($user['displaygroup'] != 0)
-	{
-		$group = $user['displaygroup'];
-	}
-	else
-	{
-		$group = $user['usergroup'];
-	}
+        $user['forumlist'] = $forumlist;
+        $usergroups[6]['user_list'][$user['uid']] = $user;
+    }
 
-	if($usergroups[$group] && $group != 6)
-	{
-		$usergroups[$group]['user_list'][$user['uid']] = $user;
-	}
+    if ($user['displaygroup'] == '6' || $user['usergroup'] == '6') {
+        $usergroups[6]['user_list'][$user['uid']] = $user;
+    }
+
+    // Are they also in another group which is being shown on the list?
+    if ($user['displaygroup'] != 0) {
+        $group = $user['displaygroup'];
+    } else {
+        $group = $user['usergroup'];
+    }
+
+    if ($usergroups[$group] && $group != 6) {
+        $usergroups[$group]['user_list'][$user['uid']] = $user;
+    }
 }
 
 // Now we have all of our user details we can display them.
-$grouplist = '';
-foreach($usergroups as $usergroup)
-{
-	$usergrouprows = $modrows = '';
+$usergrouplist = [];
+foreach ($usergroups as $usergroup) {
+    // If we have no users - don't show this group
+    if (!isset($usergroup['user_list'])) {
+        continue;
+    }
 
-	// If we have no users - don't show this group
-	if(!isset($usergroup['user_list']))
-	{
-		continue;
-	}
+    foreach ($usergroup['user_list'] as $user) {
+        $user['username'] = format_name($user['username'], $user['usergroup'], $user['displaygroup']);
+        $user['profilelink'] = get_profile_link($user['uid']);
 
-	$bgcolor = '';
-	foreach($usergroup['user_list'] as $user)
-	{
-		$user['username'] = format_name(htmlspecialchars_uni($user['username']), $user['usergroup'], $user['displaygroup']);
-		$user['profilelink'] = get_profile_link($user['uid']);
+        $user['showemail'] = false;
+        if ($user['hideemail'] != 1 && $mybb->usergroup['canviewwolinvis'] == 1 && my_strpos(",".$user['ignorelist'].",", ",".$mybb->user['uid'].",") === false) {
+            $user['showemail'] = true;
+        }
 
-		// For the postbit templates
-		$post['uid'] = $user['uid'];
-		$emailcode = $pmcode = '';
-		if($user['hideemail'] != 1)
-		{
-			eval("\$emailcode = \"".$templates->get("postbit_email")."\";");
-		}
+        $user['showpm'] = false;
+        if ($user['receivepms'] != 0 && $mybb->settings['enablepms'] != 0 && $mybb->usergroup['canusepms'] != 0 && my_strpos(",".$user['ignorelist'].",", ",".$mybb->user['uid'].",") === false) {
+            $user['showpm'] = true;
+        }
 
-		if($user['receivepms'] != 0 && $mybb->settings['enablepms'] != 0 && my_strpos(",".$user['ignorelist'].",", ",".$mybb->user['uid'].",") === false)
-		{
-			eval("\$pmcode = \"".$templates->get("postbit_pm")."\";");
-		}
+        // For the online image
+        if ($user['lastactive'] > $timecut && ($user['invisible'] == 0 || $mybb->usergroup['canviewwolinvis'] == 1) && $user['lastvisit'] != $user['lastactive']) {
+            $user['status'] = "online";
+            $user['statuslang'] = $lang->online;
+        } else if ($user['away'] == 1 && $mybb->settings['allowaway'] != 0) {
+            $user['status'] = "away";
+            $user['statuslang'] = $lang->away;
+        } else {
+            $user['status'] = "offline";
+            $user['statuslang'] = $lang->offline;
+        }
 
-		// For the online image
-		if($user['lastactive'] > $timecut && ($user['invisible'] == 0 || $mybb->usergroup['canviewwolinvis'] == 1) && $user['lastvisit'] != $user['lastactive'])
-		{
-			$status = "online";
-		}
-		elseif($user['away'] == 1 && $mybb->settings['allowaway'] != 0)
-		{
-			$status = "away";
-		}
-		else
-		{
-			$status = "offline";
-		}
+        if ($user['invisible'] == 1 && $mybb->usergroup['canviewwolinvis'] != 1 && $user['uid'] != $mybb->user['uid']) {
+            if ($user['lastactive']) {
+                $user['lastvisit'] = $lang->lastvisit_hidden;
+            } else {
+                $user['lastvisit'] = $lang->lastvisit_never;
+            }
+        } else {
+            $user['lastvisit'] = my_date('relative', $user['lastactive']);
+        }
 
-		if($user['invisible'] == 1 && $mybb->usergroup['canviewwolinvis'] != 1 && $user['uid'] != $mybb->user['uid'])
-		{
-			if($user['lastactive'])
-			{
-				$user['lastvisit'] = $lang->lastvisit_hidden;
-			}
-			else
-			{
-				$user['lastvisit'] = $lang->lastvisit_never;
-			}
-		}
-		else
-		{
-			$user['lastvisit'] = my_date('relative', $user['lastactive']);
-		}
+        $plugins->run_hooks('showteam_user');
 
-		$bgcolor = alt_trow();
+        $usergroup['users'][] = $user;
+    }
 
-		$plugins->run_hooks('showteam_user');
-
-		// If the current group is a moderator group
-		if($usergroup['gid'] == 6 && !empty($user['forumlist']))
-		{
-			$forumslist = $user['forumlist'];
-			eval("\$modrows .= \"".$templates->get("showteam_moderators_mod")."\";");
-		}
-		else
-		{
-			eval("\$usergrouprows .= \"".$templates->get("showteam_usergroup_user")."\";");
-		}
-	}
-
-	if($modrows && $usergroup['gid'] == 6)
-	{
-		eval("\$grouplist .= \"".$templates->get("showteam_moderators")."\";");
-	}
-
-	if($usergrouprows)
-	{
-		eval("\$grouplist .= \"".$templates->get("showteam_usergroup")."\";");
-	}
+    if ($usergroup['users']) {
+        $usergrouplist[] = $usergroup;
+    }
 }
 
-if(empty($grouplist))
-{
-	error($lang->error_noteamstoshow);
+if (empty($usergrouplist)) {
+    error($lang->error_noteamstoshow);
 }
 
 $plugins->run_hooks("showteam_end");
 
-eval("\$showteam = \"".$templates->get("showteam")."\";");
-output_page($showteam);
+output_page(\MyBB\template('showteam/showteam.twig', [
+    'usergrouplist' => $usergrouplist,
+]));
