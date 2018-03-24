7df112b0cc68be8fedad9971e14e0ce80d02a79c
Euan T <euantorano@users.noreply.github.com>
2018-03-24 20:15:05 +0000

    Add basic unit test setup (#3078)
    * Add basic unit test setup
    
    This configures PHPUnit and PHPSpec, along with PHPCS for the inc/src folder only. It also adds Travis CI.
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Remove PHP 7.0 from Travis build
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Set minimum stability to dev
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- inc/src/functions.php
+++ inc/src/functions.php
@@ -8,18 +8,19 @@ use Psr\Container\ContainerInterface;
 /**
  * Get an instance of a type fom the IoC container.
  *
- * @param string $className The name of the type to resolve. If this is null or an empty string, the container itself will be returned.
+ * @param string $className The name of the type to resolve.
+ * If this is null or an empty string, the container itself will be returned.
  * @param array $parameters An optional array of parameters to pass whilst resolving an instance.
  *
  * @return ContainerInterface|mixed
  */
 function app($className = null, array $parameters = [])
 {
-	if (is_null($className) || empty($className)) {
-		return Container::getInstance();
-	}
+    if (is_null($className) || empty($className)) {
+        return Container::getInstance();
+    }
 
-	return Container::getInstance()->make($className, $parameters);
+    return Container::getInstance()->make($className, $parameters);
 }
 
 /**
---
+++ tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/../inc/vendor/autoload.php';
+
+date_default_timezone_set('UTC');
