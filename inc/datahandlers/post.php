7a60a895cf31f4078f927928c73d4f8bfcb455c4
Eldenroot <778890+Eldenroot@users.noreply.github.com>
2018-04-09 17:41:15 +0200

    Update post.php

--- inc/datahandlers/post.php
+++ inc/datahandlers/post.php
@@ -1606,7 +1606,6 @@ class PostDataHandler extends DataHandler
 				$done_users = array();
 
 				// Queue up any forum subscription notices to users who are subscribed to this forum.
-				$excerpt = my_substr($thread['message'], 0, $mybb->settings['subscribeexcerpt']).$lang->emailbit_viewthread;
 				$excerpt = $thread['message'];
 
 				// Parse badwords
@@ -1615,8 +1614,9 @@ class PostDataHandler extends DataHandler
 				$excerpt = $parser->parse_badwords($excerpt);
 				$excerpt = $parser->text_parse_message($excerpt);
 				if(strlen($excerpt) > $mybb->settings['subscribeexcerpt'])
+				{
 					$excerpt = my_substr($excerpt, 0, $mybb->settings['subscribeexcerpt']).$lang->emailbit_viewthread;
-
+				}
 				$query = $db->query("
 					SELECT u.username, u.email, u.uid, u.language, u.loginkey, u.salt, u.regdate
 					FROM ".TABLE_PREFIX."forumsubscriptions fs
