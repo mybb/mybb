77be84d3c010a47fe17c0f1aeb6ccd6db26b353c
Debabrata Trivedi <me@eff.one>
2018-04-09 16:16:03 +0530

    $percent cleanup
    Hard coding removed, shifted braces & % symbol to languages.

--- usercp.php
+++ usercp.php
@@ -3542,7 +3542,8 @@ if($mybb->input['action'] == "attachments")
 	$friendlyusage = get_friendly_size((int)$totalusage);
 	if($mybb->usergroup['attachquota'])
 	{
-		$percent = "(".round(($totalusage/($mybb->usergroup['attachquota']*1024))*100)."%)";
+		$percent = round(($totalusage/($mybb->usergroup['attachquota']*1024))*100);
+		$percent = $lang->sprintf($lang->attachments_usage_percent, $percent);
 		$attachquota = get_friendly_size($mybb->usergroup['attachquota']*1024);
 		$usagenote = $lang->sprintf($lang->attachments_usage_quota, $friendlyusage, $attachquota, $percent, $totalattachments);
 	}
