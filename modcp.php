e340e4d44b1741cea3afe8d5dce08800aa503d06
Debabrata Trivedi <me@eff.one>
2018-05-01 01:19:13 +0530

    Fixes #3124 XSS and $mybb->input in Templates (#3161)
    * $mybb->input usage drop in template
    
    * Wrap up
    
    * Revert input usage restriction
    
    * Spacing correction

--- modcp.php
+++ modcp.php
@@ -2667,7 +2667,7 @@ if($mybb->input['action'] == "do_editprofile")
 		require_once MYBB_ROOT."inc/functions_warnings.php";
 		foreach($moderator_options as $option)
 		{
-			$mybb->input[$option['time']] = $mybb->get_input($option['time'], MyBB::INPUT_INT);
+			${$option['time']} = $mybb->get_input($option['time'], MyBB::INPUT_INT);
 			$mybb->input[$option['period']] = $mybb->get_input($option['period']);
 			if(empty($mybb->input[$option['action']]))
 			{
@@ -3201,9 +3201,10 @@ if($mybb->input['action'] == "editprofile")
 
 	$suspendsignature_info = $moderateposts_info = $suspendposting_info = '';
 	$action_options = $modpost_options = $suspost_options = '';
+	$modopts = array();
 	foreach($moderator_options as $option)
 	{
-		$mybb->input[$option['time']] = $mybb->get_input($option['time'], MyBB::INPUT_INT);
+		${$option['time']} = $mybb->get_input($option['time'], MyBB::INPUT_INT);
 		// Display the suspension info, if this user has this option suspended
 		if($user[$option['option']])
 		{
@@ -3275,6 +3276,13 @@ if($mybb->input['action'] == "editprofile")
 	{
 		$newtitle = '';
 	}
+
+	$birthday_year = $mybb->input['birthday_year'];
+	$user_website = $mybb->input['website'];
+	$user_icq = $mybb->input['icq'];
+	$user_skype = $mybb->input['skype'];
+	$user_google = $mybb->input['google'];
+	$user_yahoo = $mybb->input['yahoo'];
 
 	$plugins->run_hooks("modcp_editprofile_end");
 
@@ -3422,6 +3430,7 @@ if($mybb->input['action'] == "finduser")
 
 	$plugins->run_hooks("modcp_finduser_end");
 
+	$username = $mybb->get_input('username');
 	eval("\$finduser = \"".$templates->get("modcp_finduser")."\";");
 	output_page($finduser);
 }
@@ -3638,6 +3647,10 @@ if($mybb->input['action'] == "warninglogs")
 	}
 
 	$plugins->run_hooks("modcp_warninglogs_end");
+
+	$filter_username = $mybb->input['filter']['username'];
+	$filter_modusername = $mybb->input['filter']['mod_username'];
+	$filter_reason = $mybb->input['filter']['reason'];
 
 	eval("\$warninglogs = \"".$templates->get("modcp_warninglogs")."\";");
 	output_page($warninglogs);
