8fb8b5d20b16977ed2c54d7bcccc014061c724b6
Debabrata Trivedi <effone@live.in>
2018-06-08 13:23:37 +0530

    localhost check retained

--- inc/functions_serverstats.php
+++ inc/functions_serverstats.php
@@ -205,6 +205,10 @@ function build_server_stats($is_install=1, $prev_version='', $current_version=''
 	// Host URL & hostname
 	// Dropped fetching host details since v.1.8.16 as http://www.whoishostingthis.com API seems to be down and this info is not required by MyBB.
 	$info['hosturl'] = $info['hostname'] = "unknown/local";
+	if($_SERVER['HTTP_HOST'] == 'localhost')
+	{	
+		$info['hosturl'] = $info['hostname'] = "localhost";	
+	}
 
 	if(isset($_SERVER['HTTP_USER_AGENT']))
 	{
