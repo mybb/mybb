16cd6a7575a9d14c1c64825bd5892962ad6bb0d8
Eric Jackson <ericj1992@gmail.com>
2018-06-26 20:40:02 -0400

    Small Formatting Fix

--- jscripts/general.js
+++ jscripts/general.js
@@ -677,18 +677,31 @@ var expandables = {
 };
 
 var tools = {
-	hasClass: function(el, className) {
+	hasClass: function(el, className)
+	{
 		return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
 	},
 	
-	addClass: function(el, className) {
-		if (el.classList) el.classList.add(className);
-		else if (!hasClass(el, className)) el.className += ' ' + className;
+	addClass: function(el, className)
+	{
+		if (el.classList) {
+			el.classList.add(className);
+		}
+		else if (!hasClass(el, className))
+		{
+			el.className += ' ' + className;
+		}
 	},
 	
 	removeClass: function(el, className) {
-		if (el.classList) el.classList.remove(className);
-		else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+		if (el.classList) 
+		{
+			el.classList.remove(className);
+		}
+		else
+		{
+			el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+		}
 	}
 };
 
