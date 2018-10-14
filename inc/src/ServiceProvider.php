c110086da58ab11d8aced16a68a8b4c43b19a94b
Euan T <euantorano@users.noreply.github.com>
2018-10-14 15:40:16 +0100

    Reorganising bootstrap.php, changing how Twig is registered (#3462)
    * Refactoring bootstrap process
    
    Now uses service providers like Laravel, which will also be used in future modules and plugins.
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Fixing code style
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>
    
    * Updating dependencies.
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>

---
+++ inc/src/ServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MyBB;
+
+use MyBB\Utilities\BreadcrumbManager;
+use Psr\Container\ContainerInterface;
+
+/** @property \MyBB\Application $app */
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Registers bindings in the container.
+     */
+    public function register()
+    {
+        $this->app->singleton(\MyBB::class, function () {
+            return $GLOBALS['mybb'];
+        });
+
+        $this->app->singleton(\DB_Base::class, function () {
+            return $GLOBALS['db'];
+        });
+
+        $this->app->singleton(\pluginSystem::class, function () {
+            return $GLOBALS['plugins'];
+        });
+
+        $this->app->singleton(\MyLanguage::class, function () {
+            return $GLOBALS['lang'];
+        });
+
+        $this->app->singleton(BreadcrumbManager::class, function (ContainerInterface $container) {
+            /** @var \MyBB $mybb */
+            $mybb = $container[\MyBB::class];
+
+            return new BreadcrumbManager(
+                $mybb->settings['bbname'],
+                $mybb->settings['bburl']
+            );
+        });
+    }
+}
