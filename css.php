1a6b3f4d81865d572ea180ae28445c8735672ee2
Euan Torano <euantorano@gmail.com>
2018-10-16 20:56:22 +0100

    Add support for loading all css.php sheets at once
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- css.php
+++ css.php
@@ -15,26 +15,43 @@ define('THIS_SCRIPT', 'css.php');
 require_once "./inc/init.php";
 require_once MYBB_ROOT . $config['admin_dir'] . '/inc/functions_themes.php';
 
-$stylesheet = $mybb->get_input('stylesheet', MyBB::INPUT_INT);
+$stylesheets = $mybb->get_input('stylesheet', MyBB::INPUT_ARRAY);
 
-if($stylesheet)
+if(!empty($stylesheets))
 {
-	$options = array(
-		"limit" => 1
-	);
-	$query = $db->simple_select("themestylesheets", "stylesheet", "sid=".$stylesheet, $options);
-	$stylesheet = $db->fetch_field($query, "stylesheet");
+	$stylesheet_list = '';
+	$sep = '';
 
-	$plugins->run_hooks("css_start");
-
-	if(!empty($mybb->settings['minifycss']))
+	foreach($stylesheets as $stylesheet)
 	{
-		$stylesheet = minify_stylesheet($stylesheet);
+		$stylesheet_list .= $sep . (int) $stylesheet;
+		$sep = ', ';
 	}
 
-	$plugins->run_hooks("css_end");
+	$content = '';
 
-	header("Content-type: text/css");
-	echo $stylesheet;
+	$query = $db->simple_select('themestylesheets', 'stylesheet', "sid IN ({$stylesheet_list})", array(
+		'order_by' => 'sid',
+		'order_dir' => 'ASC',
+	));
+
+	while($row = $db->fetch_array($query))
+	{
+		$stylesheet = $row['stylesheet'];
+
+		$plugins->run_hooks('css_start');
+
+		if(!empty($mybb->settings['minifycss']))
+		{
+			$stylesheet = minify_stylesheet($stylesheet);
+		}
+
+		$plugins->run_hooks('css_end');
+
+		$content .= $stylesheet;
+	}
+
+	header('Content-type: text/css');
+	echo $content;
 }
 exit;
