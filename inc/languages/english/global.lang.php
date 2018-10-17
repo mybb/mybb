dff3ca65c10f90e3f2b61891868a07399921459a
Justin Soltesz <justin@mybb.com>
2018-10-16 20:17:31 -0400

    WIP moving post options in dropdown

--- inc/languages/english/global.lang.php
+++ inc/languages/english/global.lang.php
@@ -286,6 +286,7 @@ $l['postbit_post_unapproved'] = "This post is awaiting approval.";
 $l['postbit_thread_deleted'] = "This thread has been deleted.";
 $l['postbit_thread_unapproved'] = "This thread is awaiting approval.";
 $l['postbit_deleted_post_user'] = "This post by {1} has been deleted.";
+$l['postbit_post_options'] = "Post Options";
 
 $l['postbit_button_reputation_add'] = 'Rate';
 $l['postbit_button_website'] = 'Website';
