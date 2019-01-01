09d6ba383274b3e92d5fee7ebcbef8269cfae672
Tomasz Mlynski <devilshakerz@gmail.com>
2019-01-01 16:16:34 +0100

    1.9.20 meta

--- inc/class_core.php
+++ inc/class_core.php
@@ -14,14 +14,14 @@ class MyBB {
 	 *
 	 * @var string
 	 */
-	public $version = "1.8.19";
+	public $version = "1.8.20";
 
 	/**
 	 * The version code of MyBB we're running.
 	 *
 	 * @var integer
 	 */
-	public $version_code = 1819;
+	public $version_code = 1820;
 
 	/**
 	 * The current working directory.
