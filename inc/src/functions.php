d43f750eb54f445515606e4cbecd9492763951c5
Euan Torano <euantorano@gmail.com>
2019-01-02 20:40:44 +0000

    Updating composer dependencies
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- inc/src/functions.php
+++ inc/src/functions.php
@@ -42,3 +42,28 @@ function template(string $name, array $context = [])
 
     return $twig->render($name, $context);
 }
+
+/**
+ * Register the given Twig extension with the Twig environment.
+ *
+ * @param string $className The full name of the extension class to register.
+ * @param array $parameters Any parameters required to construct the given extension class.
+ *
+ * @return \Twig_ExtensionInterface The extension instance.
+ */
+function registerTwigExtension(string $className, array $parameters = []): \Twig_ExtensionInterface
+{
+    /** @var \Twig_Environment $twig */
+    $twig = app(\Twig_Environment::class);
+
+    if (!$twig->hasExtension($className)) {
+        /** @var \Twig\Extension\ExtensionInterface $extension */
+        $extension = app($className, $parameters);
+
+        $twig->addExtension($extension);
+
+        return $extension;
+    }
+
+    return $twig->getExtension($className);
+}
