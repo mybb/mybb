db4a6a09ee7f8765a8c255b57befcaf11a5bab65
Euan Torano <euantorano@gmail.com>
2019-01-05 19:10:55 +0000

    Order stylesheets by sid as passed to css.php.
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- css.php
+++ css.php
@@ -19,21 +19,41 @@ $stylesheets = $mybb->get_input('stylesheet', MyBB::INPUT_ARRAY);
 
 if(!empty($stylesheets))
 {
-	$stylesheet_list = '';
-	$sep = '';
-
-	foreach($stylesheets as $stylesheet)
-	{
-		$stylesheet_list .= $sep . (int) $stylesheet;
-		$sep = ', ';
-	}
+	$stylesheet_list = implode(', ', array_map('intval', $stylesheets));
 
 	$content = '';
 
-	$query = $db->simple_select('themestylesheets', 'stylesheet', "sid IN ({$stylesheet_list})", array(
-		'order_by' => 'sid',
-		'order_dir' => 'ASC',
-	));
+	switch($db->type)
+	{
+		case 'pgsql':
+		case 'sqlite':
+			$sql = <<<SQL
+SELECT stylesheet FROM themestylesheets
+  WHERE sid IN ({$stylesheet_list})
+  ORDER BY CASE sid
+SQL;
+
+			$i = 0;
+			foreach($stylesheets as $sid)
+			{
+				$sid = (int) $sid;
+
+				$sql .= "WHEN {$sid} THEN {$i}\n";
+				$i++;
+			}
+
+			$sql .= 'END;';
+			break;
+		default:
+			$sql = <<<SQL
+SELECT stylesheet FROM themestylesheets
+  WHERE sid IN ({$stylesheet_list})
+  ORDER BY FIELD(sid, {$stylesheet_list});
+SQL;
+			break;
+	}
+
+	$query = $db->query($sql);
 
 	while($row = $db->fetch_array($query))
 	{
