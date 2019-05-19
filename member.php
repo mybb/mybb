133991a34735bc8fc7f735aed7f8de109bd9444e
demtor <9848968+demtor@users.noreply.github.com>
2019-05-19 16:26:34 +0200

    Necessary shift of member_profile_start hook

--- member.php
+++ member.php
@@ -2120,8 +2120,6 @@ if($mybb->input['action'] == "viewnotes")
 
 if($mybb->input['action'] == "profile")
 {
-	$plugins->run_hooks("member_profile_start");
-
 	if($mybb->usergroup['canviewprofiles'] == 0)
 	{
 		error_no_permission();
@@ -2147,6 +2145,8 @@ if($mybb->input['action'] == "profile")
 	}
 
 	$uid = $memprofile['uid'];
+
+	$plugins->run_hooks("member_profile_start");
 
 	$me_username = $memprofile['username'];
 	$memprofile['username'] = htmlspecialchars_uni($memprofile['username']);
