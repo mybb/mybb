104701c38638e54a06186fdf78d90a52230d1831
Tomasz Mlynski <devilshakerz@gmail.com>
2019-05-22 17:52:14 +0200

    Add missing modify_column(), rename_column() implementations

--- inc/db_base.php
+++ inc/db_base.php
@@ -376,8 +376,8 @@ interface DB_Base
 	 * @param string $table The table
 	 * @param string $column The column name
 	 * @param string $new_definition the new column definition
-	 * @param boolean $new_not_null Whether to drop or set a column
-	 * @param boolean $new_default_value The new default value (if one is to be set)
+	 * @param boolean|string $new_not_null Whether to "drop" or "set" the NOT NULL attribute (no change if false)
+	 * @param boolean|string $new_default_value The new default value, or false to drop the attribute
 	 * @return bool Returns true if all queries are executed successfully or false if one of them failed
 	 */
 	function modify_column($table, $column, $new_definition, $new_not_null=false, $new_default_value=false);
@@ -389,8 +389,8 @@ interface DB_Base
 	 * @param string $old_column The old column name
 	 * @param string $new_column the new column name
 	 * @param string $new_definition the new column definition
-	 * @param boolean $new_not_null Whether to drop or set a column
-	 * @param boolean $new_default_value The new default value (if one is to be set)
+	 * @param boolean|string $new_not_null Whether to "drop" or "set" the NOT NULL attribute (no change if false)
+	 * @param boolean|string $new_default_value The new default value, or false to drop the attribute
 	 * @return bool Returns true if all queries are executed successfully
 	 */
 	function rename_column($table, $old_column, $new_column, $new_definition, $new_not_null=false, $new_default_value=false);
