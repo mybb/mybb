bb28b8bf94c8e088c36a1e9ec98f4edb2c1ae317
teklay2108 <49643717+teklay2108@users.noreply.github.com>
2019-05-28 21:43:12 -0900

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * MyBB 1.8
+ * NatnaBB 1.8
  * Copyright 2014 MyBB Group, All Rights Reserved
  *
- * Website: http://www.mybb.com
+ * 
  * License: http://www.mybb.com/about/license
  *
  */
