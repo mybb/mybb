eb9be29fb1f385d26d1380c9a491833bbda6a5ba
Euan Torano <euantorano@gmail.com>
2019-06-11 20:09:14 +0100

    Refactor redirect into a function
    Fix a minor issue noticed in make_parent_list().
    
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- admin/modules/forum/management.php
+++ admin/modules/forum/management.php
@@ -1989,33 +1989,7 @@ if($mybb->input['action'] == "delete")
 
 			echo $form->generate_hidden_field("fid", $fid);
 			echo $form->generate_hidden_field("action", "delete");
-			echo "<div class=\"confirm_action\">\n";
-			echo "<p>{$lang->confirm_proceed_deletion}</p>\n";
-			echo "<br />\n";
-			echo "<script type=\"text/javascript\">
-				$(function() { 
-					var button = $(\"#proceed_button\"); 
-					if (button.length > 0) {
-						// create a temporary div element to render the text within, unescaping HTML entities
-						var textElement = $('<div/>').html('{$lang->automatically_redirecting}');
-					
-						button.val(textElement.text());
-						button.attr(\"disabled\", true);
-						button.css(\"color\", \"#aaa\");
-						button.css(\"borderColor\", \"#aaa\");
-						
-						var parent_form = button.closest('form');
-
-						if (parent_form.length > 0) {
-							parent_form.submit();
-						}
-					}
-				});
-			</script>";
-			echo "<p class=\"buttons\">\n";
-			echo $form->generate_submit_button($lang->proceed, array('class' => 'button_yes', 'id' => 'proceed_button'));
-			echo "</p>\n";
-			echo "</div>\n";
+			output_auto_redirect($form, $lang->confirm_proceed_deletion);
 
 			$form->end();
 
