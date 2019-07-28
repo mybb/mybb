e02b7857cb7d4d414439031deb5dfa3be886be9f
Omniscient <labrocca@users.noreply.github.com>
2019-07-28 06:47:09 -0700

    Poll Voting Efficiency Fix (#3722)
    * Poll Voting Efficiency Fix
    
    Removes a query against the non-indexed `poll` column in the `threads` table in favor of get_thread() using the poll tid.
    
    * Polls Theme Efficiency Fix
    
    Removes a query against the non-indexed `poll` column in the `threads` table in favor of JOINING into the `threads` table from the `polls` table.

--- global.php
+++ global.php
@@ -211,7 +211,7 @@ if(in_array($current_page, $valid))
 	// If we're accessing poll results, fetch the forum theme for it and if we're overriding it
 	elseif(isset($mybb->input['pid']) && THIS_SCRIPT == "polls.php")
 	{
-		$query = $db->simple_select('threads', 'fid', "poll = '{$mybb->input['pid']}'", array('limit' => 1));
+		$query = $db->query("SELECT t.fid FROM ".TABLE_PREFIX."polls p LEFT JOIN ".TABLE_PREFIX."threads t ON (t.tid=p.tid) WHERE p.pid = '{$mybb->input['pid']}' LIMIT 1");
 		$fid = $db->fetch_field($query, 'fid');
 
 		if($fid)
