c0fb8ae9a89cc777ce8a66009178ec8a1d8e5f76
Tomasz Mlynski <devilshakerz@gmail.com>
2019-08-12 14:52:07 +0200

    Limit scope of general mybb_session SELECT queries with isunique = 1

--- forumdisplay.php
+++ forumdisplay.php
@@ -287,7 +287,7 @@ if($mybb->settings['browsingthisforum'] != 0)
 		SELECT s.ip, s.uid, u.username, s.time, u.invisible, u.usergroup, u.usergroup, u.displaygroup
 		FROM ".TABLE_PREFIX."sessions s
 		LEFT JOIN ".TABLE_PREFIX."users u ON (s.uid=u.uid)
-		WHERE s.time > '$timecut' AND location1='$fid' AND nopermission != 1
+		WHERE s.isunique = 1 AND s.time > '$timecut' AND location1='$fid' AND nopermission != 1
 		ORDER BY u.username ASC, s.time DESC
 	");
 
