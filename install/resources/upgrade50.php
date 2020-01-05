1655c614b49b6147ec6cacb1dada4d70773f1c93
Eldenroot <778890+Eldenroot@users.noreply.github.com>
2020-01-05 15:47:32 +0100

    Add upgrade50.php

---
+++ install/resources/upgrade50.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MyBB 1.8
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+/**
+ * Upgrade Script: 1.8.23
+ */
+
+$upgrade_detail = array(
+    "revert_all_templates" => 0,
+    "revert_all_themes" => 0,
+    "revert_all_settings" => 0
+);
+
+@set_time_limit(0);
+
+function upgrade50_dbchanges()
+{
+	global $output, $db;
+
+	$output->print_header("Updating Database");
+	echo "<p>Performing necessary upgrade queries...</p>";
+	flush();
+
+	$db->update_query('profilefields', array(
+		'description' => "REPLACE(description, 'sex', 'gender')",
+	), 'fid = 3');
+
+	$output->print_contents("<p>Click next to continue with the upgrade process.</p>");
+	$output->print_footer("50_done");
+}
