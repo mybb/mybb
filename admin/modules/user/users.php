e9d0344dcd0785896f76c65a998aa1fa064ee167
effone <effone@mybb.com>
2020-04-21 22:57:31 +0530

    ACP Username Display

--- admin/modules/user/users.php
+++ admin/modules/user/users.php
@@ -3901,8 +3901,13 @@ function build_user_view_card($user, $view, &$i)
 	}
 
 	// And build the final card
+	$uname = "";
+	if(in_array('username', $view['fields']))
+	{
+		$uname = $user['view']['username'];
+	}
 	$card = "<fieldset id=\"uid_{$user['uid']}\" style=\"width: 47%; float: {$float};\">\n";
-	$card .= "<legend><input type=\"checkbox\" class=\"checkbox\" name=\"inlinemod_{$user['uid']}\" id=\"inlinemod_{$user['uid']}\" value=\"1\" onclick=\"$('#uid_{$user['uid']}').toggleClass('inline_selected');\" /> {$user['view']['username']}</legend>\n";
+	$card .= "<legend><input type=\"checkbox\" class=\"checkbox\" name=\"inlinemod_{$user['uid']}\" id=\"inlinemod_{$user['uid']}\" value=\"1\" onclick=\"$('#uid_{$user['uid']}').toggleClass('inline_selected');\" /> {$uname}</legend>\n";
 	if($avatar)
 	{
 		$card .= "<div class=\"user_avatar\">{$avatar}</div>\n";
