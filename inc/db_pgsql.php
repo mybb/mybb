0714630240f95e1db95451c88841e7b992c9ee35
Tomasz Mlynski <devilshakerz@gmail.com>
2020-05-06 23:50:23 +0200

    Improve query match pattern in DB_PgSQL::insert_id()

--- inc/db_pgsql.php
+++ inc/db_pgsql.php
@@ -479,9 +479,7 @@ class DB_PgSQL implements DB_Base
 	 */
 	function insert_id()
 	{
-		$this->last_query = str_replace(array("\r", "\t"), '', $this->last_query);
-		$this->last_query = str_replace("\n", ' ', $this->last_query);
-		preg_match('#INSERT INTO ([a-zA-Z0-9_\-]+)#i', $this->last_query, $matches);
+		preg_match('#INSERT\s+INTO\s+([a-zA-Z0-9_\-]+)#i', $this->last_query, $matches);
 
 		$table = $matches[1];
 
