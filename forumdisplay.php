66227c2dce02c842f17e6c7ac2c640f358045e60
Laird <lairdshaw@users.noreply.github.com>
2020-05-11 00:10:22 +1000

    Adds thread start datetime to thread listings in forumdisplay.php. (#3977)
    [Rebased for 1.9 by Laird]
    
    * Adds thread start datetime to thread listings in forumdisplay.php.
    
    * Styles thread start datetime in forumdisplay.php with a class.
    
    * Use $lang->comma instead
    
    Note: the [comma lang entry for English](https://github.com/mybb/mybb/blob/feature/inc/languages/english/global.lang.php#L529) includes a space
    
    Co-authored-by: Euan <euantorano@users.noreply.github.com>

--- forumdisplay.php
+++ forumdisplay.php
@@ -1043,6 +1043,14 @@ if(!empty($threadCache) && is_array($threadCache))
 
 		$plugins->run_hooks('forumdisplay_thread_end');
 
+		if($fpermissions['canviewdeletionnotice'] != 0 && $thread['visible'] == -1 && !is_moderator($fid, "canviewdeleted"))
+		{
+		}
+		else
+		{
+			$thread['start_datetime'] = my_date('relative', $thread['dateline']);
+		}
+
 		$threadCache[$k] = $thread;
 	}
 
