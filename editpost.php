4614bde509193d669dd254b1f5c5c3bdb2f70105
Ben <10902901+Ben-MyBB@users.noreply.github.com>
2020-05-26 17:48:47 +0100

    update logic

--- editpost.php
+++ editpost.php
@@ -579,18 +579,11 @@ if(!$mybb->input['action'] || $mybb->input['action'] == "editpost")
 	$mybb->user['username'] = htmlspecialchars_uni($mybb->user['username']);
 	eval("\$loginbox = \"".$templates->get("changeuserbox")."\";");
 
-	if($thread['firstpost'] == $pid) {
-		if($post['visible'] != -1 && (is_moderator($fid, "candeletethreads") || $forumpermissions['candeletethreads'] == 1 && $mybb->user['uid'] == $post['uid']))
-		{
-			eval("\$deletebox = \"".$templates->get("editpost_delete")."\";");
-		}
-	} else {
-		// Can we delete posts?
-		if($post['visible'] != -1 && (is_moderator($fid, "candeleteposts") || $forumpermissions['candeleteposts'] == 1 && $mybb->user['uid'] == $post['uid']))
-		{
-			eval("\$deletebox = \"".$templates->get("editpost_delete")."\";");
-		}
-	}
+	$deletebox = '';
+	if(($thread['firstpost'] == $pid && $post['visible'] != -1 && (is_moderator($fid, "candeletethreads")) || ($forumpermissions['candeletethreads'] == 1 && $mybb->user['uid'] == $post['uid'] || $post['visible'] != -1 && (is_moderator($fid, "candeleteposts") || $forumpermissions['candeleteposts'] == 1 && $mybb->user['uid'] == $post['uid'])) {
+
+        eval("\$deletebox = \"".$templates->get("editpost_delete")."\";");
+    }
 
 	$bgcolor = "trow1";
 	if($mybb->settings['enableattachments'] != 0 && $forumpermissions['canpostattachments'] != 0)
