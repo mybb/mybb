e33d62972d08154646327644c37f9d0a34a9eaa7
Ben <10902901+Ben-MyBB@users.noreply.github.com>
2020-05-26 20:10:18 +0100

    Resolves #3781 - Long avatar URL causes MySQL error (#4011)
    [Rebased for 1.9 by Laird]
    
    * Inital changes
    
    * formatting
    
    * language file update
    
    adding a missing fullstop in another language string also

--- usercp.php
+++ usercp.php
@@ -1844,6 +1844,12 @@ if($mybb->input['action'] == "do_avatar" && $mybb->request_method == "post")
 				}
 			}
 
+			// Limiting URL string to stay within database limit
+			if(strlen($mybb->input['avatarurl']) > 200)
+			{
+				$error = $lang->error_avatarurltoolong;
+			}
+
 			if(empty($error))
 			{
 				if($width > 0 && $height > 0)
