f317e37b062e08ccb26a93180d57d6d84a6c0150
Ben <10902901+Ben-MyBB@users.noreply.github.com>
2020-05-28 11:15:16 +0100

    Apply suggestions from code review
    Co-authored-by: Euan <euantorano@users.noreply.github.com>

--- usercp.php
+++ usercp.php
@@ -243,21 +243,23 @@ if($mybb->input['action'] == "do_profile" && $mybb->request_method == "post")
 		}
 
 		if($cfield == 'icq')
-        {
-            $user[$cfield] = $mybb->get_input($cfield, 1);
-            if(my_strlen($user[$cfield]) > 10)
-            {
-                error($lang->contact_field_icqerror);
-            }
-        }
-        else
-        {
-            $user[$cfield] = $mybb->get_input($cfield);
-            if(my_strlen($user[$cfield]) > 75)
-            {
-                error($lang->contact_field_error);
-            }
-        }
+		{
+			$user[$cfield] = $mybb->get_input($cfield, 1);
+
+			if(my_strlen($user[$cfield]) > 10)
+			{
+				error($lang->contact_field_icqerror);
+			}
+		}
+		else
+		{
+			$user[$cfield] = $mybb->get_input($cfield);
+
+			if(my_strlen($user[$cfield]) > 75)
+			{
+				error($lang->contact_field_error);
+			}
+		}
 	}
 
 	if($mybb->usergroup['canchangewebsite'] == 1)
