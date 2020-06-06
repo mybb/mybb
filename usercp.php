98c2ead0ef5cf5a7c7aafaa1069e13b765b114e8
Ben <10902901+Ben-MyBB@users.noreply.github.com>
2020-06-06 15:46:43 +0100

    Update usercp.php
    Co-authored-by: Jordan Mussi <JordanMussi@users.noreply.github.com>

--- usercp.php
+++ usercp.php
@@ -2356,6 +2356,7 @@ if($mybb->input['action'] == "editsig")
 		{
 			$codebuttons = build_mycode_inserter("signature");
 		}
+
 		$plugins->run_hooks("usercp_editsig_end");
 
 		eval("\$editsig = \"".$templates->get("usercp_editsig")."\";");
