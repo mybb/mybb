78e5f02f0a7f1cdfabf04407f830364852caf9e8
Debabrata Trivedi <effone@mybb.com>
2020-06-09 20:29:52 +0530

    Update install/resources/upgrade50.php
    Co-authored-by: Jordan Mussi <JordanMussi@users.noreply.github.com>

--- install/resources/upgrade50.php
+++ install/resources/upgrade50.php
@@ -30,7 +30,17 @@ function upgrade50_dbchanges()
 
 	$cache->delete("banned");
 	
-	$db->update_query('users', array('pmfolders' => "0**$%%$1**$%%$2**$%%$3**$%%$4**"), "pmfolders NOT LIKE '0%'");
+	$db->update_query('users', array('pmfolders' => "0**$%%$1**$%%$2**$%%$3**$%%$4**"), "pmfolders = ''");
+	switch($db->type)
+	{
+		case "pgsql":
+		case "sqlite":
+			$update = "'0**$%%$' || pmfolders";
+			break;
+		default:
+			$update = "CONCAT('0**$%%$', pmfolders)";
+	}
+	$db->write_query("UPDATE ".TABLE_PREFIX."users SET pmfolders=".$update." WHERE pmfolders NOT LIKE '0%'");
 	$db->update_query('settings', array('value' => 1), "name='nocacheheaders'");
 
 	// Add hCaptcha support
