f9d2108e614629c377e8de924ae784e10bc57668
Debabrata Trivedi <me@eff.one>
2020-06-28 22:09:12 +0530

    Fixes #4054 Default PM Folder Structure (#4056)
    [Rebased for 1.9 by Laird]
    
    Co-authored-by: Jordan Mussi <JordanMussi@users.noreply.github.com>

--- inc/datahandlers/user.php
+++ inc/datahandlers/user.php
@@ -1149,7 +1149,7 @@ class UserDataHandler extends DataHandler
 			"referrals" => 0,
 			"buddylist" => '',
 			"ignorelist" => '',
-			"pmfolders" => '',
+			"pmfolders" => "0**$%%$1**$%%$2**$%%$3**$%%$4**",
 			"notepad" => '',
 			"warningpoints" => 0,
 			"moderateposts" => 0,
