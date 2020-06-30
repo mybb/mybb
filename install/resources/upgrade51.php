1818230f5f7ba73542b361ec32225331515bb2f8
Ben <10902901+Ben-MyBB@users.noreply.github.com>
2020-06-30 20:38:29 +0100

    Fix indenting

--- install/resources/upgrade51.php
+++ install/resources/upgrade51.php
@@ -12,10 +12,10 @@
  * Upgrade Script: 1.8.24
  */
 
-$upgrade_detail = array(
-    "revert_all_templates" => 0,
-    "revert_all_themes" => 0,
-    "revert_all_settings" => 0
+ $upgrade_detail = array(
+     "revert_all_templates" => 0,
+     "revert_all_themes" => 0,
+     "revert_all_settings" => 0
 );
 
 @set_time_limit(0);
@@ -23,12 +23,12 @@ $upgrade_detail = array(
 function upgrade51_dbchanges()
 {
     global $output, $cache, $db, $mybb;
-
+    
     $output->print_header("Updating Database");
-
+    
     echo "<p>Performing necessary upgrade queries...</p>";
     flush();
-
+    
     switch($db->type)
 	{
         // Add new setting for new usergroup permission if group members can hide online status
