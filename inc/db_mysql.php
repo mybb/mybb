e8e76991a3e8189b3e1b31fbe5ef9576972f99ef
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2020-07-05 21:05:24 +0200

    escape_string_like(): escape the '\' escape character (#4108)

--- inc/db_mysql.php
+++ inc/db_mysql.php
@@ -1045,7 +1045,7 @@ class DB_MySQL implements DB_Base
 	 */
 	function escape_string_like($string)
 	{
-		return $this->escape_string(str_replace(array('%', '_') , array('\\%' , '\\_') , $string));
+		return $this->escape_string(str_replace(array('\\', '%', '_') , array('\\\\', '\\%' , '\\_') , $string));
 	}
 
 	/**
