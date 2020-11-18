beaecfa0f6e4a437492125e421fb205dc7c5f031
Omar Gonzalez <Sama34@users.noreply.github.com>
2020-11-17 16:14:08 -0800

    Fix #4167 Repeated code in newreply.php

--- newreply.php
+++ newreply.php
@@ -273,6 +273,11 @@ if($mybb->usergroup['maxposts'] > 0)
 	}
 }
 
+if(!$mybb->settings['postsperpage'] || (int)$mybb->settings['postsperpage'] < 1)
+{
+	$mybb->settings['postsperpage'] = 20;
+}
+
 if($mybb->input['action'] == "do_newreply" && $mybb->request_method == "post")
 {
 	// Verify incoming POST request
@@ -586,11 +591,6 @@ if($mybb->input['action'] == "do_newreply" && $mybb->request_method == "post")
 					{
 						redirect(get_thread_link($tid, 0, "lastpost"));
 					}
-				}
-
-				if(!$mybb->settings['postsperpage'] || (int)$mybb->settings['postsperpage'] < 1)
-				{
-					$mybb->settings['postsperpage'] = 20;
 				}
 
 				// Lets see if this post is on the same page as the one we're viewing or not
@@ -1202,11 +1202,6 @@ if($mybb->input['action'] == "newreply" || $mybb->input['action'] == "editdraft"
 	$reviewmore = '';
 	if($mybb->settings['threadreview'] != 0)
 	{
-		if(!$mybb->settings['postsperpage'] || (int)$mybb->settings['postsperpage'] < 1)
-		{
-			$mybb->settings['postsperpage'] = 20;
-		}
-
 		if(is_moderator($fid, "canviewunapprove") || $mybb->settings['showownunapproved'])
 		{
 			$visibility = "(visible='1' OR visible='0')";
@@ -1217,11 +1212,6 @@ if($mybb->input['action'] == "newreply" || $mybb->input['action'] == "editdraft"
 		}
 		$query = $db->simple_select("posts", "COUNT(pid) AS post_count", "tid='{$tid}' AND {$visibility}");
 		$numposts = $db->fetch_field($query, "post_count");
-
-		if(!$mybb->settings['postsperpage'] || (int)$mybb->settings['postsperpage'] < 1)
-		{
-			$mybb->settings['postsperpage'] = 20;
-		}
 
 		if($numposts > $mybb->settings['postsperpage'])
 		{
