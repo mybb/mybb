ad166fd51f3128a6029b491619dc02dfd1d57157
Euan Torano <euan@torano.co.uk>
2020-12-20 19:57:22 +0000

    Add GitHub workflow to lint PHP files for syntax errors. Fix a missing closing brace in upgrade52.php found using this workflow, along with warning si upgrade30.php found using the same lint.

--- install/resources/upgrade52.php
+++ install/resources/upgrade52.php
@@ -22,21 +22,22 @@
 
 function upgrade51_dbchanges()
 {
-    global $output, $cache, $db, $mybb;
-    
-    $output->print_header("Updating Database");
-    
-    echo "<p>Performing necessary upgrade queries...</p>";
-    flush();
-    
-    switch($db->type)
-	{
-        // Add new setting for new usergroup permission if group members can hide online status
-        case "pgsql":
-            $db->add_column("usergroups", "canbeinvisible", "smallint NOT NULL default '1' AFTER canusercp");
-            break;
+	global $output, $cache, $db, $mybb;
 
-        default:
-            $db->add_column("usergroups", "canbeinvisible", "tinyint(1) NOT NULL default '1' AFTER canusercp");
-            break;
-    }
+	$output->print_header("Updating Database");
+
+	echo "<p>Performing necessary upgrade queries...</p>";
+	flush();
+
+	switch($db->type)
+	{
+		// Add new setting for new usergroup permission if group members can hide online status
+		case "pgsql":
+			$db->add_column("usergroups", "canbeinvisible", "smallint NOT NULL default '1' AFTER canusercp");
+			break;
+
+		default:
+			$db->add_column("usergroups", "canbeinvisible", "tinyint(1) NOT NULL default '1' AFTER canusercp");
+			break;
+	}
+}
