c96852b96b57322e1747ddbdae9f5db628d8bada
Mathias Mogensen <42929161+Xazin@users.noreply.github.com>
2020-12-29 17:13:20 +0100

    Normalization of (") usage

--- inc/languages/english/datahandler_post.lang.php
+++ inc/languages/english/datahandler_post.lang.php
@@ -5,23 +5,23 @@
  *
  */
 
-$l['postdata_invalid_user_id'] = 'The user id does not exist. Please supply a valid user id.';
-$l['postdata_firstpost_no_subject'] = 'The thread does not have a subject. Please enter a subject.';
-$l['postdata_missing_subject'] = 'The subject is missing. Please enter a subject.';
-$l['postdata_missing_message'] = 'The message is missing. Please enter a message.';
-$l['postdata_message_too_long'] = 'The message is too long. Please enter a message shorter than {1} characters (currently {2}).';
-$l['postdata_message_too_short'] = 'The message is too short. Please enter a message longer than {1} characters.';
-$l['postdata_subject_too_long'] = 'The subject is too long. Please enter a subject shorter than 85 characters (currently {1}).';
-$l['postdata_banned_username'] = 'The username you entered may not be registered. Please enter a different username.';
-$l['postdata_bad_characters_username'] = 'The username you entered contains bad characters. Please enter a different username.';
-$l['postdata_invalid_username_length'] = 'The username you entered is of invalid length. Please enter a username between {1} and {2} characters.';
-$l['postdata_username_exists'] = 'The username you entered already exists. Please enter a different username.';
-$l['postdata_post_flooding'] = 'You are trying to post a message too quickly after posting a previous message. Please wait {1} more seconds.';
-$l['postdata_post_flooding_one_second'] = 'You are trying to post a message too quickly after posting a previous message. Please wait 1 more second.';
-$l['postdata_too_many_images'] = 'The message you entered contained {1} images, while only {2} images per message are allowed. Please lower the image count in your message to meet the limit.';
-$l['postdata_too_many_videos'] = 'The message you entered contained {1} videos, while only {2} videos per message are allowed. Please lower the video count in your message to meet the limit.';
-$l['postdata_invalid_prefix'] = 'The selected prefix is invalid. Please select a valid prefix.';
-$l['postdata_require_prefix'] = 'This forum requires a thread prefix to be set. Please select a valid prefix.';
+$l['postdata_invalid_user_id'] = "The user id does not exist. Please supply a valid user id.";
+$l['postdata_firstpost_no_subject'] = "The thread does not have a subject. Please enter a subject.";
+$l['postdata_missing_subject'] = "The subject is missing. Please enter a subject.";
+$l['postdata_missing_message'] = "The message is missing. Please enter a message.";
+$l['postdata_message_too_long'] = "The message is too long. Please enter a message shorter than {1} characters (currently {2}).";
+$l['postdata_message_too_short'] = "The message is too short. Please enter a message longer than {1} characters.";
+$l['postdata_subject_too_long'] = "The subject is too long. Please enter a subject shorter than 85 characters (currently {1}).";
+$l['postdata_banned_username'] = "The username you entered may not be registered. Please enter a different username.";
+$l['postdata_bad_characters_username'] = "The username you entered contains bad characters. Please enter a different username.";
+$l['postdata_invalid_username_length'] = "The username you entered is of invalid length. Please enter a username between {1} and {2} characters.";
+$l['postdata_username_exists'] = "The username you entered already exists. Please enter a different username.";
+$l['postdata_post_flooding'] = "You are trying to post a message too quickly after posting a previous message. Please wait {1} more seconds.";
+$l['postdata_post_flooding_one_second'] = "You are trying to post a message too quickly after posting a previous message. Please wait 1 more second.";
+$l['postdata_too_many_images'] = "The message you entered contained {1} images, while only {2} images per message are allowed. Please lower the image count in your message to meet the limit.";
+$l['postdata_too_many_videos'] = "The message you entered contained {1} videos, while only {2} videos per message are allowed. Please lower the video count in your message to meet the limit.";
+$l['postdata_invalid_prefix'] = "The selected prefix is invalid. Please select a valid prefix.";
+$l['postdata_require_prefix'] = "This forum requires a thread prefix to be set. Please select a valid prefix.";
 
 $l['thread_closed'] = "Thread Closed";
 $l['thread_opened'] = "Thread Opened";
