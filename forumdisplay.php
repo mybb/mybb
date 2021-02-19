cf079e7916712b3096512f0f8d96fef47f427089
Tomasz Mlynski <devilshakerz@gmail.com>
2021-02-19 16:03:30 +0100

    Use forums table counters for calculating thread count in forum view

--- forumdisplay.php
+++ forumdisplay.php
@@ -515,8 +515,39 @@ if(isset($fpermissions['canonlyviewownthreads']) && $fpermissions['canonlyviewow
 if($fpermissions['canviewthreads'] != 0)
 {
 	// How many threads are there?
-	$query = $db->simple_select("threads t", "COUNT(tid) AS threads", "fid = '$fid' $tuseronly $tvisibleonly $datecutsql2 $prefixsql2");
-	$threadcount = $db->fetch_field($query, "threads");
+	if ($useronly === "" && $datecutsql === "" && $prefixsql === "")
+	{
+		$threadcount = 0;
+
+		$query = $db->simple_select("forums", "threads, unapprovedthreads, deletedthreads", "fid=".(int)$fid);
+		$forum_threads = $db->fetch_array($query);
+
+		if(in_array(1, $visible_states))
+		{
+			$threadcount += $forum_threads['threads'];
+		}
+
+		if(in_array(-1, $visible_states))
+		{
+			$threadcount += $forum_threads['deletedthreads'];
+		}
+
+		if(in_array(0, $visible_states))
+		{
+			$threadcount += $forum_threads['unapprovedthreads'];
+		}
+		elseif($mybb->user['uid'] && $mybb->settings['showownunapproved'])
+		{
+			$query = $db->simple_select("threads t", "COUNT(tid) AS threads", "fid = '$fid' AND t.visible=0 AND t.uid=".(int)$mybb->user['uid']);
+			$threadcount += $db->fetch_field($query, "threads");
+		}
+	}
+	else
+	{
+		$query = $db->simple_select("threads t", "COUNT(tid) AS threads", "fid = '$fid' $tuseronly $tvisibleonly $datecutsql2 $prefixsql2");
+
+		$threadcount = $db->fetch_field($query, "threads");
+	}
 }
 
 $threadcount = (int)$threadcount;
