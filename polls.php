aa415f08bce01f95a8319b707bb18eb67833f4c1
Tomasz Mlynski <devilshakerz@gmail.com>
2021-03-10 17:23:08 +0100

    Fix Poll vote count SQL injection

--- polls.php
+++ polls.php
@@ -690,7 +690,7 @@ if($mybb->input['action'] == "do_editpoll" && $mybb->request_method == "post")
 			{
 				$votes[$i] = "0";
 			}
-			$voteslist .= $votes[$i];
+			$voteslist .= (int)$votes[$i];
 			$numvotes = (int)$numvotes + (int)$votes[$i];
 		}
 	}
