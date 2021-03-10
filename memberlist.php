de0a3fed43bdd2cab2502bd58ca484dbaaa2ed98
Laird Shaw <lairdshaw77@gmail.com>
2021-03-10 16:35:44 +1000

    Fix #4294: Searching for members with underscores in their name via memberlist.php fails
    Fix #4294.

--- memberlist.php
+++ memberlist.php
@@ -196,7 +196,8 @@ else
 		// Exact
 		else
 		{
-			$search_query .= " AND u.username='{$username_like_query}'";
+			$username_esc = $db->escape_string($search_username); 
+			$search_query .= " AND u.username='{$username_esc}'";
 		}
 
 		$memberlist['search_url'] .= "&username=".urlencode($memberlist['username']);
