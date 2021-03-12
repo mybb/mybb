2e82a33a7052655f420ea876bc5d06a384f49789
dvz <devilshakerz@gmail.com>
2021-03-12 18:24:30 +0100

    1.8.27 meta

--- inc/languages/english.php
+++ inc/languages/english.php
@@ -27,7 +27,7 @@ $langinfo['website'] = "https://mybb.com/";
 //$langinfo['additional_name'] = " ";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1824";
+$langinfo['version'] = "1827";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
