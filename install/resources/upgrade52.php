3a16ec10bb0f2a458da150a59f83404c588a312e
dvz <devilshakerz@gmail.com>
2021-05-10 21:47:42 +0200

    Add field_exists check for new fields in upgrade52.php (#4364)

--- install/resources/upgrade52.php
+++ install/resources/upgrade52.php
@@ -29,18 +29,34 @@ function upgrade51_dbchanges()
 	echo "<p>Performing necessary upgrade queries...</p>";
 	flush();
 
-	switch($db->type)
+	// Add new setting for new usergroup permission if group members can hide online status
+	if(!$db->field_exists('canbeinvisible', 'usergroups'))
 	{
-		// Add new settings for (a) new usergroup permission if group members can hide online status & (b) attachments force download
-		case "pgsql":
-			$db->add_column("usergroups", "canbeinvisible", "smallint NOT NULL default '1' AFTER canusercp");
-			$db->add_column("attachtypes", "forcedownload", "smallint NOT NULL default '0' AFTER enabled");
-			break;
+		switch($db->type)
+		{
+			case "pgsql":
+				$db->add_column("usergroups", "canbeinvisible", "smallint NOT NULL default '1' AFTER canusercp");
+				break;
 
-		default:
-			$db->add_column("usergroups", "canbeinvisible", "tinyint(1) NOT NULL default '1' AFTER canusercp");
-			$db->add_column("attachtypes", "forcedownload", "tinyint(1) NOT NULL default '0' AFTER enabled");
-			break;
+			default:
+				$db->add_column("usergroups", "canbeinvisible", "tinyint(1) NOT NULL default '1' AFTER canusercp");
+				break;
+		}
+	}
+
+	// Add new setting for attachments force download
+	if(!$db->field_exists('forcedownload', 'attachtypes'))
+	{
+		switch($db->type)
+		{
+			case "pgsql":
+				$db->add_column("attachtypes", "forcedownload", "smallint NOT NULL default '0' AFTER enabled");
+				break;
+
+			default:
+				$db->add_column("attachtypes", "forcedownload", "tinyint(1) NOT NULL default '0' AFTER enabled");
+				break;
+		}
 	}
 
 	// Change default values for these settings for the guest group only
