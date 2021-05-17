mybb/mybb

commit 634257842da94abc2fadcf0ac628b273fc33ade9

Fix "undefined" and type issues (#4348)

inc/datahandlers/post.php
@@ -279,7 +279,24 @@ class PostDataHandler extends DataHandler
 
 			if($limit > 0 || $dblimit > 0)
 			{
-				$is_moderator = is_moderator($post['fid'], "", $post['uid']);
+				if(isset($post['fid']))
+				{
+					$fid = $post['fid'];
+				}
+				else
+				{
+					$fid = 0;
+				}
+				if(isset($post['uid']))
+				{
+					$uid = $post['uid'];
+				}
+				else
+				{
+					$uid = 0;
+				}
+
+				$is_moderator = is_moderator($fid, "", $uid);
 				// Consider minimum in user defined and database limit other than 0
 				if($limit > 0 && $dblimit > 0)
 				{
@@ -423,7 +440,7 @@ class PostDataHandler extends DataHandler
 		$thread = $db->fetch_array($query);
 
 		// Check to see if the same author has posted within the merge post time limit
-		if(((int)$mybb->settings['postmergemins'] != 0 && trim($mybb->settings['postmergemins']) != "") && (TIME_NOW-$thread['lastpost']) > ((int)$mybb->settings['postmergemins']*60))
+		if($thread && ((int)$mybb->settings['postmergemins'] != 0 && trim($mybb->settings['postmergemins']) != "") && (TIME_NOW-$thread['lastpost']) > ((int)$mybb->settings['postmergemins']*60))
 		{
 			return true;
 		}
@@ -456,7 +473,7 @@ class PostDataHandler extends DataHandler
 			return false;
 		}
 
-		if($post['uid'])
+		if(!empty($post['uid']))
 		{
 			$user_check = "uid='".$post['uid']."'";
 		}
@@ -480,8 +497,17 @@ class PostDataHandler extends DataHandler
 
 		$post = &$this->data;
 
+		if(isset($post['uid']))
+		{
+			$uid = $post['uid'];
+		}
+		else
+		{
+			$uid = null;
+		}
+
 		// Get the permissions of the user who is making this post or thread
-		$permissions = user_permissions($post['uid']);
+		$permissions = user_permissions($uid);
 
 		// Fetch the forum this post is being made in
 		if(!$post['fid'])
@@ -506,7 +532,7 @@ class PostDataHandler extends DataHandler
 				"filter_badwords" => 1
 			);
 
-			if($post['options']['disablesmilies'] != 1)
+			if(empty($post['options']['disablesmilies']))
 			{
 				$parser_options['allow_smilies'] = $forum['allowsmilies'];
 			}
@@ -541,8 +567,17 @@ class PostDataHandler extends DataHandler
 
 		$post = &$this->data;
 
+		if(isset($post['uid']))
+		{
+			$uid = $post['uid'];
+		}
+		else
+		{
+			$uid = null;
+		}
+
 		// Get the permissions of the user who is making this post or thread
-		$permissions = user_permissions($post['uid']);
+		$permissions = user_permissions($uid);
 
 		// Check if this post contains more videos than the forum allows
 		if((!isset($post['savedraft']) || $post['savedraft'] != 1) && $mybb->settings['maxpostvideos'] != 0 && $permissions['cancp'] != 1)
@@ -1828,11 +1863,20 @@ class PostDataHandler extends DataHandler
 		$post['tid'] = $existing_post['tid'];
 		$post['fid'] = $existing_post['fid'];
 
+		if(isset($post['uid']))
+		{
+			$uid = $post['uid'];
+		}
+		else
+		{
+			$uid = 0;
+		}
+
 		$forum = get_forum($post['fid']);
-		$forumpermissions = forum_permissions($post['fid'], $post['uid']);
+		$forumpermissions = forum_permissions($post['fid'], $uid);
 
 		// Decide on the visibility of this post.
-		$ismod = is_moderator($post['fid'], "", $post['uid']);
+		$ismod = is_moderator($post['fid'], "", $uid);
 
 		// Keep visibility for unapproved and deleted posts
 		if($existing_post['visible'] == 0)
@@ -1948,7 +1992,7 @@ class PostDataHandler extends DataHandler
 		$db->update_query("posts", $this->post_update_data, "pid='".(int)$post['pid']."'");
 
 		// Automatic subscription to the thread
-		if($post['options']['subscriptionmethod'] != "" && $post['uid'] > 0)
+		if($post && !empty($post['options']['subscriptionmethod']) && $uid > 0)
 		{
 			switch($post['options']['subscriptionmethod'])
 			{
@@ -1966,7 +2010,7 @@ class PostDataHandler extends DataHandler
 		}
 		else
 		{
-			$db->delete_query("threadsubscriptions", "uid='".(int)$post['uid']."' AND tid='".(int)$post['tid']."'");
+			$db->delete_query("threadsubscriptions", "uid='".(int)$uid."' AND tid='".(int)$post['tid']."'");
 		}
 
 		update_forum_lastpost($post['fid']);

admin/modules/config/settings.php
@@ -968,19 +968,29 @@ if($mybb->input['action'] == "change")
 		// Validate minnamelength, maxnamelength, minpasswordlength (complex and regular) and maxpasswordlength
 		if ($gid == 9)
 		{
-			if ($mybb->input['upsetting']['minnamelength'] > 0 && $mybb->input['upsetting']['maxnamelength'] > 0 && $mybb->input['upsetting']['minnamelength'] > $mybb->input['upsetting']['maxnamelength'])
+			if (
+				isset($mybb->input['upsetting']['minnamelength'], $mybb->input['upsetting']['maxnamelength']) &&
+				$mybb->input['upsetting']['minnamelength'] > 0 && $mybb->input['upsetting']['maxnamelength'] > 0 &&
+				$mybb->input['upsetting']['minnamelength'] > $mybb->input['upsetting']['maxnamelength'])
 			{
 				flash_message($lang->error_field_minnamelength, 'error');
 				admin_redirect("index.php?module=config-settings&action=change&gid=".$gid);
 			}
 
-			if ($mybb->input['upsetting']['minpasswordlength'] > 0 && $mybb->input['upsetting']['maxpasswordlength'] > 0 && $mybb->input['upsetting']['minpasswordlength'] > $mybb->input['upsetting']['maxpasswordlength'])
+			if (
+				isset($mybb->input['upsetting']['minpasswordlength'], $mybb->input['upsetting']['maxpasswordlength']) &&
+				$mybb->input['upsetting']['minpasswordlength'] > 0 && $mybb->input['upsetting']['maxpasswordlength'] > 0 &&
+				$mybb->input['upsetting']['minpasswordlength'] > $mybb->input['upsetting']['maxpasswordlength']
+			)
 			{
 				flash_message($lang->error_field_minpasswordlength, 'error');
 				admin_redirect("index.php?module=config-settings&action=change&gid=".$gid);
 			}
 
-			if ($mybb->input['upsetting']['requirecomplexpasswords'] && $mybb->input['upsetting']['minpasswordlength'] < 3)
+			if (
+				isset($mybb->input['upsetting']['minpasswordlength'], $mybb->input['upsetting']['requirecomplexpasswords']) &&
+				$mybb->input['upsetting']['requirecomplexpasswords'] && $mybb->input['upsetting']['minpasswordlength'] < 3
+			)
 			{
 				flash_message($lang->error_field_minpasswordlength_complex, 'error');
 				admin_redirect("index.php?module=config-settings&action=change&gid=".$gid);
@@ -991,11 +1001,19 @@ if($mybb->input['action'] == "change")
 		
 		// Have we opted for a reCAPTCHA or hCaptcha and not set a public/private key in input?
 		$set_captcha_image = false;
-		if(isset($mybb->input['upsetting']['captchaimage']))
+		if(isset(
+			$mybb->input['upsetting']['captchaimage'],
+			$mybb->input['upsetting']['recaptchaprivatekey'],
+			$mybb->input['upsetting']['recaptchapublickey'],
+			$mybb->input['upsetting']['recaptchascore'],
+			$mybb->input['upsetting']['hcaptchaprivatekey'],
+			$mybb->input['upsetting']['hcaptchapublickey']
+		))
 		{
 			$captchaimage = $mybb->input['upsetting']['captchaimage'];
 			$recaptchaprivatekey = $mybb->input['upsetting']['recaptchaprivatekey'];
 			$recaptchapublickey = $mybb->input['upsetting']['recaptchapublickey'];
+			$recaptchascore = $mybb->input['upsetting']['recaptchascore'];
 			$hcaptchaprivatekey = $mybb->input['upsetting']['hcaptchaprivatekey'];
 			$hcaptchapublickey = $mybb->input['upsetting']['hcaptchapublickey'];
 
@@ -1118,7 +1136,7 @@ if($mybb->input['action'] == "change")
 		}
 
 		// Administrator is changing the login method.
-		if((int)$mybb->input['upsetting']['username_method'] > 0)
+		if(isset($mybb->input['upsetting']['username_method']) && (int)$mybb->input['upsetting']['username_method'] > 0)
 		{
 			if((int)$mybb->settings['allowmultipleemails'] == 1)
 			{
@@ -1136,17 +1154,20 @@ if($mybb->input['action'] == "change")
 			}
 		}
 
-		// Administrator is changing registration email allowance
-		if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->input['upsetting']['allowmultipleemails'] !== 0)
+		if(isset($mybb->input['upsetting']['username_method'], $mybb->input['upsetting']['allowmultipleemails']))
 		{
-			$mybb->input['upsetting']['allowmultipleemails'] = 0;
-			$lang->success_settings_updated .= $lang->success_settings_updated_allowmultipleemails;
-		}
+			// Administrator is changing registration email allowance
+			if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->input['upsetting']['allowmultipleemails'] !== 0)
+			{
+				$mybb->input['upsetting']['allowmultipleemails'] = 0;
+				$lang->success_settings_updated .= $lang->success_settings_updated_allowmultipleemails;
+			}
 
-		// Reset conflict silently, if by chance
-		if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->settings['allowmultipleemails'] == 1)
-		{
-			$mybb->input['upsetting']['allowmultipleemails'] = 0;
+			// Reset conflict silently, if by chance
+			if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->settings['allowmultipleemails'] == 1)
+			{
+				$mybb->input['upsetting']['allowmultipleemails'] = 0;
+			}
 		}
 
 		// reject dangerous/unsupported upload paths

attachment.php
@@ -181,7 +181,7 @@ else
 		case "image/png":
 		case "text/plain":
 			header("Content-type: {$attachment['filetype']}");
-			if($attachtypes[$ext]['forcedownload'])
+			if(!empty($attachtypes[$ext]['forcedownload']))
 			{
 				$disposition = "attachment";
 			}

calendar.php
@@ -1432,12 +1432,12 @@ if($mybb->input['action'] == "event")
 		"allow_videocode" => $calendar['allowvideocode']
 	);
 
-	if($mybb->user['showimages'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
+	if($mybb->user['uid'] != 0 && $mybb->user['showimages'] != 1 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
 	{
 		$event_parser_options['allow_imgcode'] = 0;
 	}
 
-	if($mybb->user['showvideos'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
+	if($mybb->user['uid'] != 0 && $mybb->user['showvideos'] != 1 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
 	{
 		$event_parser_options['allow_videocode'] = 0;
 	}
@@ -1781,12 +1781,12 @@ if($mybb->input['action'] == "dayview")
 				"allow_videocode" => $calendar['allowvideocode']
 			);
 
-			if($mybb->user['showimages'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
+			if($mybb->user['uid'] != 0 && $mybb->user['showimages'] != 1 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
 			{
 				$event_parser_options['allow_imgcode'] = 0;
 			}
 
-			if($mybb->user['showvideos'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
+			if($mybb->user['uid'] != 0 && $mybb->user['showvideos'] != 1 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
 			{
 				$event_parser_options['allow_videocode'] = 0;
 			}

contact.php
@@ -77,7 +77,7 @@ if($mybb->usergroup['emailfloodtime'] > 0)
 	$last_email = $db->fetch_array($query);
 
 	// Users last email was within the flood time, show the error
-	if($last_email['mid'])
+	if(!empty($last_email['mid']))
 	{
 		$remaining_time = ($mybb->usergroup['emailfloodtime']*60)-(TIME_NOW-$last_email['dateline']);
 

editpost.php
@@ -502,6 +502,8 @@ if($mybb->input['action'] == "restorepost" && $mybb->request_method == "post")
 	}
 }
 
+$postoptions = array();
+
 if($mybb->input['action'] == "do_editpost" && $mybb->request_method == "post")
 {
 	// Verify incoming POST request
@@ -683,6 +685,8 @@ if(!$mybb->input['action'] || $mybb->input['action'] == "editpost")
 
 		$lang->attach_quota = $lang->sprintf($lang->attach_quota, $friendlyquota);
 
+		$link_viewattachments = '';
+
 		if($usage['ausage'] !== NULL)
 		{
 			$friendlyusage = get_friendly_size($usage['ausage']);
@@ -693,6 +697,8 @@ if(!$mybb->input['action'] || $mybb->input['action'] == "editpost")
 		{
 			$lang->attach_usage = "";
 		}
+
+		$attach_update_options = '';
 
 		if($mybb->settings['maxattachments'] == 0 || ($mybb->settings['maxattachments'] != 0 && $attachcount < $mybb->settings['maxattachments']) && !$noshowattach)
 		{

global.php
@@ -571,7 +571,7 @@ else
 			break;
 	}
 
-	if($mybb->cookies['lockoutexpiry'])
+	if(!empty($mybb->cookies['lockoutexpiry']))
 	{
 		$secsleft = (int)($mybb->cookies['lockoutexpiry'] - TIME_NOW);
 		$hoursleft = floor($secsleft / 3600);
@@ -890,7 +890,7 @@ if(isset($mybb->user['pmnotice']) && $mybb->user['pmnotice'] == 2 && $mybb->user
 }
 
 $remote_avatar_notice = '';
-if(($mybb->user['avatartype'] === 'remote' || $mybb->user['avatartype'] === 'gravatar') && !$mybb->settings['allowremoteavatars'])
+if(isset($mybb->user['avatartype']) && ($mybb->user['avatartype'] === 'remote' || $mybb->user['avatartype'] === 'gravatar') && !$mybb->settings['allowremoteavatars'])
 {
 	eval('$remote_avatar_notice = "'.$templates->get('global_remote_avatar_notice').'";');
 }
@@ -1023,7 +1023,16 @@ if($mybb->settings['showlanguageselect'] != 0)
 $theme_select = $theme_options = '';
 if($mybb->settings['showthemeselect'] != 0)
 {
-	$theme_options = build_theme_select("theme", $mybb->user['style'], 0, '', false, true);
+	if(isset($mybb->user['style']))
+	{
+		$selected = $mybb->user['style'];
+	}
+	else
+	{
+		$selected = -1;
+	}
+
+	$theme_options = build_theme_select("theme", $selected, 0, '', false, true);
 
 	if(!empty($theme_options))
 	{

inc/class_core.php
@@ -349,7 +349,7 @@ class MyBB {
 
 				// Fixes conflicts with one board having a prefix and another that doesn't on the same domain
 				// Gives priority to our cookies over others (overwrites them)
-				if($this->cookies[$key])
+				if(isset($this->cookies[$key]))
 				{
 					unset($this->cookies[$key]);
 				}

commit 2ecf8a887fd6b003c4333cd017764fd2ca7aed64

Fix SQL error (regression from #4348)

inc/datahandlers/post.php
@@ -440,7 +440,12 @@ class PostDataHandler extends DataHandler
 		$thread = $db->fetch_array($query);
 
 		// Check to see if the same author has posted within the merge post time limit
-		if($thread && ((int)$mybb->settings['postmergemins'] != 0 && trim($mybb->settings['postmergemins']) != "") && (TIME_NOW-$thread['lastpost']) > ((int)$mybb->settings['postmergemins']*60))
+		if(
+			!$thread || (
+				((int)$mybb->settings['postmergemins'] != 0 && trim($mybb->settings['postmergemins']) != "") &&
+				(TIME_NOW-$thread['lastpost']) > ((int)$mybb->settings['postmergemins']*60)
+			)
+		)
 		{
 			return true;
 		}