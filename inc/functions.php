mybb/mybb

commit 634257842da94abc2fadcf0ac628b273fc33ade9

Fix "undefined" and type issues (#4348)

inc/functions.php
@@ -792,18 +792,21 @@ function get_child_list($fid)
 			}
 		}
 	}
-	if(!is_array($forums_by_parent[$fid]))
+	if(isset($forums_by_parent[$fid]))
 	{
-		return $forums;
-	}
-
-	foreach($forums_by_parent[$fid] as $forum)
-	{
-		$forums[] = (int)$forum['fid'];
-		$children = get_child_list($forum['fid']);
-		if(is_array($children))
+		if(!is_array($forums_by_parent[$fid]))
 		{
-			$forums = array_merge($forums, $children);
+			return $forums;
+		}
+
+		foreach($forums_by_parent[$fid] as $forum)
+		{
+			$forums[] = (int)$forum['fid'];
+			$children = get_child_list($forum['fid']);
+			if(is_array($children))
+			{
+				$forums = array_merge($forums, $children);
+			}
 		}
 	}
 	return $forums;
@@ -1014,7 +1017,7 @@ function redirect($url, $message="", $title="", $force_redirect=false)
 	}
 
 	// Show redirects only if both ACP and UCP settings are enabled, or ACP is enabled, and user is a guest, or they are forced.
-	if($force_redirect == true || ($mybb->settings['redirects'] == 1 && ($mybb->user['showredirect'] == 1 || !$mybb->user['uid'])))
+	if($force_redirect == true || ($mybb->settings['redirects'] == 1 && (!$mybb->user['uid'] || !$mybb->user['showredirect'] == 1)))
 	{
 		$url = str_replace("&amp;", "&", $url);
 		$url = htmlspecialchars_uni($url);
@@ -2283,10 +2286,9 @@ function my_set_array_cookie($name, $id, $value, $expires="")
 {
 	global $mybb;
 
-	$cookie = $mybb->cookies['mybb'];
-	if(isset($cookie[$name]))
+	if(isset($mybb->cookies['mybb'][$name]))
 	{
-		$newcookie = my_unserialize($cookie[$name]);
+		$newcookie = my_unserialize($mybb->cookies['mybb'][$name]);
 	}
 	else
 	{
@@ -6745,7 +6747,7 @@ function login_attempt_check($uid = 0, $fatal = true)
 		}
 	}
 	// This user has a cookie lockout, show waiting time
-	elseif($mybb->cookies['lockoutexpiry'] && $mybb->cookies['lockoutexpiry'] > $now)
+	elseif(!empty($mybb->cookies['lockoutexpiry']) && $mybb->cookies['lockoutexpiry'] > $now)
 	{
 		if($fatal)
 		{
@@ -6760,7 +6762,7 @@ function login_attempt_check($uid = 0, $fatal = true)
 		return false;
 	}
 
-	if($mybb->settings['failedlogincount'] > 0 && $attempts['loginattempts'] >= $mybb->settings['failedlogincount'])
+	if($mybb->settings['failedlogincount'] > 0 && isset($attempts['loginattempts']) && $attempts['loginattempts'] >= $mybb->settings['failedlogincount'])
 	{
 		// Set the expiry dateline if not set yet
 		if($attempts['loginlockoutexpiry'] == 0)
@@ -6816,6 +6818,11 @@ function login_attempt_check($uid = 0, $fatal = true)
 
 			return 0;
 		}
+	}
+
+	if(!isset($attempts['loginattempts']))
+	{
+		$attempts['loginattempts'] = 0;
 	}
 
 	// User can attempt another login

admin/modules/config/settings.php
@@ -968,19 +968,29 @@ if($mybb->input['action'] == "change")
 		// Validate minnamelength, maxnamelength, minpasswordlength (complex and regular) and maxpasswordlength
 		if ($gid == 9)
 		{
-			if ($mybb->input['upsetting']['minnamelength'] > 0 && $mybb->input['upsetting']['maxnamelength'] > 0 && $mybb->input['upsetting']['minnamelength'] > $mybb->input['upsetting']['maxnamelength'])
+			if (
+				isset($mybb->input['upsetting']['minnamelength'], $mybb->input['upsetting']['maxnamelength']) &&
+				$mybb->input['upsetting']['minnamelength'] > 0 && $mybb->input['upsetting']['maxnamelength'] > 0 &&
+				$mybb->input['upsetting']['minnamelength'] > $mybb->input['upsetting']['maxnamelength'])
 			{
 				flash_message($lang->error_field_minnamelength, 'error');
 				admin_redirect("index.php?module=config-settings&action=change&gid=".$gid);
 			}
 
-			if ($mybb->input['upsetting']['minpasswordlength'] > 0 && $mybb->input['upsetting']['maxpasswordlength'] > 0 && $mybb->input['upsetting']['minpasswordlength'] > $mybb->input['upsetting']['maxpasswordlength'])
+			if (
+				isset($mybb->input['upsetting']['minpasswordlength'], $mybb->input['upsetting']['maxpasswordlength']) &&
+				$mybb->input['upsetting']['minpasswordlength'] > 0 && $mybb->input['upsetting']['maxpasswordlength'] > 0 &&
+				$mybb->input['upsetting']['minpasswordlength'] > $mybb->input['upsetting']['maxpasswordlength']
+			)
 			{
 				flash_message($lang->error_field_minpasswordlength, 'error');
 				admin_redirect("index.php?module=config-settings&action=change&gid=".$gid);
 			}
 
-			if ($mybb->input['upsetting']['requirecomplexpasswords'] && $mybb->input['upsetting']['minpasswordlength'] < 3)
+			if (
+				isset($mybb->input['upsetting']['minpasswordlength'], $mybb->input['upsetting']['requirecomplexpasswords']) &&
+				$mybb->input['upsetting']['requirecomplexpasswords'] && $mybb->input['upsetting']['minpasswordlength'] < 3
+			)
 			{
 				flash_message($lang->error_field_minpasswordlength_complex, 'error');
 				admin_redirect("index.php?module=config-settings&action=change&gid=".$gid);
@@ -991,11 +1001,19 @@ if($mybb->input['action'] == "change")
 		
 		// Have we opted for a reCAPTCHA or hCaptcha and not set a public/private key in input?
 		$set_captcha_image = false;
-		if(isset($mybb->input['upsetting']['captchaimage']))
+		if(isset(
+			$mybb->input['upsetting']['captchaimage'],
+			$mybb->input['upsetting']['recaptchaprivatekey'],
+			$mybb->input['upsetting']['recaptchapublickey'],
+			$mybb->input['upsetting']['recaptchascore'],
+			$mybb->input['upsetting']['hcaptchaprivatekey'],
+			$mybb->input['upsetting']['hcaptchapublickey']
+		))
 		{
 			$captchaimage = $mybb->input['upsetting']['captchaimage'];
 			$recaptchaprivatekey = $mybb->input['upsetting']['recaptchaprivatekey'];
 			$recaptchapublickey = $mybb->input['upsetting']['recaptchapublickey'];
+			$recaptchascore = $mybb->input['upsetting']['recaptchascore'];
 			$hcaptchaprivatekey = $mybb->input['upsetting']['hcaptchaprivatekey'];
 			$hcaptchapublickey = $mybb->input['upsetting']['hcaptchapublickey'];
 
@@ -1118,7 +1136,7 @@ if($mybb->input['action'] == "change")
 		}
 
 		// Administrator is changing the login method.
-		if((int)$mybb->input['upsetting']['username_method'] > 0)
+		if(isset($mybb->input['upsetting']['username_method']) && (int)$mybb->input['upsetting']['username_method'] > 0)
 		{
 			if((int)$mybb->settings['allowmultipleemails'] == 1)
 			{
@@ -1136,17 +1154,20 @@ if($mybb->input['action'] == "change")
 			}
 		}
 
-		// Administrator is changing registration email allowance
-		if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->input['upsetting']['allowmultipleemails'] !== 0)
+		if(isset($mybb->input['upsetting']['username_method'], $mybb->input['upsetting']['allowmultipleemails']))
 		{
-			$mybb->input['upsetting']['allowmultipleemails'] = 0;
-			$lang->success_settings_updated .= $lang->success_settings_updated_allowmultipleemails;
-		}
+			// Administrator is changing registration email allowance
+			if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->input['upsetting']['allowmultipleemails'] !== 0)
+			{
+				$mybb->input['upsetting']['allowmultipleemails'] = 0;
+				$lang->success_settings_updated .= $lang->success_settings_updated_allowmultipleemails;
+			}
 
-		// Reset conflict silently, if by chance
-		if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->settings['allowmultipleemails'] == 1)
-		{
-			$mybb->input['upsetting']['allowmultipleemails'] = 0;
+			// Reset conflict silently, if by chance
+			if((int)$mybb->settings['username_method'] > 0 && (int)$mybb->settings['allowmultipleemails'] == 1)
+			{
+				$mybb->input['upsetting']['allowmultipleemails'] = 0;
+			}
 		}
 
 		// reject dangerous/unsupported upload paths

attachment.php
@@ -181,7 +181,7 @@ else
 		case "image/png":
 		case "text/plain":
 			header("Content-type: {$attachment['filetype']}");
-			if($attachtypes[$ext]['forcedownload'])
+			if(!empty($attachtypes[$ext]['forcedownload']))
 			{
 				$disposition = "attachment";
 			}

calendar.php
@@ -1432,12 +1432,12 @@ if($mybb->input['action'] == "event")
 		"allow_videocode" => $calendar['allowvideocode']
 	);
 
-	if($mybb->user['showimages'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
+	if($mybb->user['uid'] != 0 && $mybb->user['showimages'] != 1 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
 	{
 		$event_parser_options['allow_imgcode'] = 0;
 	}
 
-	if($mybb->user['showvideos'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
+	if($mybb->user['uid'] != 0 && $mybb->user['showvideos'] != 1 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
 	{
 		$event_parser_options['allow_videocode'] = 0;
 	}
@@ -1781,12 +1781,12 @@ if($mybb->input['action'] == "dayview")
 				"allow_videocode" => $calendar['allowvideocode']
 			);
 
-			if($mybb->user['showimages'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
+			if($mybb->user['uid'] != 0 && $mybb->user['showimages'] != 1 || $mybb->settings['guestimages'] != 1 && $mybb->user['uid'] == 0)
 			{
 				$event_parser_options['allow_imgcode'] = 0;
 			}
 
-			if($mybb->user['showvideos'] != 1 && $mybb->user['uid'] != 0 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
+			if($mybb->user['uid'] != 0 && $mybb->user['showvideos'] != 1 || $mybb->settings['guestvideos'] != 1 && $mybb->user['uid'] == 0)
 			{
 				$event_parser_options['allow_videocode'] = 0;
 			}

contact.php
@@ -77,7 +77,7 @@ if($mybb->usergroup['emailfloodtime'] > 0)
 	$last_email = $db->fetch_array($query);
 
 	// Users last email was within the flood time, show the error
-	if($last_email['mid'])
+	if(!empty($last_email['mid']))
 	{
 		$remaining_time = ($mybb->usergroup['emailfloodtime']*60)-(TIME_NOW-$last_email['dateline']);
 

editpost.php
@@ -502,6 +502,8 @@ if($mybb->input['action'] == "restorepost" && $mybb->request_method == "post")
 	}
 }
 
+$postoptions = array();
+
 if($mybb->input['action'] == "do_editpost" && $mybb->request_method == "post")
 {
 	// Verify incoming POST request
@@ -683,6 +685,8 @@ if(!$mybb->input['action'] || $mybb->input['action'] == "editpost")
 
 		$lang->attach_quota = $lang->sprintf($lang->attach_quota, $friendlyquota);
 
+		$link_viewattachments = '';
+
 		if($usage['ausage'] !== NULL)
 		{
 			$friendlyusage = get_friendly_size($usage['ausage']);
@@ -693,6 +697,8 @@ if(!$mybb->input['action'] || $mybb->input['action'] == "editpost")
 		{
 			$lang->attach_usage = "";
 		}
+
+		$attach_update_options = '';
 
 		if($mybb->settings['maxattachments'] == 0 || ($mybb->settings['maxattachments'] != 0 && $attachcount < $mybb->settings['maxattachments']) && !$noshowattach)
 		{

global.php
@@ -571,7 +571,7 @@ else
 			break;
 	}
 
-	if($mybb->cookies['lockoutexpiry'])
+	if(!empty($mybb->cookies['lockoutexpiry']))
 	{
 		$secsleft = (int)($mybb->cookies['lockoutexpiry'] - TIME_NOW);
 		$hoursleft = floor($secsleft / 3600);
@@ -890,7 +890,7 @@ if(isset($mybb->user['pmnotice']) && $mybb->user['pmnotice'] == 2 && $mybb->user
 }
 
 $remote_avatar_notice = '';
-if(($mybb->user['avatartype'] === 'remote' || $mybb->user['avatartype'] === 'gravatar') && !$mybb->settings['allowremoteavatars'])
+if(isset($mybb->user['avatartype']) && ($mybb->user['avatartype'] === 'remote' || $mybb->user['avatartype'] === 'gravatar') && !$mybb->settings['allowremoteavatars'])
 {
 	eval('$remote_avatar_notice = "'.$templates->get('global_remote_avatar_notice').'";');
 }
@@ -1023,7 +1023,16 @@ if($mybb->settings['showlanguageselect'] != 0)
 $theme_select = $theme_options = '';
 if($mybb->settings['showthemeselect'] != 0)
 {
-	$theme_options = build_theme_select("theme", $mybb->user['style'], 0, '', false, true);
+	if(isset($mybb->user['style']))
+	{
+		$selected = $mybb->user['style'];
+	}
+	else
+	{
+		$selected = -1;
+	}
+
+	$theme_options = build_theme_select("theme", $selected, 0, '', false, true);
 
 	if(!empty($theme_options))
 	{

inc/class_core.php
@@ -349,7 +349,7 @@ class MyBB {
 
 				// Fixes conflicts with one board having a prefix and another that doesn't on the same domain
 				// Gives priority to our cookies over others (overwrites them)
-				if($this->cookies[$key])
+				if(isset($this->cookies[$key]))
 				{
 					unset($this->cookies[$key]);
 				}

commit 309b00bac516a71b64386d6e889fa7b953078660

Fix accidental negation
Regression from #4348 (https://github.com/mybb/mybb/pull/4348#discussion_r646854465)

inc/functions.php
@@ -1088,7 +1088,7 @@ function redirect($url, $message="", $title="", $force_redirect=false)
 	}
 
 	// Show redirects only if both ACP and UCP settings are enabled, or ACP is enabled, and user is a guest, or they are forced.
-	if($force_redirect == true || ($mybb->settings['redirects'] == 1 && (!$mybb->user['uid'] || !$mybb->user['showredirect'] == 1)))
+	if($force_redirect == true || ($mybb->settings['redirects'] == 1 && (!$mybb->user['uid'] || $mybb->user['showredirect'] == 1)))
 	{
 		$url = str_replace("&amp;", "&", $url);
 		$url = htmlspecialchars_uni($url);