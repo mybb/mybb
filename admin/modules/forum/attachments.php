d18b60d9d7e830f53703512d2ede2f250b4e9ecb
Tomasz Mlynski <devilshakerz@gmail.com>
2021-08-19 15:48:33 +0200

    Fix `forums` input type handling

--- admin/modules/forum/attachments.php
+++ admin/modules/forum/attachments.php
@@ -712,7 +712,7 @@ if(!$mybb->input['action'])
 		$forum_cache = cache_forums();
 
 		// Searching for attachments in a specific forum, we need to fetch all child forums too
-		if($mybb->get_input('forum'))
+		if(!empty($mybb->input['forum']))
 		{
 			if(!is_array($mybb->input['forum']))
 			{
