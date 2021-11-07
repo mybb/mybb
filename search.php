58ea08fac7b8d712f5d279edd2e5bcfebb5e9e86
Mateusz Słomiński <mateosl111@gmail.com>
2021-11-07 18:41:31 +0100

    Fix sql error when sorting by forum ref f2f59777c7697b845240efa5db275767c72ad906 (#4488)

--- search.php
+++ search.php
@@ -807,6 +807,7 @@ if($mybb->input['action'] == "results")
             FROM ".TABLE_PREFIX."posts p
             LEFT JOIN ".TABLE_PREFIX."threads t ON (t.tid=p.tid)
             LEFT JOIN ".TABLE_PREFIX."users u ON (u.uid=p.uid)
+            LEFT JOIN ".TABLE_PREFIX."forums f ON (t.fid=f.fid)
             WHERE p.pid IN (".$db->escape_string($search['posts']).")
             ORDER BY $sortfield $order
             LIMIT $start, $perpage
