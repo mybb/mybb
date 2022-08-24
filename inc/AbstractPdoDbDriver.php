9a768ed23e3e480d9b132561b9185a38561dcda4
dvz <devilshakerz@gmail.com>
2022-08-24 16:14:52 +0200

    Allow `DB_Base::$type` overloading in `AbstractPdoDbDriver`

--- inc/AbstractPdoDbDriver.php
+++ inc/AbstractPdoDbDriver.php
@@ -10,13 +10,6 @@
 abstract class AbstractPdoDbDriver implements DB_Base
 {
 	/**
-	 * The type of db software being used.
-	 *
-	 * @var string
-	 */
-	public $type;
-
-	/**
 	 * Whether error reporting is enabled.
 	 *
 	 * @var boolean
