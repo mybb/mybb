b1e3af0ef31faf842bff2990bde3d4ab92f774bd
Laird <lairdshaw77@gmail.com>
2022-10-02 12:11:33 +1000

    Add a clarifying warning to the theme deletion confirmation popup

--- inc/languages/english/admin/style_themes.lang.php
+++ inc/languages/english/admin/style_themes.lang.php
@@ -359,7 +359,7 @@ $l['success_new_template_saved'] = 'The new template has successfully been saved
 $l['success_template_deleted'] = 'The selected template has successfully been deleted.';
 $l['success_created_devdist'] = 'A `devdist` directory was successfully created for the theme.';
 
-$l['confirm_theme_deletion'] = "Are you sure you want to delete this theme?";
+$l['confirm_theme_deletion'] = "Are you sure you want to delete this theme? WARNING: This will delete BOTH `current` AND `devdist` modes of this theme (whichever exist).";
 $l['confirm_stylesheet_deletion'] = "Are you sure you want to delete / revert this stylesheet?";
 $l['confirm_theme_forced'] = "Are you sure you want to force this theme on all users? This may reset the theme's access level.";
 $l['confirm_template_deletion'] = 'Are you sure you want to delete / revert this template?';
