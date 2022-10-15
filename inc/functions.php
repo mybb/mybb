583870d58ee197d3da75b3a618890ac0c3457718
Laird <lairdshaw77@gmail.com>
2022-10-15 15:01:23 +1000

    Fix #3793: Caching fallback breaks when external cache system has problems

--- inc/functions.php
+++ inc/functions.php
@@ -2392,295 +2392,33 @@ function my_set_array_cookie($name, $id, $value, $expires="")
 	$mybb->cookies['mybb'][$name] = $newcookie;
 }
 
-/*
- * Arbitrary limits for _safe_unserialize()
- */
-define('MAX_SERIALIZED_INPUT_LENGTH', 10240);
-define('MAX_SERIALIZED_ARRAY_LENGTH', 256);
-define('MAX_SERIALIZED_ARRAY_DEPTH', 5);
-
 /**
- * Credits go to https://github.com/piwik
- * Safe unserialize() replacement
- * - accepts a strict subset of PHP's native my_serialized representation
- * - does not unserialize objects
+ * Simply wrap around PHP's default unserialize() function, disallowing the
+ * unserialisation of objects.
  *
- * @param string $str
- * @return mixed
- * @throw Exception if $str is malformed or contains unsupported types (e.g., resources, objects)
- */
-function _safe_unserialize($str)
-{
-	if(strlen($str) > MAX_SERIALIZED_INPUT_LENGTH)
-	{
-		// input exceeds MAX_SERIALIZED_INPUT_LENGTH
-		return false;
-	}
-
-	if(empty($str) || !is_string($str))
-	{
-		return false;
-	}
-
-	$stack = $list = $expected = array();
-
-	/*
-	 * states:
-	 *   0 - initial state, expecting a single value or array
-	 *   1 - terminal state
-	 *   2 - in array, expecting end of array or a key
-	 *   3 - in array, expecting value or another array
-	 */
-	$state = 0;
-	while($state != 1)
-	{
-		$type = isset($str[0]) ? $str[0] : '';
-
-		if($type == '}')
-		{
-			$str = substr($str, 1);
-		}
-		else if($type == 'N' && $str[1] == ';')
-		{
-			$value = null;
-			$str = substr($str, 2);
-		}
-		else if($type == 'b' && preg_match('/^b:([01]);/', $str, $matches))
-		{
-			$value = $matches[1] == '1' ? true : false;
-			$str = substr($str, 4);
-		}
-		else if($type == 'i' && preg_match('/^i:(-?[0-9]+);(.*)/s', $str, $matches))
-		{
-			$value = (int)$matches[1];
-			$str = $matches[2];
-		}
-		else if($type == 'd' && preg_match('/^d:(-?[0-9]+\.?[0-9]*(E[+-][0-9]+)?);(.*)/s', $str, $matches))
-		{
-			$value = (float)$matches[1];
-			$str = $matches[3];
-		}
-		else if($type == 's' && preg_match('/^s:([0-9]+):"(.*)/s', $str, $matches) && substr($matches[2], (int)$matches[1], 2) == '";')
-		{
-			$value = substr($matches[2], 0, (int)$matches[1]);
-			$str = substr($matches[2], (int)$matches[1] + 2);
-		}
-		else if($type == 'a' && preg_match('/^a:([0-9]+):{(.*)/s', $str, $matches) && $matches[1] < MAX_SERIALIZED_ARRAY_LENGTH)
-		{
-			$expectedLength = (int)$matches[1];
-			$str = $matches[2];
-		}
-		else
-		{
-			// object or unknown/malformed type
-			return false;
-		}
-
-		switch($state)
-		{
-			case 3: // in array, expecting value or another array
-				if($type == 'a')
-				{
-					if(count($stack) >= MAX_SERIALIZED_ARRAY_DEPTH)
-					{
-						// array nesting exceeds MAX_SERIALIZED_ARRAY_DEPTH
-						return false;
-					}
-
-					$stack[] = &$list;
-					$list[$key] = array();
-					$list = &$list[$key];
-					$expected[] = $expectedLength;
-					$state = 2;
-					break;
-				}
-				if($type != '}')
-				{
-					$list[$key] = $value;
-					$state = 2;
-					break;
-				}
-
-				// missing array value
-				return false;
-
-			case 2: // in array, expecting end of array or a key
-				if($type == '}')
-				{
-					if(count($list) < end($expected))
-					{
-						// array size less than expected
-						return false;
-					}
-
-					unset($list);
-					$list = &$stack[count($stack)-1];
-					array_pop($stack);
-
-					// go to terminal state if we're at the end of the root array
-					array_pop($expected);
-					if(count($expected) == 0) {
-						$state = 1;
-					}
-					break;
-				}
-				if($type == 'i' || $type == 's')
-				{
-					if(count($list) >= MAX_SERIALIZED_ARRAY_LENGTH)
-					{
-						// array size exceeds MAX_SERIALIZED_ARRAY_LENGTH
-						return false;
-					}
-					if(count($list) >= end($expected))
-					{
-						// array size exceeds expected length
-						return false;
-					}
-
-					$key = $value;
-					$state = 3;
-					break;
-				}
-
-				// illegal array index type
-				return false;
-
-			case 0: // expecting array or value
-				if($type == 'a')
-				{
-					if(count($stack) >= MAX_SERIALIZED_ARRAY_DEPTH)
-					{
-						// array nesting exceeds MAX_SERIALIZED_ARRAY_DEPTH
-						return false;
-					}
-
-					$data = array();
-					$list = &$data;
-					$expected[] = $expectedLength;
-					$state = 2;
-					break;
-				}
-				if($type != '}')
-				{
-					$data = $value;
-					$state = 1;
-					break;
-				}
-
-				// not in array
-				return false;
-		}
-	}
-
-	if(!empty($str))
-	{
-		// trailing data in input
-		return false;
-	}
-	return $data;
-}
-
-/**
- * Credits go to https://github.com/piwik
- * Wrapper for _safe_unserialize() that handles exceptions and multibyte encoding issue
+ * Potential TODO: Replace all calls to this function *with* PHP's default
+ * `unserialize()` function.
  *
  * @param string $str
  * @return mixed
  */
 function my_unserialize($str)
 {
-	// Ensure we use the byte count for strings even when strlen() is overloaded by mb_strlen()
-	if(function_exists('mb_internal_encoding') && (((int)ini_get('mbstring.func_overload')) & 2))
-	{
-		$mbIntEnc = mb_internal_encoding();
-		mb_internal_encoding('ASCII');
-	}
-
-	$out = _safe_unserialize($str);
-
-	if(isset($mbIntEnc))
-	{
-		mb_internal_encoding($mbIntEnc);
-	}
-
-	return $out;
+	return unserialize($str, ['allowed_classes' => false]);
 }
 
 /**
- * Credits go to https://github.com/piwik
- * Safe serialize() replacement
- * - output a strict subset of PHP's native serialized representation
- * - does not my_serialize objects
+ * Simply wrap around PHP's default serialize() function.
  *
- * @param mixed $value
- * @return string
- * @throw Exception if $value is malformed or contains unsupported types (e.g., resources, objects)
- */
-function _safe_serialize( $value )
-{
-	if(is_null($value))
-	{
-		return 'N;';
-	}
-
-	if(is_bool($value))
-	{
-		return 'b:'.(int)$value.';';
-	}
-
-	if(is_int($value))
-	{
-		return 'i:'.$value.';';
-	}
-
-	if(is_float($value))
-	{
-		return 'd:'.str_replace(',', '.', $value).';';
-	}
-
-	if(is_string($value))
-	{
-		return 's:'.strlen($value).':"'.$value.'";';
-	}
-
-	if(is_array($value))
-	{
-		$out = '';
-		foreach($value as $k => $v)
-		{
-			$out .= _safe_serialize($k) . _safe_serialize($v);
-		}
-
-		return 'a:'.count($value).':{'.$out.'}';
-	}
-
-	// safe_serialize cannot my_serialize resources or objects
-	return false;
-}
-
-/**
- * Credits go to https://github.com/piwik
- * Wrapper for _safe_serialize() that handles exceptions and multibyte encoding issue
+ * Potential TODO: Replace all calls to this function *with* PHP's default
+ * `serialize()` function.
  *
  * @param mixed $value
  * @return string
 */
 function my_serialize($value)
 {
-	// ensure we use the byte count for strings even when strlen() is overloaded by mb_strlen()
-	if(function_exists('mb_internal_encoding') && (((int)ini_get('mbstring.func_overload')) & 2))
-	{
-		$mbIntEnc = mb_internal_encoding();
-		mb_internal_encoding('ASCII');
-	}
-
-	$out = _safe_serialize($value);
-	if(isset($mbIntEnc))
-	{
-		mb_internal_encoding($mbIntEnc);
-	}
-
-	return $out;
+	return serialize($value);
 }
 
 /**
