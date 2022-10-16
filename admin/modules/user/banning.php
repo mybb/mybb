749c195813ded72b91fd091a306ce3544584103c
Yu 'noyle' Liu <noyle@yuliu.me>
2022-10-16 18:44:29 +0800

    Fixes Undefined property: MyLanguage::$add_ban_group_desc in ACP users banning module https://github.com/mybb/mybb/issues/4593#issuecomment-1279788855
    (cherry picked from commit a62d52e58c1254de0b59b9f67bb68eedd1795281)

--- admin/modules/user/banning.php
+++ admin/modules/user/banning.php
@@ -409,7 +409,7 @@ if(!$mybb->input['action'])
 	$form_container->output_row($lang->ban_reason, "", $form->generate_text_area('reason', $mybb->input['reason'], array('id' => 'reason', 'maxlength' => '255')), 'reason');
 	if(count($banned_groups) > 1)
 	{
-		$form_container->output_row($lang->ban_group, $lang->add_ban_group_desc, $form->generate_select_box('usergroup', $banned_groups, $mybb->input['usergroup'], array('id' => 'usergroup')), 'usergroup');
+		$form_container->output_row($lang->ban_group, $lang->ban_group_desc, $form->generate_select_box('usergroup', $banned_groups, $mybb->input['usergroup'], array('id' => 'usergroup')), 'usergroup');
 	}
 	foreach($ban_times as $time => $period)
 	{
