f962b93fc9d4dbf47185b3622505f82fb8eba3bd
dvz <devilshakerz@gmail.com>
2022-11-18 17:33:25 +0100

    1.8.32 meta

--- inc/class_core.php
+++ inc/class_core.php
@@ -14,14 +14,14 @@ class MyBB {
 	 *
 	 * @var string
 	 */
-	public $version = "1.8.31";
+	public $version = "1.8.32";
 
 	/**
 	 * The version code of MyBB we're running.
 	 *
 	 * @var integer
 	 */
-	public $version_code = 1831;
+	public $version_code = 1832;
 
 	/**
 	 * The current working directory.
