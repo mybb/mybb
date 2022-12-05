a01ae6b5116f27c01df8a789d649d4cf4296b228
Laird <lairdshaw77@gmail.com>
2022-12-06 07:08:18 +1000

    Add DB alterations as required on upgrade from 1.8 => 1.9

--- install/resources/upgrade56.php
+++ install/resources/upgrade56.php
@@ -31,7 +31,18 @@ function upgrade56_dbchanges()
 
 	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users CHANGE `style` `style` varchar(30) NOT NULL DEFAULT \'\'');
 	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'forums CHANGE `style` `style` varchar(30) NOT NULL DEFAULT \'\'');
-	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users ADD COLUMN password_algorithm varchar(30) NOT NULL DEFAULT \'\'');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users ADD COLUMN `password_algorithm` varchar(30) NOT NULL DEFAULT \'\'');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users CHANGE `password` `password` varchar(500) NOT NULL default \'\'');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users DROP COLUMN `icq`');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users DROP COLUMN `skype`');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'users DROP COLUMN `google`');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'profilefields ADD COLUMN `contact` tinyint(1) NOT NULL default \'0\'');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'threads ADD COLUMN `moved` int unsigned NOT NULL default \'0\'');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'threads CHANGE `closed` `closed` tinyint(1) NOT NULL default \'0\'');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'userfields ADD COLUMN `fid4` text NOT NULL');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'userfields ADD COLUMN `fid5` text NOT NULL');
+	$db->write_query('ALTER TABLE '.TABLE_PREFIX.'userfields ADD COLUMN `fid6` text NOT NULL');
+
 	$db->drop_table('themes');
 	$db->drop_table('themestylesheets');
 	$db->drop_table('templategroups');
