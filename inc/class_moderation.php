6039a4367f3145c97bd1baaac83b1e54f36f684a
Yu 'noyle' Liu <noyle@yuliu.me>
2022-12-18 22:58:51 +0800

    Fixes various PHP warnings in thread moderations:
     - Approve thread(s):
        - Undefined array key XXX in `inc/class_moderation.php` (L437).
        - Undefined array key "num_posts" in `inc/class_moderation.php` (L437).
        - Undefined array key "num_threads" in `inc/class_moderation.php` (L504).
    
     - Similarly, unapprove thread(s).

--- inc/class_moderation.php
+++ inc/class_moderation.php
@@ -434,6 +434,13 @@ class Moderation
 				$query2 = $db->simple_select("posts", "COUNT(pid) as posts, uid", "tid='{$thread['tid']}' AND (visible='1' OR pid='{$thread['firstpost']}') AND uid > 0 GROUP BY uid");
 				while($counter = $db->fetch_array($query2))
 				{
+					if(!isset($user_counters[$counter['uid']]))
+					{
+						$user_counters[$counter['uid']] = array(
+							'num_posts' => 0,
+							'num_threads' => 0
+						);
+					}
 					$user_counters[$counter['uid']]['num_posts'] += $counter['posts'];
 				}
 			}
@@ -595,6 +602,13 @@ class Moderation
 					$query2 = $db->simple_select("posts", "COUNT(pid) AS posts, uid", "tid='{$thread['tid']}' AND (visible='1' OR pid='{$thread['firstpost']}') AND uid > 0 GROUP BY uid");
 					while($counter = $db->fetch_array($query2))
 					{
+						if(!isset($user_counters[$counter['uid']]))
+						{
+							$user_counters[$counter['uid']] = array(
+								'num_posts' => 0,
+								'num_threads' => 0
+							);
+						}
 						$user_counters[$counter['uid']]['num_posts'] += $counter['posts'];
 					}
 				}
