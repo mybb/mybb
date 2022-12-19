c234fe2ff0e8ecac242d6239185529be8c703dd6
Yu 'noyle' Liu <noyle@yuliu.me>
2022-12-20 01:05:02 +0800

    Fixes global `$forum_cache` being overwritten in `get_unsearchable_forums()` in `/inc/functions_search.php`.

--- inc/functions_search.php
+++ inc/functions_search.php
@@ -83,18 +83,13 @@ function make_searchable_forums($pid=0, $selitem=0, $addselect=1, $depth='')
  */
 function get_unsearchable_forums($pid=0, $first=1)
 {
-	global $db, $forum_cache, $permissioncache, $mybb, $unsearchableforums, $unsearchable, $templates, $forumpass;
+	global $forum_cache, $permissioncache, $mybb, $unsearchableforums, $unsearchable, $templates, $forumpass;
 
 	$pid = (int)$pid;
 
 	if(!is_array($forum_cache))
 	{
-		// Get Forums
-		$query = $db->simple_select("forums", "fid,parentlist,password,active", '', array('order_by' => 'pid, disporder'));
-		while($forum = $db->fetch_array($query))
-		{
-			$forum_cache[$forum['fid']] = $forum;
-		}
+		cache_forums();
 	}
 	if(!is_array($permissioncache))
 	{
