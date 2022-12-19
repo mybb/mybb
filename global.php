fc06044714f29aa37fa4d263de33776294b3a1b4
Laird <lairdshaw77@gmail.com>
2022-12-19 21:13:40 +1000

    Revert the breaking change of e1f73af
    Revert due to the call to `$templates->cache()` in commit e1f73af which
    introduces a fatal error: `template::cache()` in `inc/templates.php`
    selects from the dropped DB table `templates`.

--- global.php
+++ global.php
@@ -459,17 +459,6 @@ if(!empty($theme['logo']) && !preg_match("#^(\.\.?(/|$)|([a-z0-9]+)://)#i", $the
 	$theme['logo'] = $mybb->get_asset_url($theme['logo']);
 }
 
-// Load Main Templates and Cached Templates
-if(isset($templatelist))
-{
-	$templatelist .= ',';
-}
-else
-{
-	$templatelist = '';
-}
-$templates->cache($db->escape_string($templatelist));
-
 // Set the current date and time now
 $datenow = my_date($mybb->settings['dateformat'], TIME_NOW, '', false);
 $timenow = my_date($mybb->settings['timeformat'], TIME_NOW);
