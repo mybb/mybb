1249e4a4c4b9f1a205bfe71b396cbbebb0d9c7fb
dvz <devilshakerz@gmail.com>
2023-02-15 21:09:52 +0100

    Fix database string error code issues

--- inc/DbException.php
+++ inc/DbException.php
@@ -3,12 +3,14 @@
 class DbException extends Exception
 {
     protected ?string $query = null;
+    protected $code = null;
 
-    public function __construct(string $message = '', int $code = 0, string $query = null, ?Throwable $previous = null)
+    public function __construct(string $message = '', mixed $code = '', string $query = null, ?Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, 0, $previous);
 
         $this->query = $query;
+        $this->code = $code;
     }
 
     public function getQuery(): ?string
