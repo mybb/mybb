c88a6f815f6626c0e78618c9d350fb8703ad6561
BettyIsBoop <102652163+BettyIsBoop@users.noreply.github.com>
2023-05-28 08:35:31 +0000

    Move to preg_replace

--- inc/functions_search.php
+++ inc/functions_search.php
@@ -285,17 +285,7 @@ function clean_keywords($keywords)
 
 	// Search for "and" or "or" and remove if it's at the beginning
 	$keywords = trim($keywords);
-	if(my_strpos($keywords, "or ") === 0)
-	{
-		$keywords = substr_replace($keywords, "", 0, 2);
-		$keywords = " ".$keywords;
-	}
-
-	if(my_strpos($keywords, "and ") === 0)
-	{
-		$keywords = substr_replace($keywords, "", 0, 3);
-		$keywords = " ".$keywords;
-	}
+	$keywords = preg_replace('/^((and|or)(\s|$))+/', '', $keywords);
 
 	if(!$keywords)
 	{
