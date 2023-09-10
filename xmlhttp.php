1376edf2905037643eebe99ce8b8a1cd3e09f0c2
Eldenroot <Eldenroot@gmail.com>
2023-09-10 14:19:24 +0200

    Fixes #4740   xmlhttp.php warning
    #4740

--- xmlhttp.php
+++ xmlhttp.php
@@ -646,7 +646,7 @@ else if($mybb->input['action'] == "edit_post")
 		header("Content-type: application/json; charset={$charset}");
 
 		$editedmsg_response = null;
-		if($editedmsg)
+		if(!empty($editedmsg))
 		{
 			$editedmsg_response = str_replace(array("\r", "\n"), "", $editedmsg);
 		}
