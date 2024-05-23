ec000085111c289cd43b932ae2b8cc72faf17381
Simon Schrape <smnscp@users.noreply.github.com>
2024-05-23 16:06:26 +0200

    Fix #4812. (#4813)
    * Fix https://github.com/mybb/mybb/issues/4812.
    
    * Check all quick login fields for presence.

--- inc/datahandlers/login.php
+++ inc/datahandlers/login.php
@@ -109,7 +109,7 @@ class LoginDataHandler extends DataHandler
 				return true;
 			}
 		}
-		else if($mybb->input['quick_login'] == 1 && $mybb->input['quick_password'] && $mybb->input['quick_username'])
+		else if(!empty($mybb->input['quick_login']) && !empty($mybb->input['quick_password']) && !empty($mybb->input['quick_username']))
 		{
 			$this->set_error('regimagerequired');
 			return false;
