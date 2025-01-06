7953a479265e207df6c33fd1b9d9da572991aaba
Eldenroot <Eldenroot@gmail.com>
2025-01-06 22:33:14 +0100

    Fixes #4826 - Warnings on newreply or newthread for guests writing in preview mode
    Warnings on newreply or newthread for guests writing in preview mode

--- newthread.php
+++ newthread.php
@@ -162,8 +162,7 @@ $errors = array();
 $maximageserror = $attacherror = '';
 
 // Handle attachments if we've got any.
-if($mybb->settings['enableattachments'] == 1 && ($mybb->get_input('newattachment') || $mybb->get_input('updateattachment') || ((($mybb->input['action'] == "do_newthread" && $mybb->get_input('submit')) || ($mybb->input['action'] == "newthread" && isset($mybb->input['previewpost'])) || isset($mybb->input['savedraft'])) && $_FILES['attachments'])))
-{
+if($mybb->settings['enableattachments'] == 1 && ($mybb->get_input('newattachment') || $mybb->get_input('updateattachment') || ((($mybb->input['action'] == "do_newthread" && $mybb->get_input('submit')) || ($mybb->input['action'] == "newthread" && isset($mybb->input['previewpost'])) || isset($mybb->input['savedraft'])) && (isset($_FILES['attachments'])?$_FILES['attachments']:false))))
 	// Verify incoming POST request
 	verify_post_check($mybb->get_input('my_post_key'));
 
@@ -1091,6 +1090,8 @@ if($mybb->input['action'] == "newthread" || $mybb->input['action'] == "editdraft
 			$captcha = $post_captcha->html;
 		}
 	}
+
+	$pollbox='';
 
 	if($forumpermissions['canpostpolls'] != 0)
 	{
