431988da457f7ff5931b432cc6d4e75a7f553f68
Axel Fontana <fontana.axel@gmail.com>
2025-04-21 14:54:35 +0200

    fix(admin): incorrect message when attempting to delete own account

--- admin/modules/user/users.php
+++ admin/modules/user/users.php
@@ -1806,6 +1806,12 @@ if($mybb->input['action'] == "delete")
 		admin_redirect("index.php?module=user-users");
 	}
 
+	if($mybb->user['uid'] == $user['uid'])
+	{
+		flash_message($lang->error_cannot_delete_self, 'error');
+		admin_redirect("index.php?module=user-users");
+	}
+
 	// User clicked no
 	if($mybb->get_input('no'))
 	{
