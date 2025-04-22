8145adfbb0570c74d48e266f00750b2cb7418d1d
dvz <devilshakerz@gmail.com>
2025-04-22 20:18:38 +0200

    Remove redundant groups in regex

--- inc/class_parser.php
+++ inc/class_parser.php
@@ -1704,13 +1704,13 @@ class postParser
 				(?P<link>
 					(?:
 						\[[0-9a-fA-F:]+(?:%[0-9a-zA-Z._-]+)?\]|	# IPv6 address with optional zone
-						(?:(?:\d{1,3}\.){3}\d{1,3})|			# IPv4 address
+						(?:\d{1,3}\.){3}\d{1,3}|				# IPv4 address
 						(?:[^\/\"\s<\[\]\.]+\.)*[\w-]+			# domain name
 					)
 					(?::[0-9]+)?								# optional port number
 					(?:/[^\"\s<\[\]&?#]*)?						# optional path
 					(?:\?(?:[^\"\s<\[\]?#]|\[\])*)?				# optional query
-					(?:\#(?:[^\"\s<\[\]])*)?					# optional fragment
+					(?:\#[^\"\s<\[\]]*)?						# optional fragment
 				)
 				(?:
 					(?<=&amp;)|(?<=&lt;)|(?<=&gt;)|				# allow trailing entities
