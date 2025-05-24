f7e15297d1c5caf73f92bdbd632c04ca46657206
dvz <devilshakerz@gmail.com>
2025-05-24 19:31:40 +0200

    1.8.39 meta

--- inc/languages/english.php
+++ inc/languages/english.php
@@ -27,7 +27,7 @@ $langinfo['website'] = "https://mybb.com/";
 //$langinfo['additional_name'] = " ";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1838";
+$langinfo['version'] = "1839";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
