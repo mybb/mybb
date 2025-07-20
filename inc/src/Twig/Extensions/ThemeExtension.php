6bd8d26ac6f65aa8367d5e64e7ecf05a7ac90ca7
dvz <devilshakerz@gmail.com>
2025-07-20 14:34:11 +0200

    Expose `get()` in Twig

--- inc/src/Twig/Extensions/ThemeExtension.php
+++ inc/src/Twig/Extensions/ThemeExtension.php
@@ -36,6 +36,7 @@ class ThemeExtension extends AbstractExtension implements GlobalsInterface
     public function getFunctions()
     {
         return [
+            new TwigFunction('get', [$this->view, 'getSharedData']),
             new TwigFunction('asset', [$this, 'getAsset']),
             new TwigFunction('asset_url', [$this, 'getAssetUrl']),
             new TwigFunction('alt_trow', [$this, 'altTrow']),
