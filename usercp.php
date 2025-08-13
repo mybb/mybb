d81f5603a9419ee518dcbfa60af49886383af272
Axel Fontana <axel.fontana@qualifio.com>
2025-08-13 17:02:49 +0200

    Fix signature save logic

--- usercp.php
+++ usercp.php
@@ -68,7 +68,7 @@ if($mybb->input['action'] == "do_editsig" && $mybb->request_method == "post")
 		$error = inline_error($userhandler->get_friendly_errors());
 	}
 
-	if(isset($error) || !empty($mybb->input['preview']))
+	if(!empty($error) || !empty($mybb->input['preview']))
 	{
 		$mybb->input['action'] = "editsig";
 	}
