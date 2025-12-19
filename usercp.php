709a12c757438ff08f3b4c4530b3aa86ebaa7afe
Axel Fontana <axel.fontana@qualifio.com>
2025-12-19 17:05:40 +0100

    Use render_avatar() instead of format_avatar() in user panel

--- usercp.php
+++ usercp.php
@@ -2028,8 +2028,6 @@ if($mybb->input['action'] == "avatar")
 		$avatarurl = htmlspecialchars_uni($mybb->user['avatar']);
 	}
 
-	$useravatar = format_avatar($mybb->user['avatar'], $mybb->user['avatardimensions'], '100x100');
-
 	if($mybb->settings['maxavatardims'] != "")
 	{
 		list($maxwidth, $maxheight) = preg_split('/[|x]/', my_strtolower($mybb->settings['maxavatardims']));
@@ -2046,7 +2044,6 @@ if($mybb->input['action'] == "avatar")
 
 	output_page(\MyBB\View\template('usercp/avatar.twig', [
 		'error' => $error,
-		'useravatar' => $useravatar,
 		'extranotes' => $extranotes
 	]));
 }
@@ -3492,8 +3489,6 @@ if(!$mybb->input['action'])
 
 	$lang->posts_day = $lang->sprintf($lang->posts_day, my_number_format($perday), $percent);
 	$mybb->user['regdate'] = my_date('relative', $mybb->user['regdate']);
-
-	$useravatar = format_avatar($mybb->user['avatar'], $mybb->user['avatardimensions'], '100x100');
 
 	// Make reputations row
 	$reputation_link = '';
