2fc9d28aed9a9af3c7b4c142470d7b6b22d8a852
Axel Fontana <axel.fontana@qualifio.com>
2025-12-22 16:47:03 +0100

    Replace undefined $thread['fid'] with $fid in newthread.php

--- newthread.php
+++ newthread.php
@@ -900,14 +900,14 @@ if($mybb->input['action'] == "newthread" || $mybb->input['action'] == "editdraft
 		}
 
 		$newthread['showcloseoption'] = false;
-		if(is_moderator($thread['fid'], "canopenclosethreads"))
+		if(is_moderator($fid, "canopenclosethreads"))
 		{
 			$newthread['showmodoptions'] = true;
 			$newthread['showcloseoption'] = true;
 		}
 
 		$newthread['showstickoption'] = false;
-		if(is_moderator($thread['fid'], "canstickunstickthreads"))
+		if(is_moderator($fid, "canstickunstickthreads"))
 		{
 			$newthread['showmodoptions'] = true;
 			$newthread['showstickoption'] = true;
