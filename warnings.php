ef884b6cf8fa3d74ec7462aff9ce54bc1263d303
Axel Fontana <axel.fontana@qualifio.com>
2025-12-22 19:19:31 +0100

    Make sure $warn_errors is initialized in warnings.php

--- warnings.php
+++ warnings.php
@@ -646,14 +646,13 @@ if($mybb->input['action'] == "view")
 	}
 
 	$revoked_user = '';
-	if(!$warning['daterevoked'])
+
+	if(!isset($warn_errors))
 	{
-		if(!isset($warn_errors))
-		{
-			$warn_errors = '';
-		}
+		$warn_errors = '';
 	}
-	else
+
+	if($warning['daterevoked'])
 	{
 		$warning['daterevoked'] = my_date('relative', $warning['daterevoked']);
 		$revoked_user = get_user($warning['revokedby']);
