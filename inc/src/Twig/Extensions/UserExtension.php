93b8ce0a45e5d6b0ca9a306636497773278ae11b
Axel Fontana <axel.fontana@qualifio.com>
2026-01-24 18:21:23 +0100

    Add is_member Twig filter to check user group membership

--- inc/src/Twig/Extensions/UserExtension.php
+++ inc/src/Twig/Extensions/UserExtension.php
@@ -13,6 +13,7 @@ class UserExtension extends AbstractExtension
             new TwigFilter('format_name', [$this, 'getFormattedName'], [
                 'is_safe' => ['html'],
             ]),
+            new TwigFilter('is_member', [$this, 'isMember']),
         ];
     }
 
@@ -33,5 +34,20 @@ class UserExtension extends AbstractExtension
         }
 
         return format_name(htmlspecialchars_uni($user['username']), $user['usergroup'] ?? 0, $user['displaygroup'] ?? 0);
+    }
+
+    /**
+     * Check if a user is a member of the specified group(s).
+     *
+     * @param array<int> $groups Array of group IDs to check membership for. Pass [-1] to check if user is in any group.
+     * @param int|array|null $user Either a user ID, user data array, or null for the current user.
+     * @return bool True if the user is a member of any of the specified groups.
+     */
+    public function isMember(array $groups, int|array|null $user = null): bool
+    {
+        if ($user === null) {
+            return (bool)is_member($groups);
+        }
+        return (bool)is_member($groups, $user);
     }
 }
