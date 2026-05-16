d12ec5f22375c546833af47d5ec6f7985636eb76
Axel Fontana <key+gpg@axelfontana.com>
2026-05-16 12:49:31 +0200

    Use icon classes for post icons in messenger

--- private.php
+++ private.php
@@ -456,10 +456,10 @@ if($mybb->input['action'] == "results")
 		if($message['icon'] > 0 && !empty($icon_cache[$message['icon']]))
 		{
 			$message['hasicon'] = true;
-			$icon = $icon_cache[$message['icon']];
-			$icon['path'] = str_replace("{theme}", $theme['imgdir'], $icon['path']);
+			$icon = build_post_icon($icon_cache[$message['icon']], $theme['imgdir']);
 			$message['icon_path'] = $icon['path'];
 			$message['icon_name'] = $icon['name'];
+			$message['icon_class'] = $icon['icon_class'];
 		}
 
 		if(!trim($message['subject']))
@@ -2331,10 +2331,10 @@ if(!$mybb->input['action'])
 			if($message['icon'] > 0 && !empty($icon_cache[$message['icon']]))
 			{
 				$message['hasicon'] = true;
-				$icon = $icon_cache[$message['icon']];
-				$icon['path'] = str_replace("{theme}", $theme['imgdir'], $icon['path']);
+				$icon = build_post_icon($icon_cache[$message['icon']], $theme['imgdir']);
 				$message['icon_path'] = $icon['path'];
 				$message['icon_name'] = $icon['name'];
+				$message['icon_class'] = $icon['icon_class'];
 			}
 
 			if(!trim($message['subject']))
