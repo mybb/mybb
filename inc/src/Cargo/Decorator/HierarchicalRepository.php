70500291ca640dc71ef7f51789cd0d9c037298fe
dvz <devilshakerz@gmail.com>
2026-06-05 19:23:18 +0200

    Use `getExisting()` to assert resolution behavior

--- inc/src/Cargo/Decorator/HierarchicalRepository.php
+++ inc/src/Cargo/Decorator/HierarchicalRepository.php
@@ -131,6 +131,7 @@ abstract class HierarchicalRepository extends RepositoryDecorator implements Rep
 
             foreach ($entities as $key => $entity) {
                 if (!in_array($key, $disinherited)) {
+                     // return hierarchical entities; don't use getExisting() to avoid premature resolution
                     $results[$key] ??= $this->get($key);
                 }
             }
@@ -149,7 +150,7 @@ abstract class HierarchicalRepository extends RepositoryDecorator implements Rep
      */
     public function getResolved(string $key): ?EntityInterface
     {
-        return $this->getResolvedRepository($key)?->get($key);
+        return $this->getResolvedRepository($key)?->getExisting($key);
     }
 
     /**
@@ -162,7 +163,7 @@ abstract class HierarchicalRepository extends RepositoryDecorator implements Rep
             throw new LogicException('`' . __FUNCTION__ . '`() cannot be called without existing reference entity');
         }
 
-        return $this->getClosestEntityAncestorRepository($key)?->get($key);
+        return $this->getClosestEntityAncestorRepository($key)?->getExisting($key);
     }
 
     /**
