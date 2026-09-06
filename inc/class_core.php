f88de74a58fd98ecab30f1c17d6afda947d9a085
dvz <devilshakerz@gmail.com>
2026-09-06 07:48:31 +0200

    1.8.41 meta

--- inc/class_core.php
+++ inc/class_core.php
@@ -14,14 +14,14 @@ class MyBB {
 	 *
 	 * @var string
 	 */
-	public $version = "1.8.40";
+	public $version = "1.8.41";
 
 	/**
 	 * The version code of MyBB we're running.
 	 *
 	 * @var integer
 	 */
-	public $version_code = 1840;
+	public $version_code = 1841;
 
 	/**
 	 * The current working directory.
