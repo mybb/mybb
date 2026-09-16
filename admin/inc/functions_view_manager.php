50fc476c5023d5264be1a95ab2fb98be9a75a208
dvz <devilshakerz@gmail.com>
2026-09-16 17:55:05 +0200

    Fix ACP Users View Manager XSS (GHSA-22wp-jxm2-5w7m)
    (cherry picked from commit e5b349664999334643302c8a57446ed2ff4e6c8e)

--- admin/inc/functions_view_manager.php
+++ admin/inc/functions_view_manager.php
@@ -606,7 +606,7 @@ document.write('".str_replace("/", "\/", $field_select)."');
 				$view['title'] = $lang->$title_string;
 			}
 
-			$table->construct_cell("<div class=\"float_right\"><img src=\"styles/{$page->style}/images/icons/{$view_type}.png\" title=\"".$lang->sprintf($lang->this_is_a_view, $view_type)."\" alt=\"{$view_type}\" /></div><div class=\"{$default_class}\"><strong><a href=\"{$base_url}&amp;action=views&amp;do=edit&amp;vid={$view['vid']}\" >{$view['title']}</a></strong>{$default_add}{$created}</div>");
+			$table->construct_cell("<div class=\"float_right\"><img src=\"styles/{$page->style}/images/icons/{$view_type}.png\" title=\"".$lang->sprintf($lang->this_is_a_view, $view_type)."\" alt=\"{$view_type}\" /></div><div class=\"{$default_class}\"><strong><a href=\"{$base_url}&amp;action=views&amp;do=edit&amp;vid={$view['vid']}\" >".htmlspecialchars_uni($view['title'])."</a></strong>{$default_add}{$created}</div>");
 
 			$popup = new PopupMenu("view_{$view['vid']}", $lang->options);
 			$popup->add_item($lang->edit_view, "{$base_url}&amp;action=views&amp;do=edit&amp;vid={$view['vid']}");
